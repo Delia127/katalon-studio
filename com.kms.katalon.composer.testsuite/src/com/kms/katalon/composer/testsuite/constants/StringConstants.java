555a793a35000dbb7a4cc3ccd9decdc642f6694b
An Truong Nguyen <antruongnguyen@kms-technology.com>
2015-09-14 17:07:08 +0700

    Fixed #826 - [Enhancement] - [Test suites] - Add a tooltip for explaining the "Re-run" function

--- com.kms.katalon.composer.testsuite/src/com/kms/katalon/composer/testsuite/constants/StringConstants.java
+++ com.kms.katalon.composer.testsuite/src/com/kms/katalon/composer/testsuite/constants/StringConstants.java
@@ -3,122 +3,122 @@ package com.kms.katalon.composer.testsuite.constants;
 import com.kms.katalon.constants.GlobalStringConstants;
 
 public class StringConstants extends GlobalStringConstants {
-	// TestDataToolItemConstants
-	public static final String CONS_ADD = ADD;
-	public static final String CONS_ADD_BEFORE = "Add before";
-	public static final String CONS_ADD_AFTER = "Add after";
-	public static final String CONS_ADD_CHILDREN = "Add children";
-	public static final String CONS_REMOVE = DELETE;
-	public static final String CONS_UP = UP;
-	public static final String CONS_DOWN = DOWN;
-	public static final String CONS_MAP = "Map";
-	public static final String CONS_MAPALL = "Map All";
+    // TestDataToolItemConstants
+    public static final String CONS_ADD = ADD;
+    public static final String CONS_ADD_BEFORE = "Add before";
+    public static final String CONS_ADD_AFTER = "Add after";
+    public static final String CONS_ADD_CHILDREN = "Add children";
+    public static final String CONS_REMOVE = DELETE;
+    public static final String CONS_UP = UP;
+    public static final String CONS_DOWN = DOWN;
+    public static final String CONS_MAP = "Map";
+    public static final String CONS_MAPALL = "Map All";
 
-	// DataColumnFinderDialog
-	public static final String DIA_LBL_SELECT_TEST_DATA = "Select a Test Data";
-	public static final String DIA_TXT_ENTER_TEXT_TO_SEARCH = "Enter text to search";
-	public static final String DIA_LBL_TEST_DATA_HIERARCHY = "Test Data's hierarchy";
-	public static final String DIA_LBL_SELECT_A_COL = "Select a column";
-	public static final String DIA_LBL_LIST_OF_COL_NAMES = "List of column names";
+    // DataColumnFinderDialog
+    public static final String DIA_LBL_SELECT_TEST_DATA = "Select a Test Data";
+    public static final String DIA_TXT_ENTER_TEXT_TO_SEARCH = "Enter text to search";
+    public static final String DIA_LBL_TEST_DATA_HIERARCHY = "Test Data's hierarchy";
+    public static final String DIA_LBL_SELECT_A_COL = "Select a column";
+    public static final String DIA_LBL_LIST_OF_COL_NAMES = "List of column names";
 
-	// DataIterationDialog
-	public static final String DIA_SHELL_DATA_ITERATION = "Data Iteration";
-	public static final String DIA_BTN_RUN_ALL_ROWS = "Run all rows";
-	public static final String DIA_BTN_RUN_FROM_ROW = "Run from row";
-	public static final String DIA_LBL_TO_ROW = "to row";
-	public static final String DIA_BTN_RUN_SPECIFIC_ROWS = "Run with specific rows";
+    // DataIterationDialog
+    public static final String DIA_SHELL_DATA_ITERATION = "Data Iteration";
+    public static final String DIA_BTN_RUN_ALL_ROWS = "Run all rows";
+    public static final String DIA_BTN_RUN_FROM_ROW = "Run from row";
+    public static final String DIA_LBL_TO_ROW = "to row";
+    public static final String DIA_BTN_RUN_SPECIFIC_ROWS = "Run with specific rows";
 
-	// NewTestSuiteDialog
-	public static final String DIA_TITLE_TEST_SUITE = TEST_SUITE;
-	public static final String DIA_MSG_CREATE_NEW_TEST_SUITE = "Create Test Suite";
+    // NewTestSuiteDialog
+    public static final String DIA_TITLE_TEST_SUITE = TEST_SUITE;
+    public static final String DIA_MSG_CREATE_NEW_TEST_SUITE = "Create Test Suite";
 
-	// TestDataLinkFinderDialog
-	public static final String DIA_SHELL_TEST_DATA_LINK_BROWSER = "Test Data Link Browser";
-	public static final String DIA_TREE_VIEWER_COL_ID = ID;
+    // TestDataLinkFinderDialog
+    public static final String DIA_SHELL_TEST_DATA_LINK_BROWSER = "Test Data Link Browser";
+    public static final String DIA_TREE_VIEWER_COL_ID = ID;
 
-	// TestDataCellEditor
-	public static final String EDI_TITLE_TEST_DATA_BROWSER = "Test Data Browser";
-	public static final String ERROR_TITLE = ERROR;
-	public static final String EDI_ERROR_MSG_CANNOT_OPEN_DIA = "Cannot open dialog.";
+    // TestDataCellEditor
+    public static final String EDI_TITLE_TEST_DATA_BROWSER = "Test Data Browser";
+    public static final String ERROR_TITLE = ERROR;
+    public static final String EDI_ERROR_MSG_CANNOT_OPEN_DIA = "Cannot open dialog.";
 
-	// DeleteTestSuiteHandler
-	public static final String HAND_ERROR_MSG_UNABLE_TO_DEL_TEST_SUITE = "Unable to delete Test Suite";
+    // DeleteTestSuiteHandler
+    public static final String HAND_ERROR_MSG_UNABLE_TO_DEL_TEST_SUITE = "Unable to delete Test Suite";
 
-	// NewTestSuiteHandler
-	public static final String HAND_DEFAULT_NAME_NEW_TEST_SUITE = "New Test Suite";
-	public static final String HAND_ERROR_MSG_UNABLE_TO_CREATE_TEST_SUITE = "Unable to create Test Suite";
+    // NewTestSuiteHandler
+    public static final String HAND_DEFAULT_NAME_NEW_TEST_SUITE = "New Test Suite";
+    public static final String HAND_ERROR_MSG_UNABLE_TO_CREATE_TEST_SUITE = "Unable to create Test Suite";
 
-	// OpenTestSuiteHandler
-	public static final String HAND_DEFAULT_CONTAINER_DATA = "100";
-	public static final String BUNDLE_URI_TEST_SUITE = "bundleclass://com.kms.katalon.composer.testsuite/";
-	public static final String HAND_ERROR_MSG_CANNOT_OPEN_TEST_SUITE = "Cannot open Test Suite.";
+    // OpenTestSuiteHandler
+    public static final String HAND_DEFAULT_CONTAINER_DATA = "100";
+    public static final String BUNDLE_URI_TEST_SUITE = "bundleclass://com.kms.katalon.composer.testsuite/";
+    public static final String HAND_ERROR_MSG_CANNOT_OPEN_TEST_SUITE = "Cannot open Test Suite.";
 
-	// RenameTestSuiteHandler
-	public static final String HAND_ERROR_MSG_UNABLE_TO_RENAME_TEST_SUITE = "Unable to rename Test Suite";
+    // RenameTestSuiteHandler
+    public static final String HAND_ERROR_MSG_UNABLE_TO_RENAME_TEST_SUITE = "Unable to rename Test Suite";
 
-	// TestDataToolItemListener
-	public static final String LIS_TITLE_TEST_DATA_BROWSER = "Test Data Browser";
-	public static final String LIS_ERROR_MSG_UNABLE_TO_ADD_TEST_DATA = "Unable to add Test Data.";
-	public static final String LIS_INFO_MSG_DONE = DONE;
+    // TestDataToolItemListener
+    public static final String LIS_TITLE_TEST_DATA_BROWSER = "Test Data Browser";
+    public static final String LIS_ERROR_MSG_UNABLE_TO_ADD_TEST_DATA = "Unable to add Test Data.";
+    public static final String LIS_INFO_MSG_DONE = DONE;
 
-	// NewTestSuitePopupMenuContribution
-	public static final String MENU_CONTEXT_TEST_SUITE = TEST_SUITE;
-	public static final String COMMAND_ID_ADD_TEST_SUITE = "com.kms.katalon.composer.testsuite.command.add";
+    // NewTestSuitePopupMenuContribution
+    public static final String MENU_CONTEXT_TEST_SUITE = TEST_SUITE;
+    public static final String COMMAND_ID_ADD_TEST_SUITE = "com.kms.katalon.composer.testsuite.command.add";
 
-	// TestSuiteCompositePart
-	public static final String PA_TAB_MAIN = "Main";
-	public static final String PA_TAB_INTEGRATION = "Integration";
-	public static final String PA_ERROR_MSG_UNABLE_TO_SAVE_PART = "Unable to save part.";
-	public static final String PA_CONFIRM_TITLE_FILE_CHANGED = "File changed";
-	public static final String PA_CONFIRM_MSG_RELOAD_FILE = "The file ''{0}'' has been changed on the file system. Do you want to reload it?";
+    // TestSuiteCompositePart
+    public static final String PA_TAB_MAIN = "Main";
+    public static final String PA_TAB_INTEGRATION = "Integration";
+    public static final String PA_ERROR_MSG_UNABLE_TO_SAVE_PART = "Unable to save part.";
+    public static final String PA_CONFIRM_TITLE_FILE_CHANGED = "File changed";
+    public static final String PA_CONFIRM_MSG_RELOAD_FILE = "The file ''{0}'' has been changed on the file system. Do you want to reload it?";
 
-	// TestSuitePart
-	public static final String PA_COL_RUN = RUN;
-	public static final String PA_COL_ID = ID;
-	public static final String PA_COL_NO = NO_;
-	public static final String PA_COL_NAME = NAME;
-	public static final String PA_COL_DESC = DESCRIPTION;
-	public static final String PA_LBL_LAST_UPDATED = "Last updated";
-	public static final String PA_LBL_CREATED_DATE = "Created date";
-	public static final String PA_LBL_DESC = DESCRIPTION;
-	public static final String PA_LBL_LAST_RUN = "Last run";
-	public static final String PA_LBL_RE_RUN = "Re-run";
-	public static final String PA_LBL_TIP_LAST_RUN = "Navigate to the report page of the last run";
-	public static final String PA_LBL_ID = ID;
-	public static final String PA_LBL_NAME = NAME;
-	public static final String PA_LBL_MAIL_RECIPIENTS = "Mail Recipients";
-	public static final String PA_LBL_PAGE_LOAD_TIMEOUT = "Page load timeout";
-	public static final String WARN_TITLE = WARN;
-	public static final String PA_WARN_MSG_REPORT_FILE_DOES_NOT_EXIST = "Report file doesn't exist";
-	public static final String PA_ERROR_MSG_UNABLE_TO_OPEN_REPORT = "Unable to open Report";
-	public static final String PA_ERROR_MSG_UNABLE_TO_LOAD_TEST_SUITE = "Unable to load Test Suite";
-	public static final String PA_BTN_ADD = ADD;
-	public static final String PA_BTN_DEL = DELETE;
-	public static final String PA_BTN_CLEAR = CLEAR;
-	public static final String PA_TOOLITEM_ADD = ADD;
-	public static final String PA_TOOLITEM_REMOVE = DELETE;
-	public static final String PA_TOOLITEM_UP = UP;
-	public static final String PA_TOOLITEM_DOWN = DOWN;
-	public static final String PA_TITLE_TEST_CASE_BROWSER = "Test Case Browser";
-	public static final String PA_ERROR_MSG_UNABLE_TO_ADD_TEST_CASES = "Unable to add Test Case(s).";
-	public static final String PA_LBL_TEST_DATA = TEST_DATA;
-	public static final String PA_TREE_VIEWER_COL_NO = NO_;
-	public static final String PA_TREE_VIEWER_COL_ID = ID;
-	public static final String PA_TREE_VIEWER_COL_NAME = NAME;
-	public static final String PA_TREE_VIEWER_COL_DATA_ITERATION = "Data Iteration";
-	public static final String PA_TREE_VIEWER_COL_TYPE = "Type";
-	public static final String PA_TREE_VIEWER_COL_DEFAULT_VAL = "Default value";
-	public static final String PA_TREE_VIEWER_COL_TEST_DATA = TEST_DATA;
-	public static final String PA_TREE_VIEWER_COL_VALUE = VALUE;
-	public static final String PA_LBL_VAR_BINDING = "Variable Binding";
-	public static final String PA_WARN_MSG_DATA_SRC_NOT_AVAILABLE = "Data source of Test Data: ''{0}'' is not available.";
-	public static final String PA_SEARCH_TEXT_DEFAULT_VALUE = "Enter text to search...";
-	public static final String PA_IMAGE_TIP_ADVANCED_SEARCH = "Advanced "
-			+ SEARCH;
-	public static final String PA_IMAGE_TIP_SEARCH = SEARCH;
-	public static final String PA_IMAGE_TIP_CLOSE_SEARCH = CLEAR;
-	public static final String PA_ERROR_MSG_RERUN_NUMBER_X_INVALID = "Invalid re-run value ''{0}'' (must be a number)";
+    // TestSuitePart
+    public static final String PA_COL_RUN = RUN;
+    public static final String PA_COL_ID = ID;
+    public static final String PA_COL_NO = NO_;
+    public static final String PA_COL_NAME = NAME;
+    public static final String PA_COL_DESC = DESCRIPTION;
+    public static final String PA_LBL_LAST_UPDATED = "Last updated";
+    public static final String PA_LBL_CREATED_DATE = "Created date";
+    public static final String PA_LBL_DESC = DESCRIPTION;
+    public static final String PA_LBL_LAST_RUN = "Last run";
+    public static final String PA_LBL_RETRY = "Retry";
+    public static final String PA_LBL_TIP_LAST_RUN = "Navigate to the report page of the last run";
+    public static final String PA_LBL_ID = ID;
+    public static final String PA_LBL_NAME = NAME;
+    public static final String PA_LBL_MAIL_RECIPIENTS = "Mail Recipients";
+    public static final String PA_LBL_PAGE_LOAD_TIMEOUT = "Page load timeout";
+    public static final String WARN_TITLE = WARN;
+    public static final String PA_WARN_MSG_REPORT_FILE_DOES_NOT_EXIST = "Report file doesn't exist";
+    public static final String PA_ERROR_MSG_UNABLE_TO_OPEN_REPORT = "Unable to open Report";
+    public static final String PA_ERROR_MSG_UNABLE_TO_LOAD_TEST_SUITE = "Unable to load Test Suite";
+    public static final String PA_BTN_ADD = ADD;
+    public static final String PA_BTN_DEL = DELETE;
+    public static final String PA_BTN_CLEAR = CLEAR;
+    public static final String PA_TOOLITEM_ADD = ADD;
+    public static final String PA_TOOLITEM_REMOVE = DELETE;
+    public static final String PA_TOOLITEM_UP = UP;
+    public static final String PA_TOOLITEM_DOWN = DOWN;
+    public static final String PA_TITLE_TEST_CASE_BROWSER = "Test Case Browser";
+    public static final String PA_ERROR_MSG_UNABLE_TO_ADD_TEST_CASES = "Unable to add Test Case(s).";
+    public static final String PA_LBL_TEST_DATA = TEST_DATA;
+    public static final String PA_TREE_VIEWER_COL_NO = NO_;
+    public static final String PA_TREE_VIEWER_COL_ID = ID;
+    public static final String PA_TREE_VIEWER_COL_NAME = NAME;
+    public static final String PA_TREE_VIEWER_COL_DATA_ITERATION = "Data Iteration";
+    public static final String PA_TREE_VIEWER_COL_TYPE = "Type";
+    public static final String PA_TREE_VIEWER_COL_DEFAULT_VAL = "Default value";
+    public static final String PA_TREE_VIEWER_COL_TEST_DATA = TEST_DATA;
+    public static final String PA_TREE_VIEWER_COL_VALUE = VALUE;
+    public static final String PA_LBL_VAR_BINDING = "Variable Binding";
+    public static final String PA_WARN_MSG_DATA_SRC_NOT_AVAILABLE = "Data source of Test Data: ''{0}'' is not available.";
+    public static final String PA_SEARCH_TEXT_DEFAULT_VALUE = "Enter text to search...";
+    public static final String PA_IMAGE_TIP_ADVANCED_SEARCH = "Advanced " + SEARCH;
+    public static final String PA_IMAGE_TIP_SEARCH = SEARCH;
+    public static final String PA_IMAGE_TIP_CLOSE_SEARCH = CLEAR;
+    public static final String PA_ERROR_MSG_RERUN_NUMBER_X_INVALID = "Invalid re-run value ''{0}'' (must be a number)";
+    public static final String PA_LBL_TOOLTIP_RETRY = "Please define the maximum number of retries that a failed test case would be re-executed until it is successfully passed.";
 
-	// VariableValueEditingSupport
-	public static final String SUP_WARN_MSG_TEST_DATA_NOT_AVAILABLE = "Test Data is not available.";
+    // VariableValueEditingSupport
+    public static final String SUP_WARN_MSG_TEST_DATA_NOT_AVAILABLE = "Test Data is not available.";
 }
