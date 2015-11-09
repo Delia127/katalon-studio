53f639a2e2198a57a5fbe1738ee53e08f2746c51
hieumai <hieumai@kms-technology.com>
2015-11-09 11:18:45 +0700

    KAT-207: Core Informatics Request - Output OS version into Katalon log file (https://incubation.kms-technology.com/browse/KAT-207)

--- source/com.kms.katalon.core.webui/src/com/kms/katalon/core/webui/driver/DriverFactory.java
+++ source/com.kms.katalon.core.webui/src/com/kms/katalon/core/webui/driver/DriverFactory.java
@@ -145,14 +145,13 @@ public class DriverFactory {
             case IOS_DRIVER:
                 webDriver = WebMobileDriverFactory.getInstance().getIosDriver(getMobileDeviceName());
                 break;
-            case EDGE_DRIVER: 
+            case EDGE_DRIVER:
                 EdgeDriverService edgeService = localEdgeDriverServiceStorage.get();
                 if (!edgeService.isRunning()) {
                     edgeService.start();
                 }
                 webDriver = new EdgeDriver(edgeService, WebDriverPropertyUtil.toDesireCapabilities(
-                        RunConfiguration.getExecutionDriverProperty(),
-                        DesiredCapabilities.edge(), false));
+                        RunConfiguration.getExecutionDriverProperty(), DesiredCapabilities.edge(), false));
                 break;
             default:
                 throw new StepFailedException(MessageFormat.format(StringConstants.DRI_ERROR_DRIVER_X_NOT_IMPLEMENTED,
@@ -175,7 +174,8 @@ public class DriverFactory {
         KeywordLogger logger = KeywordLogger.getInstance();
         logger.logRunData("sessionId", ((RemoteWebDriver) webDriver).getSessionId().toString());
         logger.logRunData("browser", WebUiCommonHelper.getBrowserAndVersion(webDriver));
-        logger.logRunData("platform", ((RemoteWebDriver) webDriver).getCapabilities().getPlatform().toString());
+        logger.logRunData("platform", webDriver.getClass() == RemoteWebDriver.class ? ((RemoteWebDriver) webDriver)
+                .getCapabilities().getPlatform().toString() : System.getProperty("os.name"));
     }
 
     public static WebDriver openWebDriver(DriverType driver, String projectDir, Object options) throws Exception {
