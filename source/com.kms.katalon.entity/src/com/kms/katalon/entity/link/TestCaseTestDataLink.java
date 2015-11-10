05f1c1e18870e1101dd31bb102bbdc66f7b72e99
Duy Luong <duyluong@kms-technology.com>
2015-11-10 16:26:20 +0700

    Fixed: KAT-66 [Test Suite] - Implement drag and drop line in Test Data

--- source/com.kms.katalon.entity/src/com/kms/katalon/entity/link/TestCaseTestDataLink.java
+++ source/com.kms.katalon.entity/src/com/kms/katalon/entity/link/TestCaseTestDataLink.java
@@ -55,4 +55,36 @@ public class TestCaseTestDataLink implements Serializable {
     public void setId(String id) {
         this.id = id;
     }
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((combinationType == null) ? 0 : combinationType.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((iterationEntity == null) ? 0 : iterationEntity.hashCode());
+        result = prime * result + ((testDataId == null) ? 0 : testDataId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        TestCaseTestDataLink other = (TestCaseTestDataLink) obj;
+        if (combinationType != other.combinationType) return false;
+        if (id == null) {
+            if (other.id != null) return false;
+        } else if (!id.equals(other.id)) return false;
+        if (iterationEntity == null) {
+            if (other.iterationEntity != null) return false;
+        } else if (!iterationEntity.equals(other.iterationEntity)) return false;
+        if (testDataId == null) {
+            if (other.testDataId != null) return false;
+        } else if (!testDataId.equals(other.testDataId)) return false;
+        return true;
+    }
+
 }
