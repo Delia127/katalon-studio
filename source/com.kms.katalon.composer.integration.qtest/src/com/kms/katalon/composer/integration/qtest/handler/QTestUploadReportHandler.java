8d83118f16ba8a013d6b499d6b8fdbabd2dcba5b
Duy Luong <duyluong@kms-technology.com>
2015-11-13 10:33:55 +0700

    Fixed a bug that causes user cannot upload test logs

--- source/com.kms.katalon.composer.integration.qtest/src/com/kms/katalon/composer/integration/qtest/handler/QTestUploadReportHandler.java
+++ source/com.kms.katalon.composer.integration.qtest/src/com/kms/katalon/composer/integration/qtest/handler/QTestUploadReportHandler.java
@@ -8,6 +8,7 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.swt.widgets.Shell;
@@ -47,7 +48,7 @@ public class QTestUploadReportHandler extends AbstractQTestHandler {
     private ESelectionService selectionService;
 
     @CanExecute
-    public boolean canExecute() {
+    public boolean canExecute(MDirectMenuItem item) {
         try {
             Object selectedEntity = getFirstSelectedObject(selectionService);
 
@@ -66,7 +67,7 @@ public class QTestUploadReportHandler extends AbstractQTestHandler {
                 if (!QTestIntegrationUtil.isFolderReportInTestSuiRepo(folderEntity, getProjectEntity())) {
                     return false;
                 }
-                
+
                 for (Object childObject : FolderController.getInstance().getAllDescentdantEntities(folderEntity)) {
                     if (!(childObject instanceof ReportEntity)) {
                         continue;
@@ -109,10 +110,15 @@ public class QTestUploadReportHandler extends AbstractQTestHandler {
         if (testSuiteLogRecord == null) {
             return null;
         }
+        
+        if (QTestIntegrationUtil.getSelectedQTestSuite(testSuiteLogRecord) == null) {
+            return null;
+        }
 
         List<TestCaseLogRecord> testCasesCanBeUploaded = new ArrayList<TestCaseLogRecord>();
 
         for (ILogRecord selectedTestCaseLogObject : testSuiteLogRecord.getChildRecords()) {
+
             TestCaseLogRecord testCaseLogRecord = (TestCaseLogRecord) selectedTestCaseLogObject;
             switch (QTestIntegrationUtil.evaluateTestCaseLog(testCaseLogRecord,
                     QTestIntegrationUtil.getSelectedQTestSuite(testSuiteLogRecord), reportEntity)) {
