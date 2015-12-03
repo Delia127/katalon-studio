7e839b4a43c2937c65f30cfc8cd3fd1d00f466ec
hieumai <hieumai@kms-technology.com>
2015-12-03 15:17:36 +0700

    KAT-290: [Log Viewer] Separate message logs and exception logs (https://incubation.kms-technology.com/browse/KAT-290)

--- source/com.kms.katalon.core/src/com/kms/katalon/core/exception/ExceptionsUtil.java
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/exception/ExceptionsUtil.java
@@ -1,26 +1,54 @@
 package com.kms.katalon.core.exception;
 
+import groovy.lang.MissingPropertyException;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class ExceptionsUtil {
-	public static String getMessageForThrowable(Throwable t) {
-		if (t == null) {
-			return "";
-		}
-		StringBuilder message = null;
+    public static String getMessageForThrowable(Throwable t) {
+        if (t == null) {
+            return "";
+        }
+        return getExceptionMessage(t);
+    }
 
-		if (!(t instanceof StepFailedException)) {
-			message = new StringBuilder(t.getClass().getName() + (t.getMessage() != null ? (": " + t.getMessage()) : ""));
-			//StringWriter sw = new StringWriter();
-			//t.printStackTrace(new PrintWriter(sw));
-			//message.append(" (Stack trace: " + sw.toString() + ")");
-		} else {
-			message = new StringBuilder(t.getMessage() != null ? (t.getMessage()) : "");
-		}
-		//if (t.getCause() != null) {
-		//	message.append(" (caused by: ");
-		//	message.append(getMessageForThrowable(t.getCause()));
-		//	message.append(")");
-		//}
-		return message.toString();
-	}
+    private static String getExceptionMessage(Throwable throwable) {
+        if (throwable instanceof MissingPropertyException) {
+            return getExceptionMessage((MissingPropertyException) throwable);
+        } else if (throwable instanceof StepFailedException) {
+            return getExceptionMessage((StepFailedException) throwable);
+        } else if (throwable instanceof StepErrorException) {
+            return getExceptionMessage((StepErrorException) throwable);
+        } else {
+            return throwable.getClass().getName()
+                    + (throwable.getMessage() != null ? (": " + throwable.getMessage()) : "");
+        }
+    }
+
+    private static String getExceptionMessage(StepFailedException exception) {
+        return exception.getMessage() != null ? (exception.getMessage()) : "";
+    }
+
+    private static String getExceptionMessage(StepErrorException exception) {
+        return exception.getMessage() != null ? (exception.getMessage()) : "";
+    }
+
+    private static String getExceptionMessage(MissingPropertyException exception) {
+        return "Variable '" + exception.getProperty() + "' is not defined for test case.";
+    }
+
+    public static String getStackTraceForThrowable(Throwable t) {
+        StringBuilder message = new StringBuilder("(Stack trace: ");
+        StringWriter sw = new StringWriter();
+        t.printStackTrace(new PrintWriter(sw));
+        message.append(sw.toString());
+        if (t.getCause() != null) {
+            message.append(" (caused by: ");
+            message.append(getStackTraceForThrowable(t.getCause()));
+            message.append(")");
+        }
+        message.append(")");
+        return message.toString();
+    }
 }
