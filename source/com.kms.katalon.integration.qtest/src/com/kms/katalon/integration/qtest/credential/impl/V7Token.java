ed094dfd724084b0e9f4c94baa5fea2ac5dcc413
Duy Luong <duyluong@kms-technology.com>
2016-01-12 11:07:49 +0700

    [qTest Integration] Added loading image when generating qTest token

--- source/com.kms.katalon.integration.qtest/src/com/kms/katalon/integration/qtest/credential/impl/V7Token.java
+++ source/com.kms.katalon.integration.qtest/src/com/kms/katalon/integration/qtest/credential/impl/V7Token.java
@@ -8,6 +8,10 @@ import com.kms.katalon.integration.qtest.setting.QTestVersion;
 
 public class V7Token extends AbstractQTestToken {
     
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String TOKEN_TYPE = "token_type";
+    public static final String DF_TOKEN_TYPE = "bearer";
+    
     public V7Token(String rawToken) {
         super(rawToken);
     }
@@ -21,16 +25,16 @@ public class V7Token extends AbstractQTestToken {
     public String getAccessTokenHeader() throws QTestInvalidFormatException {
         try {
             JsonObject js = new JsonObject(rawToken);
-            if (!js.has("access_token")) {
+            if (!js.has(ACCESS_TOKEN)) {
                 throw QTestInvalidFormatException.createInvalidTokenException("Missing access_token in " + rawToken);
             }
             
-            if (!js.has("token_type")) {
+            if (!js.has(TOKEN_TYPE)) {
                 throw QTestInvalidFormatException.createInvalidTokenException("Missing token_type in " + rawToken);
             }
             
             StringBuilder accessTokenBuilder = new StringBuilder();
-            accessTokenBuilder.append(js.get("token_type")).append(" ").append(js.get("access_token"));
+            accessTokenBuilder.append(js.get(TOKEN_TYPE)).append(" ").append(js.get(ACCESS_TOKEN));
             
             return accessTokenBuilder.toString();
         } catch (JsonException e) {
@@ -42,11 +46,11 @@ public class V7Token extends AbstractQTestToken {
     public String getAccessToken() throws QTestInvalidFormatException {
         try {
             JsonObject js = new JsonObject(rawToken);
-            if (!js.has("access_token")) {
+            if (!js.has(ACCESS_TOKEN)) {
                 throw QTestInvalidFormatException.createInvalidTokenException("Missing access_token in " + rawToken);
             }
             
-            return js.getString("access_token");
+            return js.getString(ACCESS_TOKEN);
         } catch (JsonException e) {
             throw QTestInvalidFormatException.createInvalidJsonFormatException(e.getMessage());
         }
