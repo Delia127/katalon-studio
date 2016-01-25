0f028c4318c5b2d86765875c2b072048970b909f
hieumai <hieumai@kms-technology.com>
2016-01-25 10:25:20 +0700

    KAT-528: [Keyword Browser] Improve searching functionality to support searching for separate words (https://incubation.kms-technology.com/browse/KAT-528)

--- source/com.kms.katalon.composer.testcase/src/com/kms/katalon/composer/testcase/providers/KeywordBrowserEntityViewerFilter.java
+++ source/com.kms.katalon.composer.testcase/src/com/kms/katalon/composer/testcase/providers/KeywordBrowserEntityViewerFilter.java
@@ -4,38 +4,41 @@ import org.apache.commons.lang.StringUtils;
 import org.eclipse.jface.viewers.Viewer;
 
 import com.kms.katalon.composer.components.impl.providers.AbstractEntityViewerFilter;
+import com.kms.katalon.composer.components.impl.util.TreeEntityUtil;
 import com.kms.katalon.composer.testcase.keywords.IKeywordBrowserTreeEntity;
 
 public class KeywordBrowserEntityViewerFilter extends AbstractEntityViewerFilter {
-	private String searchString;
+    private String searchString;
 
-	public void setSearchString(String searchString) {
-		this.searchString = searchString;
-	}
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
 
-	@Override
-	public boolean select(Viewer viewer, Object parentElement, Object element) {
-		if (searchString == null || searchString.equals(StringUtils.EMPTY)) {
-			return true;
-		}
-		if (element instanceof IKeywordBrowserTreeEntity) {
-			
-			IKeywordBrowserTreeEntity entity = ((IKeywordBrowserTreeEntity) element);
-			if (entity.getParent() != null && entity.getParent().getName().toLowerCase().contains(searchString.toLowerCase())) {
-				return true;
-			}
-			
-			if (entity.getName().toLowerCase().contains(searchString.toLowerCase())) {
-				return true;
-			}
-			if (entity.hasChildren()) {
-				boolean isChildSelected = false;
-				for (Object child : entity.getChildren()) {
-					isChildSelected |= select(viewer, element, child);
-				}
-				return isChildSelected;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean select(Viewer viewer, Object parentElement, Object element) {
+        if (searchString == null || searchString.equals(StringUtils.EMPTY)) {
+            return true;
+        }
+        if (element instanceof IKeywordBrowserTreeEntity) {
+
+            IKeywordBrowserTreeEntity entity = ((IKeywordBrowserTreeEntity) element);
+            if (entity.getParent() != null
+                    && entity.getParent().getName().toLowerCase().contains(searchString.toLowerCase())) {
+                return true;
+            }
+            if (entity.getName().toLowerCase().contains(searchString.toLowerCase())
+                    || TreeEntityUtil.getReadableKeywordName(entity.getName()).toLowerCase()
+                            .contains(searchString.toLowerCase())) {
+                return true;
+            }
+            if (entity.hasChildren()) {
+                boolean isChildSelected = false;
+                for (Object child : entity.getChildren()) {
+                    isChildSelected |= select(viewer, element, child);
+                }
+                return isChildSelected;
+            }
+        }
+        return false;
+    }
 }
