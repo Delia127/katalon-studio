dff389ff4683d987a2f6d806f58ee3c71e91850b
Duy Luong <duyluong@kms.com.vn>
2016-04-07 11:13:11 +0700

    KAT-728 Cannot save edited Excel when using Katalon

--- source/com.kms.katalon.core/src/com/kms/katalon/core/testdata/reader/WorbookProvider.java
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/testdata/reader/WorbookProvider.java
@@ -1,21 +1,25 @@
 package com.kms.katalon.core.testdata.reader;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import com.kms.katalon.core.testdata.ExcelData;
 
-
 public class WorbookProvider implements ExcelProvider {
     @Override
     public ExcelData getExcelData(String sourceUrl, boolean hasHeaders) throws IOException {
-        try {
-            Workbook workBook = WorkbookFactory.create(new File(sourceUrl));
-            return new SheetPOI(sourceUrl, hasHeaders, workBook);
+        File excelFile = new File(sourceUrl);
+        if (!excelFile.exists()) {
+            throw new FileNotFoundException(excelFile.toString());
+        }
+        
+        try (FileInputStream fis = new FileInputStream(excelFile)) {
+            return new SheetPOI(sourceUrl, hasHeaders, WorkbookFactory.create(fis));
         } catch (InvalidFormatException e) {
             return null;
         }
