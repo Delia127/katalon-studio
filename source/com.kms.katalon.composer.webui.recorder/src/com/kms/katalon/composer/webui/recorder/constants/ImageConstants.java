4ceea0360d99987b37f832a305a1f1e4d4da8ebc
Duy Luong <duyluong@kms-technology.com>
2016-06-30 11:08:16 +0700

    KAT-962 Fixed layout of buttons on Recorder dialog

--- source/com.kms.katalon.composer.webui.recorder/src/com/kms/katalon/composer/webui/recorder/constants/ImageConstants.java
+++ source/com.kms.katalon.composer.webui.recorder/src/com/kms/katalon/composer/webui/recorder/constants/ImageConstants.java
@@ -9,14 +9,22 @@ import com.kms.katalon.composer.components.util.ImageUtil;
 public class ImageConstants {
     private static final Bundle currentBundle = FrameworkUtil.getBundle(ImageConstants.class);
 
+    private static final Bundle componentBundle = 
+            FrameworkUtil.getBundle(com.kms.katalon.composer.components.impl.constants.ImageConstants.class);
+
     // RecorderDialog
     public static final Image IMG_28_RECORD = ImageUtil.loadImage(currentBundle, "/icons/record_28.png");
-    public static final Image IMG_16_PAUSE = ImageUtil.loadImage(currentBundle, "/icons/pause_16.png");
-    public static final Image IMG_16_STOP = ImageUtil.loadImage(currentBundle, "/icons/stop_16.png");
-    public static final Image IMG_16_PLAY = ImageUtil.loadImage(currentBundle, "/icons/play_16.png");
+
+    public static final Image IMG_28_PAUSE = ImageUtil.loadImage(componentBundle, "/icons/pause_28.png");
+
+    public static final Image IMG_28_STOP = ImageUtil.loadImage(componentBundle, "/icons/stop_28.png");
+
+    public static final Image IMG_28_PLAY = ImageUtil.loadImage(componentBundle, "/icons/play_28.png");
 
     // RecorderDialog
     public static final Image IMG_16_CHROME = ImageUtil.loadImage(currentBundle, "/icons/chrome_16.png");
+
     public static final Image IMG_16_FIREFOX = ImageUtil.loadImage(currentBundle, "/icons/firefox_16.png");
+
     public static final Image IMG_16_IE = ImageUtil.loadImage(currentBundle, "/icons/ie_16.png");
 }
