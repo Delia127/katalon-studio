ca7dab1cde8c6107b1bfacf4b1da76678ad9c15f
Duy Luong <duyluong@kms-technology.com>
2016-07-11 11:35:36 +0700

    KAT-980 Fixed editor tab header hidden issue

--- source/com.kms.katalon.composer.explorer/src/com/kms/katalon/composer/explorer/addon/ExplorerContextInjectionAddon.java
+++ source/com.kms.katalon.composer.explorer/src/com/kms/katalon/composer/explorer/addon/ExplorerContextInjectionAddon.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 
+import com.kms.katalon.composer.explorer.handlers.ReskinTextAreaHandler;
 import com.kms.katalon.composer.explorer.handlers.deletion.DeleteEntityHandlerRegister;
 
 
@@ -12,5 +13,6 @@ public class ExplorerContextInjectionAddon {
     @PostConstruct
     public void initHandlers(IEclipseContext context) {
         ContextInjectionFactory.make(DeleteEntityHandlerRegister.class, context);
+        ContextInjectionFactory.make(ReskinTextAreaHandler.class, context);
     }
 }
