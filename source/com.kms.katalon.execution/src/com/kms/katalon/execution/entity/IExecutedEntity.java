b2ca9759f9d920705c1abb90e5030d0816201386
Hieu Mai <hieumai@kms-technology.com>
2016-07-29 10:11:46 +0700

    KAT-1047: [RunConfiguration] Functions "getExecutionSource", "getExecutionSourceName", "getExecutionSourceId", "getExecutionSourceDescription" return null when executing test case

--- source/com.kms.katalon.execution/src/com/kms/katalon/execution/entity/IExecutedEntity.java
+++ source/com.kms.katalon.execution/src/com/kms/katalon/execution/entity/IExecutedEntity.java
@@ -8,16 +8,28 @@ public interface IExecutedEntity {
     String getId();
 
     /**
-     * @return Name of the executed entity, used to write log
+     * Get name of the executed source, used to write log
+     * @return name of the executed source
      */
     public String getSourceName();
 
     /**
-     * @return
+     * Get id of the executed source
+     * @return id of the executed source
      */
     public String getSourceId();
 
+    /**
+     * Get description of the executed source
+     * @return description of the executed source
+     */
     public String getSourceDescription();
+    
+    /**
+     * Get full path to the executed source
+     * @return full path to the executed source
+     */
+    public String getSourcePath();
 
     public List<IExecutedEntity> getExecutedItems();
 
