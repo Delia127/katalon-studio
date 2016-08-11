9318a6348f3bd83b67b343e0708a1ba2d1759ebc
Hieu Mai <hieumai@kms-technology.com>
2016-08-11 15:47:16 +0700

    KAT-1138: Allow users to import GlobalVariable in custom keyword

--- source/com.kms.katalon.custom/src/com/kms/katalon/custom/parser/GlobalVariableParser.java
+++ source/com.kms.katalon.custom/src/com/kms/katalon/custom/parser/GlobalVariableParser.java
@@ -3,9 +3,9 @@ package com.kms.katalon.custom.parser;
 import groovy.lang.GroovyObject;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 
@@ -13,10 +13,13 @@ import com.kms.katalon.constants.IdConstants;
 import com.kms.katalon.entity.global.GlobalVariableEntity;
 
 public class GlobalVariableParser {
+    public static final String GLOBAL_VARIABLE_CLASS_NAME = "GlobalVariable";
+    public static final String GLOBAL_VARIABLE_FILE_NAME = GLOBAL_VARIABLE_CLASS_NAME + ".groovy";
     private static GlobalVariableParser _instance;
     private final static String TEMPLATE_CLASS_NAME = IdConstants.KATALON_CUSTOM_BUNDLE_ID
             + ".generation.GlobalVariableTemplate";
-    private final static String GENERATED_GLOBAL_VARIABLE_LIB_FILE_METHOD_NAME = "generateGlobalVarialbeFile";
+    private final static String GENERATED_GLOBAL_VARIABLE_LIB_FILE_METHOD_NAME = "generateGlobalVariableFile";
+    public final static String INTERNAL_PACKAGE_NAME = "internal";
 
     private GlobalVariableParser() {
     }
@@ -28,19 +31,26 @@ public class GlobalVariableParser {
         return _instance;
     }
 
-    @SuppressWarnings("rawtypes")
     public void generateGlobalVariableLibFile(IFolder libFolder, List<GlobalVariableEntity> globalVariables)
             throws Exception {
-        File file = new File(libFolder.getRawLocation().toString(), "GlobalVariable.groovy");
-        if (!file.exists()) {
-            file.getParentFile().mkdirs();
-            file.createNewFile();
-        }
+        String libFolderPath = libFolder.getRawLocation().toString();
+        File internalPackageFolder = new File(libFolderPath, INTERNAL_PACKAGE_NAME);
+        File internalGlobalVariableFile = new File(internalPackageFolder, GLOBAL_VARIABLE_FILE_NAME);
+        File globalVariableFile = new File(libFolderPath, GLOBAL_VARIABLE_FILE_NAME);
+        
+        generateGlobalVariableFile(globalVariables, globalVariableFile, true);
+        generateGlobalVariableFile(globalVariables, internalGlobalVariableFile, false);
+        libFolder.refreshLocal(IResource.DEPTH_INFINITE, null);
+    }
 
-        Class clazz = Class.forName(TEMPLATE_CLASS_NAME);
+    private void generateGlobalVariableFile(List<GlobalVariableEntity> globalVariables, File globalVariableFile, boolean isDeprecated) throws ClassNotFoundException,
+            InstantiationException, IllegalAccessException, IOException {
+        if (!globalVariableFile.exists()) {
+            globalVariableFile.getParentFile().mkdirs();
+            globalVariableFile.createNewFile();
+        }
+        Class<?> clazz = Class.forName(TEMPLATE_CLASS_NAME);
         GroovyObject object = (GroovyObject) clazz.newInstance();
-        object.invokeMethod(GENERATED_GLOBAL_VARIABLE_LIB_FILE_METHOD_NAME, new Object[] { file, globalVariables });
-        IFile iFile = libFolder.getFile(file.getName());
-        iFile.refreshLocal(IResource.DEPTH_ZERO, null);
+        object.invokeMethod(GENERATED_GLOBAL_VARIABLE_LIB_FILE_METHOD_NAME, new Object[] { globalVariableFile, globalVariables, isDeprecated });
     }
 }
