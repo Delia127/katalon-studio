75b38d70ed6d0dabb80e33821f22547e8012414d
nhantvo <vothanhnhan.14@gmail.com>
2016-08-29 18:18:27 +0700

    KAT-1216: MacOS display black dialog when open project.

--- source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/handlers/OpenProjectHandler.java
+++ source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/handlers/OpenProjectHandler.java
@@ -104,7 +104,7 @@ public class OpenProjectHandler {
         if (!CloseProjectHandler.closeCurrentProject(partService, modelService, application, eventBrokerService)) {
             return;
         }
-        new ProgressMonitorDialog(shell).run(false, false, new IRunnableWithProgress() {
+        new ProgressMonitorDialog(shell).run(true, false, new IRunnableWithProgress() {
             @Override
             public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
                 try {
