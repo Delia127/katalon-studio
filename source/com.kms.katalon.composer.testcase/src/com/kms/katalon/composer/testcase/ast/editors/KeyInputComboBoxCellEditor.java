d35bfcaea8a732b6fb47125a50daca28a18485cd
antruongnguyen <antruongnguyen@kms-technology.com>
2016-09-05 12:40:25 +0700

    KAT-1164 The Value of "Send Keys" keyword aren't sorted orderly

--- source/com.kms.katalon.composer.testcase/src/com/kms/katalon/composer/testcase/ast/editors/KeyInputComboBoxCellEditor.java
+++ source/com.kms.katalon.composer.testcase/src/com/kms/katalon/composer/testcase/ast/editors/KeyInputComboBoxCellEditor.java
@@ -2,23 +2,35 @@ package com.kms.katalon.composer.testcase.ast.editors;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.swt.widgets.Composite;
 import org.openqa.selenium.Keys;
 
 public class KeyInputComboBoxCellEditor extends PropertyComboBoxCellEditor {
+
+    private static String[] keyNames;
+
     public KeyInputComboBoxCellEditor(Composite parent) {
         super(parent, getKeyEnumStringList());
     }
 
     private static String[] getKeyEnumStringList() {
+        if (keyNames != null) {
+            return keyNames;
+        }
+
         List<String> keysEnumList = new ArrayList<String>();
         for (Field field : Keys.class.getDeclaredFields()) {
             if (field.isEnumConstant()) {
                 keysEnumList.add(field.getName());
             }
         }
-        return keysEnumList.toArray(new String[keysEnumList.size()]);
+
+        keyNames = keysEnumList.toArray(new String[keysEnumList.size()]);
+        Arrays.sort(keyNames);
+        return keyNames;
     }
+
 }
