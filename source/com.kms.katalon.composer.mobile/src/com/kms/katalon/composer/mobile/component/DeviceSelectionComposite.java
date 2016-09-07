70975934820bc4593050e8d3316fb784c0fb9ca4
Hieu Mai <hieumai@kms-technology.com>
2016-09-07 15:46:50 +0700

    KAT-1252: Remove error message when users navigate to 'iOS' settings in Project -> Settings -> Execution -> Default -> Mobile settings on Windows version (#650)

--- source/com.kms.katalon.composer.mobile/src/com/kms/katalon/composer/mobile/component/DeviceSelectionComposite.java
+++ source/com.kms.katalon.composer.mobile/src/com/kms/katalon/composer/mobile/component/DeviceSelectionComposite.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -99,6 +100,9 @@ public class DeviceSelectionComposite extends Composite {
                 }
                 break;
             case IOS_DRIVER:
+                if (Platform.getOS() != Platform.OS_MACOSX) {
+                    break;
+                }
                 try {
                     devicesList.addAll(MobileDeviceProvider.getIosDevices());
                 } catch (IOException | InterruptedException e) {
