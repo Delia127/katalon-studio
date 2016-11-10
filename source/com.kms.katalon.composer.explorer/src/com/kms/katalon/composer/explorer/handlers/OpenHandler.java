940f89260238c85ac7fdeff8e98ef08892b06ad5
antruongnguyen <antruongnguyen@kms-technology.com>
2016-11-10 16:25:04 +0700

    KAT-989 Update context menu of Tests Explorer

--- source/com.kms.katalon.composer.explorer/src/com/kms/katalon/composer/explorer/handlers/OpenHandler.java
+++ source/com.kms.katalon.composer.explorer/src/com/kms/katalon/composer/explorer/handlers/OpenHandler.java
@@ -5,46 +5,43 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.log.Logger;
-import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 
 import com.kms.katalon.composer.components.impl.handler.CommonExplorerHandler;
+import com.kms.katalon.composer.components.impl.tree.FolderTreeEntity;
+import com.kms.katalon.composer.components.impl.tree.PackageTreeEntity;
 import com.kms.katalon.composer.components.tree.ITreeEntity;
 import com.kms.katalon.constants.EventConstants;
-import com.kms.katalon.constants.IdConstants;
 
 @SuppressWarnings("restriction")
 public class OpenHandler extends CommonExplorerHandler {
     @Inject
     private Logger logger;
-    
-    @Inject
-    private ESelectionService selectionService;
 
     @CanExecute
     public boolean canExecute() {
         if (!isExplorerPartActive()) {
             return false;
         }
-        Object[] selectedObjects = (Object[]) selectionService.getSelection(IdConstants.EXPLORER_PART_ID);
-        if (selectedObjects == null || selectedObjects.length == 0) {
+        Object[] selectedObjects = getExplorerSelection();
+        if (selectedObjects.length == 0) {
             return false;
+        }
+        for (Object item : selectedObjects) {
+            if (item instanceof FolderTreeEntity || item instanceof PackageTreeEntity) {
+                return false;
+            }
         }
         return true;
     }
 
     @Execute
     public void execute() {
-        if (selectionService != null && selectionService.getSelection(IdConstants.EXPLORER_PART_ID) != null) {
-            for (Object selectedItem : (Object[]) selectionService.getSelection(IdConstants.EXPLORER_PART_ID)) {
-                if (selectedItem instanceof ITreeEntity) {
-                    try {
-                        eventBroker.post(EventConstants.EXPLORER_OPEN_SELECTED_ITEM,
-                                ((ITreeEntity) selectedItem).getObject());
-                    } catch (Exception e) {
-                        logger.error(e);
-                    }
-                }
+        try {
+            for (ITreeEntity selectedItem : getElementSelection(ITreeEntity.class)) {
+                eventBroker.post(EventConstants.EXPLORER_OPEN_SELECTED_ITEM, selectedItem.getObject());
             }
+        } catch (Exception e) {
+            logger.error(e);
         }
     }
 }
