514ec835a4d2121caf97be56b2ad07a89e481ecf
antruongnguyen <antruongnguyen@kms-technology.com>
2016-11-21 14:10:36 +0700

    KAT-1489 Add icons for Undo/Redo menu item

--- source/com.kms.katalon.composer.resources/src/com/kms/katalon/composer/resources/constants/IImageKeys.java
+++ source/com.kms.katalon.composer.resources/src/com/kms/katalon/composer/resources/constants/IImageKeys.java
@@ -490,6 +490,10 @@ public interface IImageKeys {
     public static final String PROPERTIES_16 = "properties_16.png"; //$NON-NLS-1$
 
     public static final String FOCUS_16 = "focus_16.png"; //$NON-NLS-1$
+
+    public static final String UNDO_16 = "undo_16.png"; //$NON-NLS-1$
+
+    public static final String REDO_16 = "redo_16.png"; //$NON-NLS-1$
     // End Common
 
 }
