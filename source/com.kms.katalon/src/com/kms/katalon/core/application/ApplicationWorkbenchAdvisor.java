6d23ea5d28d056d769bd986fd9a5e8395e27d62c
Hieu Mai <hieumai@kms-technology.com>
2016-12-26 17:40:03 +0700

    KAT-1776: [Editor] Exception occurs when pasting a specific script to test case

--- source/com.kms.katalon/src/com/kms/katalon/core/application/ApplicationWorkbenchAdvisor.java
+++ source/com.kms.katalon/src/com/kms/katalon/core/application/ApplicationWorkbenchAdvisor.java
@@ -1,14 +1,26 @@
 package com.kms.katalon.core.application;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.statushandlers.AbstractStatusHandler;
+import org.eclipse.ui.statushandlers.StatusAdapter;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.eclipse.ui.statushandlers.WorkbenchErrorHandler;
 
 import com.kms.katalon.constants.IdConstants;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+    private static final String STRING_INDEX_OUT_OF_RANGE_3 = "String index out of range: -3";
+
+    /**
+     * The workbench error handler.
+     */
+    private AbstractStatusHandler workbenchErrorHandler;
+
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
     }
@@ -22,5 +34,36 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
         super.initialize(configurer);
         IDE.registerAdapters();
         configurer.setSaveAndRestore(true);
+    }
+
+    @Override
+    public synchronized AbstractStatusHandler getWorkbenchErrorHandler() {
+        if (workbenchErrorHandler != null) {
+            return workbenchErrorHandler;
+        }
+        workbenchErrorHandler = new WorkbenchErrorHandler() {
+            @Override
+            public void handle(StatusAdapter statusAdapter, int style) {
+                if (isGroovyEditorError(statusAdapter)) {
+                    handleGroovyEditorError(statusAdapter);
+                    return;
+                }
+                super.handle(statusAdapter, style);
+            }
+
+            private void handleGroovyEditorError(StatusAdapter statusAdapter) {
+                // Only log down errors, not showing in dialog
+                super.handle(statusAdapter, StatusManager.LOG);
+            }
+
+            private boolean isGroovyEditorError(StatusAdapter statusAdapter) {
+                IStatus status = statusAdapter.getStatus();
+                if (!(status.getException() instanceof StringIndexOutOfBoundsException)) {
+                    return false;
+                }
+                return status.getException().getMessage().equals(STRING_INDEX_OUT_OF_RANGE_3);
+            }
+        };
+        return workbenchErrorHandler;
     }
 }
