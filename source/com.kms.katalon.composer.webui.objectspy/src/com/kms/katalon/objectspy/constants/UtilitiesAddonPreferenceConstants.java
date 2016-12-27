478075d47d4a4d6d42fc42116c83eb6dba4b6232
Hieu Mai <hieumai@kms-technology.com>
2016-12-27 19:50:11 +0700

    KAT-1788: [macOS] Default port of Katalon Utility is set to 0

--- source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/constants/UtilitiesAddonPreferenceConstants.java
+++ source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/constants/UtilitiesAddonPreferenceConstants.java
@@ -2,7 +2,7 @@ package com.kms.katalon.objectspy.constants;
 
 public interface UtilitiesAddonPreferenceConstants {
     // Composer WebUi ObjectSpy
-    public static final String UTILITIES_ADDON_QUALIFIER = "com.kms.katalon.composer.webui.utilitiesAddon";
+    public static final String UTILITIES_ADDON_QUALIFIER = ObjectSpyPreferenceConstants.WEBUI_OBJECTSPY_QUALIFIER;
 
     public static final String UTILITIES_ADDON_ACTIVE_BROWSER_PORT = "utilitiesAddon.instantBrowserPort";
 
