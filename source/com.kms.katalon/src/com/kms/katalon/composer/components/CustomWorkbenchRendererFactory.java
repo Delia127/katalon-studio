aabb7efb77994d4cc4b46c17bd115027f769c910
Hieu Mai <hieumai@kms-technology.com>
2017-02-23 17:25:00 +0700

    KAT-1952: [Test Suite] Wrong Quick link position after closing test artifact with multiple tabs

--- source/com.kms.katalon/src/com/kms/katalon/composer/components/CustomWorkbenchRendererFactory.java
+++ source/com.kms.katalon/src/com/kms/katalon/composer/components/CustomWorkbenchRendererFactory.java
@@ -1,9 +1,15 @@
 package com.kms.katalon.composer.components;
 
+import javax.inject.Inject;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.advanced.MArea;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
+import org.eclipse.e4.ui.workbench.IPresentationEngine;
 import org.eclipse.e4.ui.workbench.renderers.swt.WorkbenchRendererFactory;
 
 import com.kms.katalon.constants.IdConstants;
@@ -15,6 +21,15 @@ public class CustomWorkbenchRendererFactory extends WorkbenchRendererFactory {
 
     private CToolBarManagerRenderer toolbarRenderer;
 
+    private CStackRenderer stackRenderer;
+
+    @Inject
+    @Preference(nodePath = "org.eclipse.e4.ui.workbench.renderers.swt")
+    private IEclipsePreferences preferences;
+    
+    @Inject
+    private IPresentationEngine presentationEngine;
+    
     @Override
     public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
         if (uiElement instanceof MArea && uiElement.getElementId().equals(IdConstants.SHARE_AREA_ID)) {
@@ -32,6 +47,15 @@ public class CustomWorkbenchRendererFactory extends WorkbenchRendererFactory {
                 initRenderer(toolbarRenderer);
             }
             return toolbarRenderer;
+        }
+
+        // Handle for main MPartStack
+        if (uiElement instanceof MPartStack) {
+            if (stackRenderer == null) {
+                stackRenderer = new CStackRenderer(presentationEngine, preferences);
+                initRenderer(stackRenderer);
+            }
+            return stackRenderer;
         }
 
         return super.getRenderer(uiElement, parent);
