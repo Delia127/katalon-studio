04d4774d3208ea123538e02d7b6b2863802c3265
Hieu Mai <hieumai@kms-technology.com>
2017-02-28 15:23:32 +0700

    KAT-1963: [Manual View] Double dialog is displayed when opening any dialog

--- source/com.kms.katalon.composer.components.impl/src/com/kms/katalon/composer/components/impl/control/CTreeViewer.java
+++ source/com.kms.katalon.composer.components.impl/src/com/kms/katalon/composer/components/impl/control/CTreeViewer.java
@@ -1,16 +1,27 @@
 package com.kms.katalon.composer.components.impl.control;
 
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.viewers.ColumnViewerEditorActivationEvent;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.viewers.ViewerRow;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.Widget;
 
 import com.kms.katalon.composer.components.impl.providers.TypeCheckedStyleCellLabelProvider;
 
 public class CTreeViewer extends TreeViewer implements CustomColumnViewer {
+    private static final int SECOND_CLICK = 2;
+
+    private MouseListener mouseListener;
 
     public CTreeViewer(Composite parent, int style) {
         super(parent, style);
@@ -39,5 +50,50 @@ public class CTreeViewer extends TreeViewer implements CustomColumnViewer {
     public void enableTooltipSupport() {
         getTree().setToolTipText(StringUtils.EMPTY);
         ColumnViewerToolTipSupport.enableFor(this);
+    }
+
+    @Override
+    protected void hookEditingSupport(Control control) {
+        if (getColumnViewerEditor() == null) {
+            return;
+        }
+
+        mouseListener = new MouseAdapter() {
+            @Override
+            public void mouseDown(MouseEvent e) {
+                // Workaround for bug 185817
+                if (e.count != SECOND_CLICK) {
+                    handleMouseDown(e);
+                }
+            }
+
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+                // Handle for the bug the mouse event for double click which have e.count = 1 and therefore creating a
+                // ColumnViewerEditorActivationEvent object with type == MOUSE_CLICK_SELECTION
+                // https://incubation.kms-technology.com/browse/KAT-1963
+                if (e.count == SECOND_CLICK) {
+                    handleMouseDown(e);
+                }
+            }
+        };
+        control.addMouseListener(mouseListener);
+    }
+
+    @Override
+    protected void handleDispose(DisposeEvent event) {
+        if (mouseListener != null && event.widget instanceof Control) {
+            ((Control) event.widget).removeMouseListener(mouseListener);
+            mouseListener = null;
+        }
+        super.handleDispose(event);
+    }
+
+    private void handleMouseDown(MouseEvent e) {
+        ViewerCell cell = getCell(new Point(e.x, e.y));
+
+        if (cell != null) {
+            triggerEditorActivationEvent(new ColumnViewerEditorActivationEvent(cell, e));
+        }
     }
 }
