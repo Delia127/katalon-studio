d5f419af9098db183d4789ddbd875ab3b5f55f29
Hieu Mai <hieumai@kms-technology.com>
2017-03-17 20:38:22 +0700

    KAT-1978: [Keyword] Can't resolve TimeUnit class when it's used in custom keyword

--- source/com.kms.katalon.custom/src/com/kms/katalon/custom/generation/CustomKeywordTemplate.groovy
+++ source/com.kms.katalon.custom/src/com/kms/katalon/custom/generation/CustomKeywordTemplate.groovy
@@ -85,7 +85,7 @@ def static "<%= key %>.<%= it.getName() %>"(<% it.getParameters().eachWithIndex 
     }
 
     private static boolean canBeImported(String className) {
-        return StringUtils.isNotEmpty(className) && className.contains(DOT) && !isGroovyImportedClassName(className) 
+        return StringUtils.isNotEmpty(className) && className.contains(DOT);
     }
 
     public static String resolveClassName(ClassNode classNode, boolean resolveGeneric) {
@@ -115,7 +115,7 @@ def static "<%= key %>.<%= it.getName() %>"(<% it.getParameters().eachWithIndex 
                 classNameBuilder.append(">")
                 return classNameBuilder.toString()
             }
-            return isGroovyImportedClassName(fullClassName) ? ClassUtils.getShortCanonicalName(fullClassName) : fullClassName
+            return fullClassName;
         } catch (Exception ignored) {
             return classNode.getUnresolvedName()
         }
