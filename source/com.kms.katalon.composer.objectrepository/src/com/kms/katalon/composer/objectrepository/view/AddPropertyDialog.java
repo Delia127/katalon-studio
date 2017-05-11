2b5ac5dacda7eba3c3c40fc9f39d8508c351e5e6
antruongnguyen <antruongnguyen@kms-technology.com>
2017-05-11 12:53:10 +0700

    KAT-2029 [Test Object] Property name is overlapped while adding new property on macOS

--- source/com.kms.katalon.composer.objectrepository/src/com/kms/katalon/composer/objectrepository/view/AddPropertyDialog.java
+++ source/com.kms.katalon.composer.objectrepository/src/com/kms/katalon/composer/objectrepository/view/AddPropertyDialog.java
@@ -15,6 +15,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import com.kms.katalon.composer.components.adapter.CComboContentAdapter;
+import com.kms.katalon.composer.components.impl.util.ControlUtils;
+import com.kms.katalon.composer.components.impl.util.PlatformUtil;
 import com.kms.katalon.composer.objectrepository.constant.StringConstants;
 import com.kms.katalon.entity.repository.WebElementPropertyEntity;
 
@@ -43,17 +45,17 @@ public class AddPropertyDialog extends Dialog {
         Composite area = (Composite) super.createDialogArea(parent);
 
         Composite container = new Composite(area, SWT.NONE);
-        container.setLayoutData(new GridData(GridData.FILL_BOTH));
+        container.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
         GridLayout gl_container = new GridLayout(2, false);
-        gl_container.horizontalSpacing = 15;
-        gl_container.verticalSpacing = 7;
+        gl_container.marginWidth = 0;
+        gl_container.marginHeight = 0;
         container.setLayout(gl_container);
 
         Label lblName = new Label(container, SWT.NONE);
         lblName.setText(StringConstants.VIEW_LBL_NAME);
 
         ccbName = new CCombo(container, SWT.BORDER | SWT.FLAT);
-        ccbName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        ccbName.setLayoutData(platformGridData(new GridData(SWT.FILL, SWT.CENTER, true, false)));
         ccbName.setItems(commonNames);
         new AutoCompleteField(ccbName, new CComboContentAdapter(), commonNames);
 
@@ -61,7 +63,7 @@ public class AddPropertyDialog extends Dialog {
         lblCondition.setText(StringConstants.VIEW_LBL_MATCH_COND);
 
         ccbConditions = new CCombo(container, SWT.BORDER | SWT.READ_ONLY);
-        ccbConditions.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        ccbConditions.setLayoutData(platformGridData(new GridData(SWT.FILL, SWT.CENTER, true, false)));
         ccbConditions.setItems(WebElementPropertyEntity.MATCH_CONDITION.getTextVlues());
         ccbConditions.select(0);
 
@@ -70,12 +72,19 @@ public class AddPropertyDialog extends Dialog {
         lblValue.setText(StringConstants.VIEW_LBL_VALUE);
 
         txtValue = new Text(container, SWT.BORDER);
-        txtValue.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        txtValue.setLayoutData(platformGridData(new GridData(SWT.FILL, SWT.CENTER, true, false)));
 
         Label separator = new Label(parent, SWT.HORIZONTAL | SWT.SEPARATOR);
         separator.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
         return area;
+    }
+
+    private GridData platformGridData(GridData gridData) {
+        if (PlatformUtil.isMacOS()) {
+            gridData.heightHint = ControlUtils.DF_CONTROL_HEIGHT;
+        }
+        return gridData;
     }
 
     @Override
@@ -86,7 +95,7 @@ public class AddPropertyDialog extends Dialog {
 
     @Override
     protected Point getInitialSize() {
-        return new Point(450, 200);
+        return new Point(450, 180);
     }
 
     @Override
@@ -94,7 +103,7 @@ public class AddPropertyDialog extends Dialog {
         name = ccbName.getText();
         value = txtValue.getText();
         condition = ccbConditions.getItem(ccbConditions.getSelectionIndex());
-        if (name.trim().equals("")) {
+        if (name.trim().isEmpty()) {
             MessageDialog.openWarning(getParentShell(), StringConstants.WARN_TITLE,
                     StringConstants.VIEW_WARN_MSG_PROPERTY_CANNOT_BE_BLANK);
         } else {
