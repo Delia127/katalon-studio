b220af8df5029702a60e1a87364d0a6ad768511b
antruongnguyen <antruongnguyen@kms-technology.com>
2017-07-11 19:02:11 +0700

    KAT-2186 [WebUI Keyword] Ability to check for broken Links/Images
    KAT-2186 Add 5 new keywords GetAllLinksOnCurrentPageKeyword, VerifyResponseStatusCodeKeyword, VerifyResponseStatusCodeInRangeKeyword, VerifyLinksAccessibleKeyword, VerifyAllLinksOnCurrentPageAccessibleKeyword
    KAT-2320 Wrong parameter name when using keyword with Failure Handling
    KAT-2322 Failure Handling is missing in Javadoc of 5 new keywords while using Failure Handling
    KAT-2326 Typo mistakes in Javadoc
    KAT-2475 [Verify all links on current page accessible] Error occured after execution

--- source/com.kms.katalon.core/src/com/kms/katalon/core/keyword/internal/AbstractKeyword.groovy
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/keyword/internal/AbstractKeyword.groovy
@@ -2,7 +2,9 @@ package com.kms.katalon.core.keyword.internal;
 
 import org.apache.commons.lang.ObjectUtils
 
+import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testobject.ObjectRepository
 import com.kms.katalon.core.testobject.TestObject
 
@@ -15,5 +17,26 @@ public abstract class AbstractKeyword implements IKeyword {
             return (TestObject) param
         }
         return ObjectRepository.findTestObject(ObjectUtils.toString(param))
+    }
+
+    protected FailureHandling getFailureHandling(Object[] params, int index) {
+        Object param = getParam(params, index)
+        if (param instanceof FailureHandling) {
+            return (FailureHandling) param
+        }
+        return RunConfiguration.getDefaultFailureHandling()
+    }
+
+    protected boolean getBooleanValue(Object[] params, int index) {
+        Object param = getParam(params, index)
+        return param instanceof Boolean && (boolean) param
+    }
+
+    protected Object getParam(Object[] params, int index) {
+        try {
+            return params[index]
+        } catch(Exception e) {
+            return null
+        }
     }
 }
