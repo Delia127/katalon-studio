8356060561f0f416318c041d21e28bc72acfb847
Duy Luong <duyluong@kms-technology.com>
2017-08-22 15:05:03 +0700

    KAT-2382 Remove .external folder

--- source/com.kms.katalon/src/com/kms/katalon/core/application/MetadataCorruptedResolver.java
+++ source/com.kms.katalon/src/com/kms/katalon/core/application/MetadataCorruptedResolver.java
@@ -1,7 +1,6 @@
 package com.kms.katalon.core.application;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
@@ -11,8 +10,6 @@ import org.eclipse.core.runtime.Platform;
 import com.kms.katalon.composer.components.log.LoggerSingleton;
 
 public class MetadataCorruptedResolver {
-
-    private static final String SNAP = ".snap";
 
     public boolean isMetaFolderCorrupted() {
         try {
@@ -27,32 +24,12 @@ public class MetadataCorruptedResolver {
         try {
             File resourcesFolder = new File(
                     Platform.getInstanceLocation().getDataArea(ResourcesPlugin.PI_RESOURCES).getFile());
-            if (!resourcesFolder.exists()) {
-                return false;
-            }
-            cleanSnapFiles(resourcesFolder);
+            FileUtils.deleteDirectory(resourcesFolder);
             return true;
         } catch (IOException e) {
             LoggerSingleton.logError(e);
             return false;
         }
 
-    }
-
-    private void cleanSnapFiles(File resourcesFolder) {
-        File[] snapFiles = resourcesFolder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(SNAP);
-            }
-        });
-
-        if (snapFiles == null) {
-            return;
-        }
-
-        for (File snap : snapFiles) {
-            FileUtils.deleteQuietly(snap);
-        }
     }
 }
