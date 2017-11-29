d5e4ee5176fe83083d2bb75636fd5560b715ad1f
Duy Luong <duyluong@kms-technology.com>
2017-11-29 14:59:46 +0700

    KAT-2709 [Katalon Studio] Proxy connection for KA

--- source/com.kms.katalon.integration.analytics/src/com/kms/katalon/integration/analytics/providers/AnalyticsApiProvider.java
+++ source/com.kms.katalon.integration.analytics/src/com/kms/katalon/integration/analytics/providers/AnalyticsApiProvider.java
@@ -1,5 +1,7 @@
 package com.kms.katalon.integration.analytics.providers;
 
+import static com.kms.katalon.integration.analytics.providers.HttpClientProxyBuilder.create;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -25,11 +27,12 @@ import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.kms.katalon.execution.preferences.ProxyPreferences;
 import com.kms.katalon.integration.analytics.constants.AnalyticsStringConstants;
 import com.kms.katalon.integration.analytics.constants.IntegrationAnalyticsMessages;
 import com.kms.katalon.integration.analytics.entity.AnalyticsProject;
@@ -63,8 +66,10 @@ public class AnalyticsApiProvider {
     public static AnalyticsTokenInfo requestToken(String serverUrl, String email, String password)
             throws AnalyticsApiExeception {
         try {
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            CloseableHttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
+
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_TOKEN);
-            HttpClient httpClient = HttpClientBuilder.create().build();
             URIBuilder uriBuilder = new URIBuilder(uri);
             uriBuilder.setParameter(LOGIN_PARAM_USERNAME, email);
             uriBuilder.setParameter(LOGIN_PARAM_PASSWORD, password);
@@ -74,7 +79,7 @@ public class AnalyticsApiProvider {
             String clientCredentials = OAUTH2_CLIENT_ID + ":" + OAUTH2_CLIENT_SECRET;
             httpPost.setHeader(HEADER_AUTHORIZATION,
                     HEADER_AUTHORIZATION_PREFIX + Base64.getEncoder().encodeToString(clientCredentials.getBytes()));
-            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpResponse httpResponse = httpClient.execute(httpPost, httpClientProxyBuilder.getClientContext());
             String responseString = EntityUtils.toString(httpResponse.getEntity());
             Gson gson = new GsonBuilder().create();
             AnalyticsTokenInfo tokenInfo = gson.fromJson(responseString, AnalyticsTokenInfo.class);
@@ -86,17 +91,17 @@ public class AnalyticsApiProvider {
             throw new AnalyticsApiExeception(e);
         }
     }
-    
-    public static List<AnalyticsTeam> getTeams(String serverUrl, String accessToken)
-            throws AnalyticsApiExeception {
+
+    public static List<AnalyticsTeam> getTeams(String serverUrl, String accessToken) throws AnalyticsApiExeception {
         try {
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_TEAMS);
-            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             URIBuilder uriBuilder = new URIBuilder(uri);
             uriBuilder.setParameter("sort", "name,asc");
             HttpGet httpGet = new HttpGet(uriBuilder.build().toASCIIString());
             httpGet.setHeader(HEADER_AUTHORIZATION, HEADER_VALUE_AUTHORIZATION_PREFIX + accessToken);
-            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpResponse httpResponse = httpClient.execute(httpGet, httpClientProxyBuilder.getClientContext());
             String responseString = EntityUtils.toString(httpResponse.getEntity());
             Gson gson = new GsonBuilder().create();
             AnalyticsTeamPage teamPage = gson.fromJson(responseString, AnalyticsTeamPage.class);
@@ -110,7 +115,8 @@ public class AnalyticsApiProvider {
             throws AnalyticsApiExeception {
         try {
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_PROJECTS);
-            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             URIBuilder uriBuilder = new URIBuilder(uri);
             if (team != null && team.getId() != 0) {
                 uriBuilder.setParameter("teamId", team.getId() + "");
@@ -118,7 +124,7 @@ public class AnalyticsApiProvider {
             }
             HttpGet httpGet = new HttpGet(uriBuilder.build().toASCIIString());
             httpGet.setHeader(HEADER_AUTHORIZATION, HEADER_VALUE_AUTHORIZATION_PREFIX + accessToken);
-            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpResponse httpResponse = httpClient.execute(httpGet, httpClientProxyBuilder.getClientContext());
             String responseString = EntityUtils.toString(httpResponse.getEntity());
             Gson gson = new GsonBuilder().create();
             AnalyticsProjectPage projectPage = gson.fromJson(responseString, AnalyticsProjectPage.class);
@@ -127,49 +133,46 @@ public class AnalyticsApiProvider {
             throw new AnalyticsApiExeception(e);
         }
     }
-    
-    public static AnalyticsProject createProject(String serverUrl, String projectName, AnalyticsTeam team, String accessToken) 
-            throws AnalyticsApiExeception{
+
+    public static AnalyticsProject createProject(String serverUrl, String projectName, AnalyticsTeam team,
+            String accessToken) throws AnalyticsApiExeception {
         try {
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_PROJECTS);
-            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             HttpPost httpPost = new HttpPost(uri);
             httpPost.setHeader("Accept", "application/json");
             httpPost.setHeader("Content-type", "application/json");
             httpPost.setHeader(HEADER_AUTHORIZATION, HEADER_VALUE_AUTHORIZATION_PREFIX + accessToken);
-            
+
             Map<String, String> map = new HashMap<>();
             map.put("name", projectName);
-            
+
             if (team != null && team.getId() != null) {
                 map.put("teamId", team.getId() + "");
             }
-            
-            Gson gson = new Gson(); 
+
+            Gson gson = new Gson();
             StringEntity entity = new StringEntity(gson.toJson(map));
             httpPost.setEntity(entity);
-            
-            HttpResponse httpResponse = httpClient.execute(httpPost);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost, httpClientProxyBuilder.getClientContext());
             String responseString = EntityUtils.toString(httpResponse.getEntity());
             return gson.fromJson(responseString, AnalyticsProject.class);
         } catch (Exception e) {
             throw new AnalyticsApiExeception(e);
         }
     }
-    
-    public static void sendLog(String serverUrl, 
-                                long projectId, 
-                                long timestamp, 
-                                String folderName, 
-                                File file, 
-                                boolean isEnd,
-                                String token) throws AnalyticsApiExeception {
+
+    public static void sendLog(String serverUrl, long projectId, long timestamp, String folderName, File file,
+            boolean isEnd, String token) throws AnalyticsApiExeception {
         try {
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_KATALON_TEST_REPORTS);
-            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             HttpPost httpPost = new HttpPost(uri);
             httpPost.setHeader(HEADER_AUTHORIZATION, HEADER_VALUE_AUTHORIZATION_PREFIX + token);
-            
+
             StringBody projectIdPart = new StringBody(projectId + "", ContentType.MULTIPART_FORM_DATA);
             StringBody batchPart = new StringBody(timestamp + "", ContentType.MULTIPART_FORM_DATA);
             StringBody isEndPart = new StringBody(isEnd + "", ContentType.MULTIPART_FORM_DATA);
@@ -183,25 +186,27 @@ public class AnalyticsApiProvider {
             builder.addPart("folderPath", folderPathPart);
             builder.addPart("isEnd", isEndPart);
             builder.addPart("file", fileBodyPart);
-            
+
             HttpEntity entity = builder.build();
             httpPost.setEntity(entity);
-            httpClient.execute(httpPost);
-            
+            httpClient.execute(httpPost, httpClientProxyBuilder.getClientContext());
+
         } catch (Exception e) {
             throw new AnalyticsApiExeception(e);
         }
     }
-    
-    public static AnalyticsUploadInfo getUploadInfo(String serverUrl, String token, long projectId) throws AnalyticsApiExeception {
+
+    public static AnalyticsUploadInfo getUploadInfo(String serverUrl, String token, long projectId)
+            throws AnalyticsApiExeception {
         try {
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_UPLOAD_URL);
-            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             URIBuilder uriBuilder = new URIBuilder(uri);
             uriBuilder.setParameter("projectId", String.valueOf(projectId));
             HttpGet httpGet = new HttpGet(uriBuilder.build());
             httpGet.setHeader(HEADER_AUTHORIZATION, HEADER_VALUE_AUTHORIZATION_PREFIX + token);
-            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpResponse httpResponse = httpClient.execute(httpGet, httpClientProxyBuilder.getClientContext());
             String responseString = EntityUtils.toString(httpResponse.getEntity());
             Gson gson = new GsonBuilder().create();
             return gson.fromJson(responseString, AnalyticsUploadInfo.class);
@@ -209,28 +214,22 @@ public class AnalyticsApiProvider {
             throw new AnalyticsApiExeception(e);
         }
     }
-    
+
     public static void uploadFile(String url, File file) throws AnalyticsApiExeception {
         try (InputStream content = new FileInputStream(file)) {
-            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             HttpEntity entity = new InputStreamEntity(content, file.length());
             HttpPut httpPut = new HttpPut(url);
             httpPut.setEntity(entity);
-            httpClient.execute(httpPut);
+            httpClient.execute(httpPut, httpClientProxyBuilder.getClientContext());
         } catch (Exception e) {
             throw new AnalyticsApiExeception(e);
         }
     }
-    
-    public static void uploadFileInfo(
-            String serverUrl,
-            long projectId,
-            long timestamp,
-            String folderName,
-            String fileName,
-            String uploadedPath,
-            boolean isEnd,
-            String token) throws AnalyticsApiExeception {
+
+    public static void uploadFileInfo(String serverUrl, long projectId, long timestamp, String folderName,
+            String fileName, String uploadedPath, boolean isEnd, String token) throws AnalyticsApiExeception {
 
         try {
             URI uri = getApiURI(serverUrl, AnalyticsStringConstants.ANALYTICS_API_KATALON_TEST_REPORTS);
@@ -241,12 +240,13 @@ public class AnalyticsApiProvider {
             uriBuilder.setParameter("isEnd", String.valueOf(isEnd));
             uriBuilder.setParameter("fileName", fileName);
             uriBuilder.setParameter("uploadedPath", uploadedPath);
-            
-            HttpClient httpClient = HttpClientBuilder.create().build();
+
+            HttpClientProxyBuilder httpClientProxyBuilder = create(ProxyPreferences.getProxyInformation());
+            HttpClient httpClient = httpClientProxyBuilder.getClientBuilder().build();
             HttpPost httpPost = new HttpPost(uriBuilder.build());
             httpPost.setHeader(HEADER_AUTHORIZATION, HEADER_VALUE_AUTHORIZATION_PREFIX + token);
 
-            httpClient.execute(httpPost);
+            httpClient.execute(httpPost, httpClientProxyBuilder.getClientContext());
         } catch (Exception e) {
             throw new AnalyticsApiExeception(e);
         }
@@ -255,6 +255,5 @@ public class AnalyticsApiProvider {
     private static URI getApiURI(String host, String path) throws URISyntaxException {
         return new URIBuilder().setPath(host + path).build();
     }
-
 
 }
