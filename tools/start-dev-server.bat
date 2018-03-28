d92c2044aaf8d12be5fd087066b8176f3967d99a
Hai Minh Nguyen <haimnguyen@qasymphony.com>
2018-03-28 11:30:34 +0700

    add start dev servers batch

---
+++ tools/start-dev-server.bat
@@ -0,0 +1,3 @@
+SET PROJ_HOME=%~dp0..\source
+start cmd /k "cd %PROJ_HOME%\com.kms.katalon.repo && mvn -Djetty.port=9999 jetty:run"
+start cmd /k "cd %PROJ_HOME%\com.kms.katalon.repo && mvn -Djetty.port=33333 jetty:run"
