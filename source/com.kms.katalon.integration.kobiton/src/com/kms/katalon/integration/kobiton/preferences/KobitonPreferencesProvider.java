d9a8fac78a1655d42fa98f38036a2dc7ec04ee07
KMS\thaiquoctran <thaiquoctran@kms-technology.com>
2018-04-09 16:18:42 +0700

    KAT-3142: Support passing username, password Kobiton for console mode.

--- source/com.kms.katalon.integration.kobiton/src/com/kms/katalon/integration/kobiton/preferences/KobitonPreferencesProvider.java
+++ source/com.kms.katalon.integration.kobiton/src/com/kms/katalon/integration/kobiton/preferences/KobitonPreferencesProvider.java
@@ -1,8 +1,13 @@
 package com.kms.katalon.integration.kobiton.preferences;
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 import com.kms.katalon.integration.kobiton.constants.KobitonPreferenceConstants;
+import com.kms.katalon.integration.kobiton.entity.KobitonApiKey;
+import com.kms.katalon.integration.kobiton.entity.KobitonLoginInfo;
+import com.kms.katalon.integration.kobiton.providers.KobitonApiProvider;
 import com.kms.katalon.preferences.internal.PreferenceStoreManager;
 import com.kms.katalon.preferences.internal.ScopedPreferenceStore;
 
@@ -38,7 +43,27 @@ public class KobitonPreferencesProvider {
     }
     
     public static String getKobitonToken() {
-        return getPreferencetStore().getString(KobitonPreferenceConstants.KOBITON_AUTHENTICATION_TOKEN);
+        String kobitonToken = getPreferencetStore().getString(KobitonPreferenceConstants.KOBITON_AUTHENTICATION_TOKEN);
+
+        // Console mode: we just passed username and password. So, we need to generate token automatically.
+        if (StringUtils.isEmpty(kobitonToken)) {
+            KobitonLoginInfo loginInfo;
+            try {
+                loginInfo = KobitonApiProvider.login(getKobitonUserName(), getKobitonPassword());
+                kobitonToken = loginInfo.getToken();
+                KobitonPreferencesProvider.saveKobitonToken(loginInfo.getToken());
+                List<KobitonApiKey> apiKeys = KobitonApiProvider.getApiKeyList(loginInfo.getToken());
+                if (!apiKeys.isEmpty()) {
+                    KobitonPreferencesProvider.saveKobitonApiKey(apiKeys.get(0).getKey());
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(
+                        "Authentication kobiton system failed:  Please check username and password again.", e);
+            }
+        }
+
+        return kobitonToken;
     }
     
     public static void saveKobitonToken(String token) {
