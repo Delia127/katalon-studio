6e3a666642af15bf34a62b32225332f7a0b06165
HuyNguyen <huynguyen@kms-technology.com>
2018-04-11 10:13:55 +0700

    Update used test suite's profiles in Test Suite Collection to default when users delete it

--- source/com.kms.katalon.composer.global/src/com/kms/katalon/composer/global/handler/DeleteExecutionProfileHandler.java
+++ source/com.kms.katalon.composer.global/src/com/kms/katalon/composer/global/handler/DeleteExecutionProfileHandler.java
@@ -1,5 +1,7 @@
 package com.kms.katalon.composer.global.handler;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -10,7 +12,11 @@ import com.kms.katalon.composer.components.tree.ITreeEntity;
 import com.kms.katalon.composer.explorer.handlers.deletion.IDeleteEntityHandler;
 import com.kms.katalon.constants.EventConstants;
 import com.kms.katalon.controller.GlobalVariableController;
+import com.kms.katalon.controller.ProjectController;
+import com.kms.katalon.controller.TestSuiteCollectionController;
 import com.kms.katalon.entity.global.ExecutionProfileEntity;
+import com.kms.katalon.entity.project.ProjectEntity;
+import com.kms.katalon.entity.testsuite.TestSuiteCollectionEntity;
 
 public class DeleteExecutionProfileHandler implements IDeleteEntityHandler {
 
@@ -28,11 +34,21 @@ public class DeleteExecutionProfileHandler implements IDeleteEntityHandler {
             return false;
         }
 
+        ProjectEntity project = ProjectController.getInstance().getCurrentProject();
         ExecutionProfileEntity executionProfile = null;
         try {
             executionProfile = (ExecutionProfileEntity) treeEntity.getObject();
             GlobalVariableController.getInstance().deleteExecutionProfile(executionProfile);
+            List<TestSuiteCollectionEntity> updatedTestSuiteCollections = TestSuiteCollectionController.getInstance().
+                updateProfileNameInAllTestSuiteCollections(
+                        project,
+                        executionProfile.getName(),
+                        ExecutionProfileEntity.DF_PROFILE_NAME);
+            
             eventBroker.post(EventConstants.EXECUTION_PROFILE_DELETED, executionProfile);
+            
+            updatedTestSuiteCollections.forEach(tsc-> eventBroker.post(EventConstants.TEST_SUITE_COLLECTION_UPDATED,
+                    new Object[]{ tsc.getId(), tsc}));
         } catch (Exception ignored) {}
 
         return true;
