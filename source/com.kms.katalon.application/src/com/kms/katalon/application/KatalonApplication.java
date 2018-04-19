a04b13357bddc2b7d3bf1960dd993d585587bf8b
Duy Luong <duyluong@kms-technology.com>
2018-04-19 16:06:07 +0700

    [KS - Tracking] Replace macaddress tracking property by the combination of hostname & macaddress

--- source/com.kms.katalon.application/src/com/kms/katalon/application/KatalonApplication.java
+++ source/com.kms.katalon.application/src/com/kms/katalon/application/KatalonApplication.java
@@ -1,11 +1,17 @@
 package com.kms.katalon.application;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.kms.katalon.core.util.internal.JsonUtil;
+import com.kms.katalon.logging.LogUtil;
+import com.kms.katalon.util.FileHashUtil;
 import com.kms.katalon.util.NetworkUtil;
 
 public class KatalonApplication {
@@ -19,7 +25,30 @@ public class KatalonApplication {
     static {
         SESSION_ID = UUID.randomUUID().toString();
 
-        MAC_ADDRESS = getMacAddress();
+        MAC_ADDRESS = hashMacAndHostName();
+    }
+    
+    private static String hashMacAndHostName() {
+        String hostName = getDefaultLocalName();
+        String macAddress = getMacAddress();
+        
+        LocalMacAdress localMacAddress = new LocalMacAdress(hostName, macAddress);
+        try {
+            return FileHashUtil.hash(JsonUtil.toJson(localMacAddress), "MD5");
+        } catch (NoSuchAlgorithmException | IOException e) {
+            LogUtil.logError(e);
+            return macAddress;
+        }
+    }
+
+    private static String getDefaultLocalName() {
+        String hostName;
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            hostName = UNKNOWN_HOST;
+        }
+        return hostName;
     }
 
     private static String getMacAddress() {
@@ -29,5 +58,31 @@ public class KatalonApplication {
         } catch (SocketException | UnknownHostException e) {
         }
         return StringUtils.defaultString(macAdress, UNKNOWN_HOST);
+    }
+    
+    private static class LocalMacAdress {
+        private String hostName;
+        private String macAddress;
+        
+        public LocalMacAdress(String hostName, String macAddress) {
+            this.setHostName(hostName);
+            this.setMacAddress(macAddress);
+        }
+
+        public String getHostName() {
+            return hostName;
+        }
+
+        public void setHostName(String hostName) {
+            this.hostName = hostName;
+        }
+
+        public String getMacAddress() {
+            return macAddress;
+        }
+
+        public void setMacAddress(String macAddress) {
+            this.macAddress = macAddress;
+        }
     }
 }
