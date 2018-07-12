d5097d602a10b21d15e8a2835517ead0dbaa3714
dungleonhart <dungleonhart@gmail.com>
2018-07-12 11:23:30 +0700

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -4,7 +4,7 @@ BRANCH="$1"
 
 PRODUCT_NAME="Katalon Studio"
 VOLUME_DIR="/Volumes/${PRODUCT_NAME}"
-PACKAGE_DIR="/Users/katalon/Public/Katalon Studio/${BRANCH}"
+PACKAGE_DIR="${HOME}/Public/Katalon Studio/${BRANCH}"
 MAC_DMG="${PRODUCT_NAME}.dmg"
 MAC_APP="${PRODUCT_NAME}.app"
 
@@ -17,4 +17,4 @@ cp -R "${VOLUME_DIR}/${MAC_APP}" /Applications
 echo "Installing ${PRODUCT_NAME} ... Done"
 
 echo "Verify package ..."
-"/Applications/${MAC_APP}/Contents/MacOS/katalon" --args -noSplash  -runMode=console -consoleLog -projectPath="/Users/katalon/Katalon Studio/TEST WEBUI/TEST WEBUI.prj" -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -executionProfile="default" -browserType="Chrome" --extra -katalon.buildLabel="${BRANCH}"
+"/Applications/${MAC_APP}/Contents/MacOS/katalon" --args -noSplash  -runMode=console -consoleLog -projectPath="${HOME}/Katalon Studio/TEST WEBUI/TEST WEBUI.prj" -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -executionProfile="default" -browserType="Chrome" --extra -katalon.buildLabel="${BRANCH}"
