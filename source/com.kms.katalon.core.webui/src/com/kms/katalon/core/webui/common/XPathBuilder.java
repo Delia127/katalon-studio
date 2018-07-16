7a23b2c5163426676e0d0a8c11bee90fcf618286
Thanh To <thanhto@kms.com.vn>
2018-07-16 17:22:26 +0700

    Revert "Revert "KAT-3081 Investigate record & playback failure on Airbnb's search box""
    This reverts commit 84173c9fdeb3a223ce921828a3a93bee1835fc99.

--- source/com.kms.katalon.core.webui/src/com/kms/katalon/core/webui/common/XPathBuilder.java
+++ source/com.kms.katalon.core.webui/src/com/kms/katalon/core/webui/common/XPathBuilder.java
@@ -1,7 +1,9 @@
 package com.kms.katalon.core.webui.common;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -9,8 +11,13 @@ import com.kms.katalon.core.testobject.ConditionType;
 import com.kms.katalon.core.testobject.TestObjectProperty;
 
 public class XPathBuilder {
+    
+    public static enum AggregationType {
+        UNION,
+        INTERSECT
+    }
 
-    private static final String XPATH_INTESECTION_FORMULA = "%s[count(. | %s) = count(%s)]";
+    private static final String XPATH_INTERSECT_FORMULA = "%s[count(. | %s) = count(%s)]";
 
     private static final String XPATH_CONDITION_TYPE_NOT_MATCHES = "not(matches(%s, '%s'))";
 
@@ -39,36 +46,57 @@ public class XPathBuilder {
     public XPathBuilder(List<TestObjectProperty> properties) {
         this.properties = properties;
     }
-
+    
+    /**
+     * convenient function to avoid changing signature
+     * @return
+     */
     public String build() {
+        return build(AggregationType.INTERSECT);
+    }
+
+    /**
+     * Union: "or" of all XPath locators, each contains a single condition
+     * @param aggregationType
+     * @return
+     */
+    public String build(AggregationType aggregationType) {
+        
+        boolean isIntersect = aggregationType.equals(AggregationType.INTERSECT);
+        boolean isUnion = !isIntersect;
+        
+        boolean hasTagCondition = false;
         tag = "*";
         predicates = new ArrayList<>();
         xpath = StringUtils.EMPTY;
 
         if (properties != null && !properties.isEmpty()) {
-            properties.stream().filter(p -> p.isActive()).forEach(p -> {
-                String propertyName = p.getName();
-                String propertyValue = p.getValue();
-                ConditionType conditionType = p.getCondition();
-                switch (PropertyType.nameOf(propertyName)) {
-                    case ATTRIBUTE:
-                        predicates.add(buildExpression("@" + propertyName, propertyValue, conditionType));
-                        break;
-                    case TAG:
-                        tag = propertyValue;
-                        break;
-                    case TEXT:
-                        String textExpression = buildExpression("text()", propertyValue, conditionType);
-                        String dotExpression = buildExpression(".", propertyValue, conditionType);
-                        predicates.add(String.format("(%s or %s)", textExpression, dotExpression));
-                        break;
-                    case XPATH:
-                        xpath = propertyValue;
-                        break;
-                    default:
-                        break;
+            for (TestObjectProperty p : properties) {
+                if (isUnion || p.isActive()) { // if union, use all properties
+                    String propertyName = p.getName();
+                    String propertyValue = p.getValue();
+                    ConditionType conditionType = p.getCondition();
+                    switch (PropertyType.nameOf(propertyName)) {
+                        case ATTRIBUTE:
+                            predicates.add(buildExpression("@" + propertyName, propertyValue, conditionType));
+                            break;
+                        case TAG:
+                            hasTagCondition = true;
+                            tag = propertyValue;
+                            break;
+                        case TEXT:
+                            String textExpression = buildExpression("text()", propertyValue, conditionType);
+                            String dotExpression = buildExpression(".", propertyValue, conditionType);
+                            predicates.add(String.format("(%s or %s)", textExpression, dotExpression));
+                            break;
+                        case XPATH:
+                            xpath = propertyValue;
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            });
+            }
         }
 
         List<String> xpaths = new ArrayList<>();
@@ -79,28 +107,74 @@ public class XPathBuilder {
 
         if (!predicates.isEmpty()) {
             StringBuilder propertyBuilder = new StringBuilder();
+            final String operator = isIntersect ? " and " : " or "; // intersect = and, union = or 
+            final String xpathTag = isIntersect ? tag : "*";
             propertyBuilder.append("//")
-                    .append(tag)
+                    .append(xpathTag)
                     .append("[")
-                    .append(StringUtils.join(predicates, " and "))
+                    .append(StringUtils.join(predicates, operator))
                     .append("]");
             xpaths.add(propertyBuilder.toString());
         }
+        
+        if (isUnion && hasTagCondition) { // union
+            xpaths.add("//" + tag);
+        }
 
-        return getXpathSelectorValue(xpaths);
+        return combineXpathLocators(xpaths, aggregationType);
     }
-
-    private String getXpathSelectorValue(List<String> xpathList) {
-        StringBuilder xpathString = new StringBuilder();
-        for (String xpath : xpathList) {
-            if (xpathString.toString().isEmpty()) {
-                xpathString.append(xpath);
-            } else {
-                String existingXpath = xpathString.toString();
-                xpathString = new StringBuilder(String.format(XPATH_INTESECTION_FORMULA, existingXpath, xpath, xpath));
+    
+    public List<Entry<String, String>> buildXpathBasedLocators() {
+        
+        List<Entry<String, String>> locators = new ArrayList<>();
+        
+        if (properties != null && !properties.isEmpty()) {
+            for (TestObjectProperty p : properties) {
+                String propertyName = p.getName();
+                String propertyValue = p.getValue();
+                ConditionType conditionType = p.getCondition();
+                Entry<String, String> entry;
+                switch (PropertyType.nameOf(propertyName)) {
+                    case TEXT:
+                        String textExpression = buildExpression("text()", propertyValue, conditionType);
+                        String dotExpression = buildExpression(".", propertyValue, conditionType);
+                        String predicate = String.format("(%s or %s)", textExpression, dotExpression);
+                        String locator = "//*[" + predicate + "]";
+                        entry = new AbstractMap.SimpleEntry<>(propertyName, locator);
+                        break;
+                    case XPATH:
+                        entry = new AbstractMap.SimpleEntry<>(propertyName, propertyValue);
+                        break;
+                    default:
+                        entry = null;
+                        break;
+                }
+                if (entry != null) {
+                    locators.add(entry);
+                }
             }
         }
-        return xpathString.toString();
+        
+        return locators;
+    }
+
+    private String combineXpathLocators(List<String> xpathList, AggregationType aggregationType) {
+        String xpathString;
+        if (aggregationType.equals(AggregationType.INTERSECT)) {
+            StringBuilder xpathStringBuilder = new StringBuilder();
+            for (String xpath : xpathList) {
+                if (xpathStringBuilder.toString().isEmpty()) {
+                    xpathStringBuilder.append(xpath);
+                } else {
+                    String existingXpath = xpathStringBuilder.toString();
+                    xpathStringBuilder = new StringBuilder(String.format(XPATH_INTERSECT_FORMULA, existingXpath, xpath, xpath));
+                }
+            }
+            xpathString = xpathStringBuilder.toString();
+        } else {
+            xpathString = StringUtils.join(xpathList, " | ");
+        }
+        return xpathString;
     }
 
     private String buildExpression(String propertyName, String propertyValue, ConditionType contidionType) {
@@ -150,7 +224,7 @@ public class XPathBuilder {
         return "'" + s + "'";
     }
 
-    private enum PropertyType {
+    public enum PropertyType {
         TAG, ATTRIBUTE, TEXT, XPATH;
 
         public static PropertyType nameOf(String name) {
