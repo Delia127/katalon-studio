967e6f93113315a26f479923c998a9b738d7b3cc
Tuan Anh Pham <tuanapham@kms.com.vn>
2018-07-30 15:59:29 +0700

    Fix performOK issue

--- source/com.kms.katalon.composer.integration.analytics/src/com/kms/katalon/composer/integration/analytics/preferences/AnalyticsPreferencesPage.java
+++ source/com.kms.katalon.composer.integration.analytics/src/com/kms/katalon/composer/integration/analytics/preferences/AnalyticsPreferencesPage.java
@@ -247,14 +247,15 @@ public class AnalyticsPreferencesPage extends FieldEditorPreferencePageWithHelp 
 
     @Override
     public boolean performOk() {
+
+        if (!isInitialized()) {
+            return true;
+        }
+        
         boolean integrationEnabled = enableAnalyticsIntegration.getSelection();
         
         if (!integrationEnabled) {
             updateDataStore();
-            return true;
-        }
-
-        if (!isInitialized()) {
             return true;
         }
 
