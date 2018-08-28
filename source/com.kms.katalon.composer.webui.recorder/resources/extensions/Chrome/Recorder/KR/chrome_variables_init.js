05f80d80f1fa89b2ae79ea699834708a6e4d82c7
Thanh To <thanhto@kms.com.vn>
2018-08-28 10:49:45 +0700

    Change Xpath into XPath

--- source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
+++ source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
@@ -1,4 +1,4 @@
-katalonServerPort = '63135'
+katalonServerPort = '52957'
 katalonOnOffStatus = true
 spy_captureObjectHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":false,"useMetaKey":false};
 spy_loadDomMapHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":true,"useMetaKey":false};
