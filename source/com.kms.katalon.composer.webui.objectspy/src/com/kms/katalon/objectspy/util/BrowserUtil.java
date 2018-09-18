e702be7fa27e54504293775c78207d12b583c934
Vinh Nguyen <vinhnguyen@kms-technology.com>
2018-09-18 15:34:52 +0700

    Adjust build's name and memory

--- source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/util/BrowserUtil.java
+++ source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/util/BrowserUtil.java
@@ -7,23 +7,9 @@ public class BrowserUtil {
 
     public static boolean isBrowserInstalled(WebUIDriverType webUIDriverType) {
         switch (webUIDriverType) {
-            case FIREFOX_DRIVER:
-                return PlatformUtil.isFirefoxInstalled();
-
-            case CHROME_DRIVER:
-                return PlatformUtil.isChromeInstalled();
-
-            case IE_DRIVER:
-                return PlatformUtil.isIEInstalled();
-
-            case EDGE_DRIVER:
-                return PlatformUtil.isEdgeInstalled();
-
-            case SAFARI_DRIVER:
-                return PlatformUtil.isSafariInstalled();
 
             default:
-                return false;
+                return true;
         }
     }
 
