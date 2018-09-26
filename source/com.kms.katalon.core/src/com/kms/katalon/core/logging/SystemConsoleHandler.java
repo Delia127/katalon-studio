f919164725639e3972a084772df9ef549a7995e0
Hai Minh Nguyen <nguyen.minh.hai@live.com>
2018-09-26 14:25:50 +0700

    update console color

--- source/com.kms.katalon.core/src/com/kms/katalon/core/logging/SystemConsoleHandler.java
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/logging/SystemConsoleHandler.java
@@ -8,16 +8,14 @@ import java.util.logging.ErrorManager;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
 
 public class SystemConsoleHandler extends ConsoleHandler {
-
-    private static final String ANSI_CONSOLE_ESCAPE = "\u001b[";
-    
-    private static final String ANSI_CONSOLE_RESET = ANSI_CONSOLE_ESCAPE + "0m";
     
     private static final Map<String, String> recordLevelWithPaddingLookup = new ConcurrentHashMap<>();
     
-    private static final Map<Integer, String> foregroundColorPrefixLookup = new ConcurrentHashMap<>();
+    private static final Map<Integer, Color> foregroundColorLookup = new ConcurrentHashMap<>();
 
     public SystemConsoleHandler() {
         super();
@@ -25,60 +23,46 @@ public class SystemConsoleHandler extends ConsoleHandler {
 
             public String format(LogRecord record) {
                 
-                String foregroundColorPrefix = getForegroundColor(record);
+                Color foregroundColor = getForegroundColor(record);
                 
-                String foregroundColorSuffix = "";
-                if (foregroundColorPrefix != null) {
-                    foregroundColorSuffix = ANSI_CONSOLE_RESET;
-                }
-
                 String recordLevelWithPadding = getRecordLevelWithPadding(record);
                 
-                String message = record.getMessage()
-                        .replaceAll("\r\n", "\n")
-                        .replaceAll("\n", foregroundColorSuffix + "\r\n" + foregroundColorPrefix);
-                
-                return foregroundColorPrefix
-                        + XMLLoggerParser.getRecordDate(record) 
+                String prologue = XMLLoggerParser.getRecordDate(record) 
                         + " " 
                         + recordLevelWithPadding 
-                        + " : "
-                        + message
-                        + foregroundColorSuffix 
-                        + "\r\n";
+                        + " : ";
+                if (foregroundColor != null) {
+                    prologue = ansi().fg(foregroundColor).a(prologue).reset().toString(); 
+                }
+                return prologue + record.getMessage() + "\r\n";
             }
 
-            private String getForegroundColor(LogRecord record) {
+            private Color getForegroundColor(LogRecord record) {
                 
                 int recordLevel = record.getLevel().intValue();
-                String foregroundColorPrefix = foregroundColorPrefixLookup.get(recordLevel);
+                Color foregroundColor = foregroundColorLookup.get(recordLevel);
                 
-                if (foregroundColorPrefix == null) {
+                if (foregroundColor == null) {
 
-                    String foregroundColor = null;
-                    
                     if (recordLevel == LogLevel.PASSED.getValue()) {
-                        foregroundColor = "2"; // green
+                        foregroundColor = GREEN; // green
                     } else if (recordLevel == LogLevel.WARNING.getValue() 
                             || recordLevel == LogLevel.FAILED.getValue() 
                             || recordLevel == LogLevel.ERROR.getValue()
                             || recordLevel == LogLevel.ABORTED.getValue()
                             || recordLevel == LogLevel.INCOMPLETE.getValue()) {
-                        foregroundColor = "9"; // red
+                        foregroundColor = RED; // red
                     } else if (recordLevel == LogLevel.NOT_RUN.getValue()) {
-                        foregroundColor = "6"; // cyan
+                        foregroundColor = CYAN; // cyan
                     } else if (recordLevel == LogLevel.RUN_DATA.getValue()) {
-                        foregroundColor = "12"; // blue
+                        foregroundColor = BLUE; // blue
                     }
-                    if (foregroundColor == null) {
-                        foregroundColorPrefix = "";
-                    } else {
-                        foregroundColorPrefix = ANSI_CONSOLE_ESCAPE + "38;5;" + foregroundColor + "m"; // xterm colors https://github.com/sindresorhus/xterm-colors
+                   
+                    if (foregroundColor != null) {
+                        foregroundColorLookup.put(recordLevel, foregroundColor);
                     }
-                    
-                    foregroundColorPrefixLookup.put(recordLevel, foregroundColorPrefix);
                 }
-                return foregroundColorPrefix;
+                return foregroundColor;
             }
 
             private String getRecordLevelWithPadding(LogRecord record) {
@@ -107,7 +91,9 @@ public class SystemConsoleHandler extends ConsoleHandler {
         try {
             String message = getFormatter().format(record);
             int recordLevel = record.getLevel().intValue();
-            if (recordLevel == LogLevel.END.getValue()) {
+            if (recordLevel != LogLevel.RUN_DATA.getValue() 
+                    && recordLevel >= LogLevel.WARNING.getValue()) {
+                System.err.write(message.getBytes());
             } else {
                 System.out.write(message.getBytes());
             }
