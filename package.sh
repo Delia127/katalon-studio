bff92f3e09a35060b17246f4d88ebd74ac6536b9
Vinh Nguyen <vinhnguyen@kms-technology.com>
2018-10-04 15:47:53 +0700

    Update package.sh

--- package.sh
+++ package.sh
@@ -69,8 +69,8 @@ sudo mv "${MAC_PACKAGE}" "${BRANCH_FOLDER}/"
 sudo cp "${WINDOWS_32_FILE}" "${BRANCH_FOLDER}/"
 sudo cp "${WINDOWS_64_FILE}" "${BRANCH_FOLDER}/"
 sudo mkdir -p $KATABUILD
-sudo umount -f /private/tmp/katabuild
 sudo mount_smbfs //katabuild:[katalon2018]@192.168.34.7/Katalon/public $KATABUILD
 rsync -rl $DISTRIBUTION_FOLDER/* $KATABUILD/
+sudo umount -f /private/tmp/katabuild
 sudo umount -f $KATABUILD
 echo "Distribute packages ... Done"
