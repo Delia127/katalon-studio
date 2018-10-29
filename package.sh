49103e7be69b3a55aa9d1a15b753356c0e1c4381
datquach <datquach@kms-technology.com>
2018-10-29 17:17:16 +0700

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,6 +5,7 @@ KATABUILD2=/tmp/katabuild2
 PACKAGE_FOLDER="source/com.kms.katalon.product/target/products"
 PRODUCT_NAME="Katalon_Studio"
 MAC_PRODUCT_NAME="Katalon Studio"
+TIMESTAMP="(date +"%T")"
 
 WINDOWS_32_FILE="${PACKAGE_FOLDER}/${PRODUCT_NAME}_Windows_32.zip"
 WINDOWS_64_FILE="${PACKAGE_FOLDER}/${PRODUCT_NAME}_Windows_64.zip"
@@ -90,11 +91,11 @@ BRANCH_FOLDER="${DISTRIBUTION_FOLDER}/${1}/${2}/" # JOB_BASE_NAME
 #sudo mv "${PRODUCT_NAME}_Windows_64.zip" "${PRODUCT_NAME}_Windows_64-${3}.zip"
 #cp -Rf $CURRENT
 cd $TEMP_PATH
-sudo mv "Katalon_Studio_Linux_64.tar.gz" "Katalon_Studio_Linux_64-$1.tar.gz"
-sudo mv "Katalon_Studio_MacOS.tar.gz" "Katalon_Studio_MacOS-$1.tar.gz"
-sudo mv "Katalon_Studio_Windows_32.zip" "Katalon_Studio_Windows_32-$1.zip"
-sudo mv "Katalon_Studio_Windows_64.zip" "Katalon_Studio_Windows_64-$1.zip"
-sudo mv "Katalon_Studio.dmg" "Katalon_Studio-MacOS-$1.dmg"
+sudo mv "Katalon_Studio_Linux_64.tar.gz" "Katalon_Studio_Linux_64-$1-$TIMESTAMP.tar.gz"
+sudo mv "Katalon_Studio_MacOS.tar.gz" "Katalon_Studio_MacOS-$1-$TIMESTAMP.tar.gz"
+sudo mv "Katalon_Studio_Windows_32.zip" "Katalon_Studio_Windows_32-$1-$TIMESTAMP.zip"
+sudo mv "Katalon_Studio_Windows_64.zip" "Katalon_Studio_Windows_64-$1-$TIMESTAMP.zip"
+sudo mv "Katalon_Studio.dmg" "Katalon_Studio-MacOS-$1-$TIMESTAMP.dmg"
 # echo $WORKSPACE
 mkdir -p $KATABUILD
 if [ ! -d "${KATABUILD}" ]; then
@@ -107,10 +108,10 @@ sudo mount_smbfs //katabuild:[katalon2018]@192.168.34.7/Katalon/public $KATABUIL
 sudo mount_smbfs //katabuild:[katalon2018]@192.168.35.52/share/build $KATABUILD2
 #sudo rsync -vaE --progress $DISTRIBUTION_FOLDER/ $KATABUILD/
 #sudo cp -Rf $DISTRIBUTION_FOLDER/* $KATABUILD/
-cp -Rf "Katalon_Studio_Linux_64-$1.tar.gz" $KATABUILD/
-cp -Rf "Katalon_Studio_Windows_32-$1.zip" $KATABUILD/
-cp -Rf "Katalon_Studio_Windows_64-$1.zip" $KATABUILD/
-cp -Rf "Katalon_Studio-MacOS-$1.dmg" $KATABUILD/
+cp -Rf "Katalon_Studio_Linux_64-$1-$TIMESTAMP.tar.gz" $KATABUILD/
+cp -Rf "Katalon_Studio_Windows_32-$1-$TIMESTAMP.zip" $KATABUILD/
+cp -Rf "Katalon_Studio_Windows_64-$1-$TIMESTAMP.zip" $KATABUILD/
+cp -Rf "Katalon_Studio-MacOS-$1-$TIMESTAMP.dmg" $KATABUILD/
 #sudo cp -Rf $DISTRIBUTION_FOLDER/* $KATABUILD2/
 sudo umount -f $KATABUILD
 echo "Distribute packages on macOS ... Done"
