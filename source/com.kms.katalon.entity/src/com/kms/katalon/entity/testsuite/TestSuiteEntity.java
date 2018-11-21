06c8235f7225d1573481250b1aee93c2a439a7ef
Huy Nguyen <huynguyen@kms-technology.com>
2018-11-21 10:05:27 +0700

    KAT-3769: Fix build error

--- source/com.kms.katalon.entity/src/com/kms/katalon/entity/testsuite/TestSuiteEntity.java
+++ source/com.kms.katalon.entity/src/com/kms/katalon/entity/testsuite/TestSuiteEntity.java
@@ -2,7 +2,6 @@ package com.kms.katalon.entity.testsuite;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
@@ -148,7 +147,6 @@ public class TestSuiteEntity extends IntegratedFileEntity {
         result = prime * result + (isRerun ? 1231 : 1237);
         result = prime * result + (isPageLoadTimeoutDefault ? 1231 : 1237);
         result = prime * result + (rerunFailedTestCasesOnly ? 1231 : 1237);
-        result = prime * result + ((lastRun == null) ? 0 : lastRun.hashCode());
         result = prime * result + pageLoadTimeout;
         result = prime * result + numberOfRerun;
         return result;
