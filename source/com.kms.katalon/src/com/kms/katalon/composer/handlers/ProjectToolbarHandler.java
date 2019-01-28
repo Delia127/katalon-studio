f24096caad51f1d95e2295828644ecaeae405a48
Huy Nguyen <huynguyen@kms-technology.com>
2019-01-28 14:52:48 +0700

    KAT-4019: Update KS layout for introducing Plugin Store

--- source/com.kms.katalon/src/com/kms/katalon/composer/handlers/ProjectToolbarHandler.java
+++ source/com.kms.katalon/src/com/kms/katalon/composer/handlers/ProjectToolbarHandler.java
@@ -9,6 +9,9 @@ import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MHandledToolItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.ToolItem;
 import org.openqa.selenium.Platform;
 import org.osgi.service.event.Event;
 
