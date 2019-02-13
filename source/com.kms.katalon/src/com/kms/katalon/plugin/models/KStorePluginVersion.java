e66b5452073d2919397d42d4430203ecf6e76461
Huy Nguyen <huynguyen@kms-technology.com>
2019-02-13 14:07:00 +0700

    KAT-4111 [KStore-Integration]: Fix error when parsing date

--- source/com.kms.katalon/src/com/kms/katalon/plugin/models/KStorePluginVersion.java
+++ source/com.kms.katalon/src/com/kms/katalon/plugin/models/KStorePluginVersion.java
@@ -1,7 +1,5 @@
 package com.kms.katalon.plugin.models;
 
-import java.util.Date;
-
 public class KStorePluginVersion {
     private long id;
     
@@ -9,9 +7,9 @@ public class KStorePluginVersion {
     
     private String status;
     
-    private Date createdAt;
+    private String createdAt;
     
-    private Date updatedAt;
+    private String updatedAt;
 
     public long getId() {
         return id;
@@ -37,19 +35,19 @@ public class KStorePluginVersion {
         this.status = status;
     }
 
-    public Date getCreatedAt() {
+    public String getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Date createdAt) {
+    public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
     }
 
-    public Date getUpdatedAt() {
+    public String getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
+    public void setUpdatedAt(String updatedAt) {
         this.updatedAt = updatedAt;
     }
 }
