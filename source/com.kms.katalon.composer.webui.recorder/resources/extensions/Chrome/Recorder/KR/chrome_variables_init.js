d7021346e7551f009667e1f8ce44944af8ea8f7c
Hiep Dang <hiepdang@kms.com.vn>
2019-02-18 09:38:02 +0700

    KAT-3670 : Generated more than one step when recording password field

--- source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
+++ source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
@@ -1,4 +1,4 @@
-katalonServerPort = '53328'
+katalonServerPort = '50499'
 katalonOnOffStatus = true
 spy_captureObjectHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":false,"useMetaKey":false};
 spy_loadDomMapHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":true,"useMetaKey":false};
