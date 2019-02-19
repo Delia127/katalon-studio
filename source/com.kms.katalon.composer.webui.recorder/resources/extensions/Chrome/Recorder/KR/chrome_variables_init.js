a2b3c2ed1137fe71c7117fd52d63a5f8e5cb76f2
Hiep Dang <hiepdang@kms.com.vn>
2019-02-19 18:14:50 +0700

    KAT-4045 : Override Global Variable for Test Suite Collection

--- source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
+++ source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
@@ -1,5 +1,4 @@
 katalonServerPort = '51577'
->>>>>>> origin/staging-6.0.1
 katalonOnOffStatus = true
 spy_captureObjectHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":false,"useMetaKey":false};
 spy_loadDomMapHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":true,"useMetaKey":false};
