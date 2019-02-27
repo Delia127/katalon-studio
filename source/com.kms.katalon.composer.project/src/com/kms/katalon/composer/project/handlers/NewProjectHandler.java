1259f9e17d50c68af5161a8f64eb47cc3bc6e5bb
Tô Minh Thành <thanhto@kms-technology.com>
2019-02-27 12:34:23 +0700

    Remove redundnacy

--- source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/handlers/NewProjectHandler.java
+++ source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/handlers/NewProjectHandler.java
@@ -1,8 +1,10 @@
 package com.kms.katalon.composer.project.handlers;
 
 import java.io.FileNotFoundException;
+
 import javax.xml.bind.MarshalException;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
@@ -16,29 +18,29 @@ import com.kms.katalon.entity.project.ProjectEntity;
 
 @SuppressWarnings("restriction")
 public class NewProjectHandler {
-	public static final String TEMPL_CUSTOM_KW_PKG_REL_PATH = "Keywords/com/example";
+    public static final String TEMPL_CUSTOM_KW_PKG_REL_PATH = "Keywords/com/example";
 
-	@Execute
-	public void execute(Shell shell) {
-		NewProjectDialog dialog = new NewProjectDialog(shell);
-		dialog.open();
-	}
+    @Execute
+    public void execute(Shell shell) {
+        NewProjectDialog dialog = new NewProjectDialog(shell);
+        dialog.open();
+    }
 
-	public static ProjectEntity createNewProject(String projectName, String projectLocation, String projectDescription)
-			throws Exception {
-		try {
-			ProjectEntity newProject = ProjectController.getInstance().addNewProject(projectName, projectDescription,
-					projectLocation);
-			// EntityTrackingHelper.trackProjectCreated();
-			return newProject;
-		} catch (MarshalException ex) {
-			if (!(ex.getLinkedException() instanceof FileNotFoundException)) {
-				throw ex;
-			}
-			LoggerSingleton.getInstance().getLogger().error(ex);
-			MessageDialog.openError(Display.getCurrent().getActiveShell(), StringConstants.ERROR_TITLE,
-					StringConstants.HAND_ERROR_MSG_NEW_PROJ_LOCATION_INVALID);
-		}
-		return null;
-	}
+    public static ProjectEntity createNewProject(String projectName, String projectLocation, String projectDescription)
+            throws Exception {
+        try {
+            ProjectEntity newProject = ProjectController.getInstance().addNewProject(projectName, projectDescription,
+                    projectLocation);
+//            EntityTrackingHelper.trackProjectCreated();
+            return newProject;
+        } catch (MarshalException ex) {
+            if (!(ex.getLinkedException() instanceof FileNotFoundException)) {
+                throw ex;
+            }
+            LoggerSingleton.getInstance().getLogger().error(ex);
+            MessageDialog.openError(Display.getCurrent().getActiveShell(), StringConstants.ERROR_TITLE,
+                    StringConstants.HAND_ERROR_MSG_NEW_PROJ_LOCATION_INVALID);
+        }
+        return null;
+    }
 }
