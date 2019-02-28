58d880276e5c63457f63bb541890ee0723aab3d7
Duy Luong <duyluong@kms-technology.com>
2019-02-28 14:59:11 +0700

    Add saving callback for test case integration view

--- source/com.kms.katalon.composer.testcase/src/com/kms/katalon/composer/testcase/parts/TestCaseIntegrationPart.java
+++ source/com.kms.katalon.composer.testcase/src/com/kms/katalon/composer/testcase/parts/TestCaseIntegrationPart.java
@@ -22,6 +22,7 @@ import org.eclipse.swt.widgets.ToolItem;
 import com.kms.katalon.composer.testcase.integration.TestCaseIntegrationFactory;
 import com.kms.katalon.composer.testcase.parts.integration.AbstractTestCaseIntegrationView;
 import com.kms.katalon.entity.integration.IntegratedEntity;
+import com.kms.katalon.entity.testcase.TestCaseEntity;
 
 public class TestCaseIntegrationPart {
     private ToolBar toolBar;
@@ -79,9 +80,9 @@ public class TestCaseIntegrationPart {
         mpart.setDirty(true);
         parentTestCaseCompositePart.updateDirty();
     }
-    
+
     public void reloadInput() {
-        
+
     }
 
     public void loadInput() {
@@ -91,8 +92,8 @@ public class TestCaseIntegrationPart {
         TestCaseIntegrationFactory.getInstance().getSortedViewBuilders().forEach(builderEntry -> {
             ToolItem item = new ToolItem(toolBar, SWT.CHECK);
             item.setText(builderEntry.getName());
-            integrationCompositeMap.put(builderEntry.getName(),
-                    builderEntry.getIntegrationView(parentTestCaseCompositePart.getTestCase(), mpart, parentTestCaseCompositePart));
+            integrationCompositeMap.put(builderEntry.getName(), builderEntry
+                    .getIntegrationView(parentTestCaseCompositePart.getTestCase(), mpart, parentTestCaseCompositePart));
         });
 
         for (ToolItem item : toolBar.getItems()) {
@@ -165,6 +166,18 @@ public class TestCaseIntegrationPart {
         });
 
         return editingIntegratedEntities;
+    }
+
+    public void onSaveSuccess(TestCaseEntity testCase) {
+        integrationCompositeMap.entrySet().stream().forEach(entry -> {
+            entry.getValue().onSaveSuccess(testCase);
+        });
+    }
+
+    public void onSaveFailure(Exception e) {
+        integrationCompositeMap.entrySet().stream().forEach(entry -> {
+            entry.getValue().onSaveFailure(e);
+        });
     }
 
 }
--- source/com.kms.katalon.platform.internal/src/com/kms/katalon/platform/internal/testcase/TestCaseIntegrationPlatformBuilderImpl.java
+++ source/com.kms.katalon.platform.internal/src/com/kms/katalon/platform/internal/testcase/TestCaseIntegrationPlatformBuilderImpl.java
@@ -26,133 +26,158 @@ import com.kms.katalon.entity.integration.IntegratedEntity;
 import com.kms.katalon.entity.integration.IntegratedType;
 import com.kms.katalon.entity.project.ProjectEntity;
 import com.kms.katalon.entity.testcase.TestCaseEntity;
+import com.kms.katalon.logging.LogUtil;
 import com.kms.katalon.platform.internal.entity.ProjectEntityImpl;
 import com.kms.katalon.platform.internal.entity.TestCaseEntityImpl;
 
 public class TestCaseIntegrationPlatformBuilderImpl implements TestCaseIntegrationPlatformBuilder {
 
-	@Inject
-	private IEclipseContext context;
+    @Inject
+    private IEclipseContext context;
 
-	private TestCaseIntegrationViewBuilder getViewerBuilder(TestCaseIntegrationViewDescription pluginViewDescription) {
-		String name = pluginViewDescription.getName();
-		TestCaseIntegrationView testCaseIntegrationView = ContextInjectionFactory
-				.make(pluginViewDescription.getTestCaseIntegrationView(), context);
-		return new PluginInterationViewBuilder(name, testCaseIntegrationView);
-	}
+    private TestCaseIntegrationViewBuilder getViewerBuilder(TestCaseIntegrationViewDescription pluginViewDescription) {
+        String name = pluginViewDescription.getName();
+        TestCaseIntegrationView testCaseIntegrationView = ContextInjectionFactory
+                .make(pluginViewDescription.getTestCaseIntegrationView(), context);
+        return new PluginInterationViewBuilder(name, testCaseIntegrationView);
+    }
 
-	@Override
-	public List<TestCaseIntegrationViewBuilder> getBuilders() {
-		com.katalon.platform.api.model.ProjectEntity project = new ProjectEntityImpl(
-				ProjectController.getInstance().getCurrentProject());
-		return ApplicationManager.getInstance().getExtensionManager()
-				.getExtensions(TestCaseIntegrationViewDescription.EXTENSION_POINT_ID).stream().filter(e -> {
-					return e.getImplementationClass() instanceof TestCaseIntegrationViewDescription
-							&& ((TestCaseIntegrationViewDescription) e.getImplementationClass()).isEnabled(project);
-				}).map(e -> getViewerBuilder((TestCaseIntegrationViewDescription) e.getImplementationClass()))
-				.collect(Collectors.toList());
-	}
+    @Override
+    public List<TestCaseIntegrationViewBuilder> getBuilders() {
+        com.katalon.platform.api.model.ProjectEntity project = new ProjectEntityImpl(
+                ProjectController.getInstance().getCurrentProject());
+        return ApplicationManager.getInstance()
+                .getExtensionManager()
+                .getExtensions(TestCaseIntegrationViewDescription.EXTENSION_POINT_ID)
+                .stream()
+                .filter(e -> {
+                    return e.getImplementationClass() instanceof TestCaseIntegrationViewDescription
+                            && ((TestCaseIntegrationViewDescription) e.getImplementationClass()).isEnabled(project);
+                })
+                .map(e -> getViewerBuilder((TestCaseIntegrationViewDescription) e.getImplementationClass()))
+                .collect(Collectors.toList());
+    }
 
-	private static class PluginInterationViewBuilder implements TestCaseIntegrationViewBuilder {
+    private static class PluginInterationViewBuilder implements TestCaseIntegrationViewBuilder {
 
-		private TestCaseIntegrationViewDescription description;
+        private TestCaseIntegrationViewDescription description;
 
-		private TestCaseIntegrationView testCaseIntegrationView;
+        private TestCaseIntegrationView testCaseIntegrationView;
 
-		private String name;
+        private String name;
 
-		public PluginInterationViewBuilder(String name, TestCaseIntegrationView testCaseIntegrationView) {
-			this.name = name;
-			this.testCaseIntegrationView = testCaseIntegrationView;
-		}
+        public PluginInterationViewBuilder(String name, TestCaseIntegrationView testCaseIntegrationView) {
+            this.name = name;
+            this.testCaseIntegrationView = testCaseIntegrationView;
+        }
 
-		@Override
-		public AbstractTestCaseIntegrationView getIntegrationView(TestCaseEntity testCase, MPart mpart,
-				SavableCompositePart parentPart) {
-			return new PluginIntegrationView(testCase, mpart, testCaseIntegrationView, parentPart);
-		}
+        @Override
+        public AbstractTestCaseIntegrationView getIntegrationView(TestCaseEntity testCase, MPart mpart,
+                SavableCompositePart parentPart) {
+            return new PluginIntegrationView(name, testCase, mpart, testCaseIntegrationView, parentPart);
+        }
 
-		@Override
-		public int preferredOrder() {
-			return 0;
-		}
+        @Override
+        public int preferredOrder() {
+            return 0;
+        }
 
-		@Override
-		public String getName() {
-			return name;
-		}
+        @Override
+        public String getName() {
+            return name;
+        }
 
-		@Override
-		public boolean isEnabled(ProjectEntity projectEntity) {
-			com.katalon.platform.api.model.ProjectEntity project = new ProjectEntityImpl(
-					ProjectController.getInstance().getCurrentProject());
-			return description.isEnabled(project);
-		}
-	}
+        @Override
+        public boolean isEnabled(ProjectEntity projectEntity) {
+            com.katalon.platform.api.model.ProjectEntity project = new ProjectEntityImpl(
+                    ProjectController.getInstance().getCurrentProject());
+            return description.isEnabled(project);
+        }
+    }
 
-	private static class PluginIntegrationView extends AbstractTestCaseIntegrationView {
+    private static class PluginIntegrationView extends AbstractTestCaseIntegrationView {
 
-		private TestCaseIntegrationView integrationView;
-		private PartActionServiceImpl partActionService;
-		private SavableCompositePart parentPart;
+        private TestCaseIntegrationView integrationView;
 
-		public PluginIntegrationView(TestCaseEntity testCaseEntity, MPart mpart,
-				TestCaseIntegrationView integrationView,
-				SavableCompositePart parentPart) {
-			super(testCaseEntity, mpart);
-			this.integrationView = integrationView;
-			this.parentPart = parentPart;
-		}
+        private PartActionServiceImpl partActionService;
 
-		@Override
-		public Composite createContainer(Composite parent) {
-			Composite container = new Composite(parent, SWT.NONE);
-			container.setLayout(new FillLayout());
+        private SavableCompositePart parentPart;
 
-			partActionService = new PartActionServiceImpl(testCaseEntity, mpart, parentPart);
-			integrationView.onCreateView(container, partActionService, new TestCaseEntityImpl(testCaseEntity));
+        private String name;
 
-			return container;
-		}
+        public PluginIntegrationView(String integrationName, TestCaseEntity testCaseEntity, MPart mpart,
+                TestCaseIntegrationView integrationView, SavableCompositePart parentPart) {
+            super(testCaseEntity, mpart);
+            this.name = integrationName;
+            this.integrationView = integrationView;
+            this.parentPart = parentPart;
+        }
 
-		@Override
-		public boolean needsSaving() {
-			return integrationView.needsSaving();
-		}
+        @Override
+        public Composite createContainer(Composite parent) {
+            Composite container = new Composite(parent, SWT.NONE);
+            container.setLayout(new FillLayout());
 
-		@Override
-		public IntegratedEntity getEditingIntegrated() {
-			Integration integration = integrationView.getIntegrationBeforeSaving();
-			if (integration == null) {
-				return null;
-			}
-			IntegratedEntity integratedEntity = new IntegratedEntity();
-			integratedEntity.setProductName(integration.getName());
-			integratedEntity.setType(IntegratedType.TESTCASE);
-			integratedEntity.setProperties(integration.getProperties());
-			return integratedEntity;
-		}
-	}
+            partActionService = new PartActionServiceImpl(testCaseEntity, mpart, parentPart);
+            try {
+                integrationView.onCreateView(container, partActionService, new TestCaseEntityImpl(testCaseEntity));
+            } catch (Exception e) {
+                LogUtil.printAndLogError(e, "Unable to create integration view for: " + name);
+            }
 
-	private static class PartActionServiceImpl implements PartActionService {
+            return container;
+        }
 
-		private MPart mpart;
-		private SavableCompositePart parentPart;
+        @Override
+        public boolean needsSaving() {
+            return integrationView.needsSaving();
+        }
 
-		public PartActionServiceImpl(TestCaseEntity testCaseEntity, MPart mpart, SavableCompositePart parentPart) {
-			this.mpart = mpart;
-			this.parentPart = parentPart;
-		}
+        @Override
+        public IntegratedEntity getEditingIntegrated() {
+            Integration integration = integrationView.getIntegrationBeforeSaving();
+            if (integration == null) {
+                return null;
+            }
+            IntegratedEntity integratedEntity = new IntegratedEntity();
+            integratedEntity.setProductName(integration.getName());
+            integratedEntity.setType(IntegratedType.TESTCASE);
+            integratedEntity.setProperties(integration.getProperties());
+            return integratedEntity;
+        }
 
-		@Override
-		public void markDirty() {
-			parentPart.setDirty(true);
-		}
+        @Override
+        public void onSaveSuccess(TestCaseEntity testCase) {
+            this.testCaseEntity = testCase;
+            integrationView.onSaveSuccess(new TestCaseEntityImpl(testCaseEntity));
+        }
 
-		@Override
-		public boolean isDirty() {
-			return parentPart.isDirty();
-		}
-	}
+        @Override
+        public void onSaveFailure(Exception failure) {
+            integrationView.onSaveFailure(failure);
+        }
+    }
+
+    private static class PartActionServiceImpl implements PartActionService {
+
+        private MPart mpart;
+
+        private SavableCompositePart parentPart;
+
+        public PartActionServiceImpl(TestCaseEntity testCaseEntity, MPart mpart, SavableCompositePart parentPart) {
+            this.mpart = mpart;
+            this.parentPart = parentPart;
+        }
+
+        @Override
+        public void markDirty() {
+            parentPart.setDirty(true);
+        }
+
+        @Override
+        public boolean isDirty() {
+            return parentPart.isDirty();
+        }
+    }
 
 }
