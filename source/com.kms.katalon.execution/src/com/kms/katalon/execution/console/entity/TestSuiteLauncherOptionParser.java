1b3aacd68d1c90480cda614f75b15d43a208c792
Hiep Dang <hiepdang@kms.com.vn>
2019-03-05 16:50:21 +0700

    KAT-4045 : Override Global Variable for Test Suite Collection

--- source/com.kms.katalon.execution/src/com/kms/katalon/execution/console/entity/TestSuiteLauncherOptionParser.java
+++ source/com.kms.katalon.execution/src/com/kms/katalon/execution/console/entity/TestSuiteLauncherOptionParser.java
@@ -121,6 +121,11 @@ public class TestSuiteLauncherOptionParser extends ReportableLauncherOptionParse
         allOptions.add(katalonStoreApiKeyOption);
         allOptions.add(installPluginOption);
         allOptions.add(testSuiteQuery);
+        ProjectEntity currentProject = ProjectController.getInstance().getCurrentProject();
+        if (currentProject != null && overridingOptions.isEmpty()) {
+            overridingOptions = new OverridingParametersConsoleOptionContributor(currentProject).getConsoleOptionList();
+        }
+        allOptions.addAll(overridingOptions);
         return allOptions;
     }
 
