a06b35c7c0726c2b76f743c7da08e443e4e0d074
Tô Minh Thành <thanhto@kms-technology.com>
2019-03-07 14:33:34 +0700

    Removed unnecessary changes

--- source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
+++ source/com.kms.katalon.composer.webui.recorder/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
@@ -1,4 +1,4 @@
-katalonServerPort = '58392'
+katalonServerPort = '56153'
 katalonOnOffStatus = true
 spy_captureObjectHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":false,"useMetaKey":false};
 spy_loadDomMapHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":true,"useMetaKey":false};
