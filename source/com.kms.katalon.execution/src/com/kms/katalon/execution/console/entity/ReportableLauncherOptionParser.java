8df2305f13ec3f0c79052677f3b797ab0a4d3409
Duy Luong <duyluong@kms-technology.com>
2019-03-08 11:14:34 +0700

    Revert "KAT-4114 : [Zendesk] Fail to merge Objects to Object Repository"
    This reverts commit b39ec1ff03525462bd36f8aa6d3159dca58fc1dc.

--- source/com.kms.katalon.execution/src/com/kms/katalon/execution/console/entity/ReportableLauncherOptionParser.java
+++ source/com.kms.katalon.execution/src/com/kms/katalon/execution/console/entity/ReportableLauncherOptionParser.java
@@ -10,6 +10,7 @@ import com.kms.katalon.controller.ProjectController;
 import com.kms.katalon.entity.project.ProjectEntity;
 import com.kms.katalon.execution.entity.DefaultReportSetting;
 import com.kms.katalon.execution.entity.DefaultRerunSetting;
+import com.kms.katalon.execution.exception.ExecutionException;
 
 public abstract class ReportableLauncherOptionParser implements LauncherOptionParser {
     protected DefaultReportSetting reportableSetting;
