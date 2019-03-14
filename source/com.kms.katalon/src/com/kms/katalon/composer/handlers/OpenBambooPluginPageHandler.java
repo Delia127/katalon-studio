d268b7919df9ffa5f8199993fdf302cc3bfcaa85
Huy Nguyen <huynguyen@kms-technology.com>
2019-03-14 16:31:23 +0700

    KAT-4187: Add integration menu

---
+++ source/com.kms.katalon/src/com/kms/katalon/composer/handlers/OpenBambooPluginPageHandler.java
@@ -0,0 +1,8 @@
+package com.kms.katalon.composer.handlers;
+
+public class OpenBambooPluginPageHandler extends OpenIntegrationPluginPageHandler {
+    @Override
+    protected String getPageUrl() {
+        return "https://marketplace.atlassian.com/apps/1220235/katalon-studio-for-bamboo";
+    }
+}
