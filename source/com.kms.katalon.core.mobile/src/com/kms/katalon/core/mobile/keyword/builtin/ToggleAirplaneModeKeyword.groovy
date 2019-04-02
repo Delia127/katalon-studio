f83005351f4cf15ea1b0d0fdf69f0b3d630d6ccb
Duy Luong <duyluong@kms-technology.com>
2019-04-02 17:27:34 +0700

    Fix ToggleAirplaneModeKeyword

--- source/com.kms.katalon.core.mobile/src/com/kms/katalon/core/mobile/keyword/builtin/ToggleAirplaneModeKeyword.groovy
+++ source/com.kms.katalon.core.mobile/src/com/kms/katalon/core/mobile/keyword/builtin/ToggleAirplaneModeKeyword.groovy
@@ -14,16 +14,18 @@ import com.kms.katalon.core.mobile.constants.StringConstants
 import com.kms.katalon.core.mobile.helper.MobileCommonHelper
 import com.kms.katalon.core.mobile.keyword.*
 import com.kms.katalon.core.mobile.keyword.internal.MobileAbstractKeyword
-import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
 import com.kms.katalon.core.mobile.keyword.internal.MobileKeywordMain
 import com.kms.katalon.core.model.FailureHandling
 
 import groovy.transform.CompileStatic
 import io.appium.java_client.AppiumDriver
+import io.appium.java_client.MobileElement
 import io.appium.java_client.TouchAction
 import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.android.connection.ConnectionState
-import io.appium.java_client.touch.offset.PointOption
+import io.appium.java_client.ios.IOSDriver
+import io.appium.java_client.touch.TapOptions
+import io.appium.java_client.touch.offset.ElementOption
 
 @Action(value = "toggleAirplaneMode")
 public class ToggleAirplaneModeKeyword extends MobileAbstractKeyword {
@@ -57,33 +59,42 @@ public class ToggleAirplaneModeKeyword extends MobileAbstractKeyword {
                 if (driver instanceof AndroidDriver) {
                     AndroidDriver androidDriver = (AndroidDriver) driver
                     androidDriver.setConnection(isTurnOn ? new ConnectionState(ConnectionState.AIRPLANE_MODE_MASK)
-                        : new ConnectionState(ConnectionState.WIFI_MASK))
+                            : new ConnectionState(ConnectionState.WIFI_MASK))
                 } else {
-                    String deviceModel = MobileDriverFactory.getDeviceModel()
-                    //ResourceBundle resourceBundle = ResourceBundle.getBundle("resource")
-                    //String[] point = resourceBundle.getString(deviceModel).split(";")
-                    if(MobileCommonHelper.deviceModels.get(deviceModel) == null){
-                        throw new StepFailedException("Device info not found. Please use ideviceinfo -u <udid> to read ProductType of iOS devices")
-                    }
-                    if(MobileCommonHelper.airPlaneButtonCoords.get(MobileCommonHelper.deviceModels.get(deviceModel)) == null || MobileCommonHelper.airPlaneButtonCoords.get(MobileCommonHelper.deviceModels.get(deviceModel)).equals("")) {
-                        throw new StepFailedException("AirplaneMode button coordinator not found.")
-                    }
-
-                    String[] point = MobileCommonHelper.airPlaneButtonCoords.get(MobileCommonHelper.deviceModels.get(deviceModel)).split(";")
-                    int x = Integer.parseInt(point[0])
-                    int y = Integer.parseInt(point[1])
+                    IOSDriver iOSDriver = (IOSDriver) driver
                     Dimension size = driver.manage().window().getSize()
-                    MobileCommonHelper.swipe(driver, 50, size.height, 50, size.height - 300)
-                    Thread.sleep(500)
-                    TouchAction tap = new TouchAction(driver).tap(PointOption.point(x, y)).release()
-                    tap.perform()
-                    MobileCommonHelper.swipe(driver, 50, 1, 50, size.height)
+
+                    MobileCommonHelper.swipe(driver,
+                            (size.getWidth() / 2) as int,
+                            size.getHeight(),
+                            (size.getWidth() / 2) as int,
+                            (size.getHeight() / 2) as int)
+
+                    MobileElement airplaneButton = iOSDriver.findElementsByXPath("//XCUIElementTypeSwitch[@visible='true' and @label='Airplane Mode']").get(0);
+                    boolean isEnabled = airplaneButton.getAttribute("value") == "1" ? true : false
+                    if (isTurnOn != isEnabled) {
+                        TouchAction tapAtAirPlaneButton = new TouchAction(driver)
+                                .tap(TapOptions.tapOptions().withElement(ElementOption.element(airplaneButton, 1, 1)))
+                        tapAtAirPlaneButton.release().perform()
+
+                        logger.logInfo("Airplane Mode switched from " + getSwitchStatus(isEnabled) + " to " + getSwitchStatus(isTurnOn))
+                    } else {
+                        logger.logInfo("Airplane Mode already switched to " + getSwitchStatus(isEnabled))
+                    }
+                    MobileElement backButton = iOSDriver.findElementsByXPath("//XCUIElementTypeOther[@visible='true' and @name='ControlCenterView']/XCUIElementTypeOther[@visible='true']").get(0);
+                    TouchAction tapAtBackButton = new TouchAction(driver)
+                            .tap(TapOptions.tapOptions().withElement(ElementOption.element(backButton, backButton.center)))
+                    tapAtBackButton.release().perform()
                 }
                 logger.logPassed(MessageFormat.format(StringConstants.KW_LOG_PASSED_TOGGLE_AIRPLANE_MODE, mode))
             } finally {
                 driver.context(context)
             }
-
         }, flowControl, true, StringConstants.KW_MSG_CANNOT_TOGGLE_AIRPLANE_MODE)
+    }
+
+    @CompileStatic
+    String getSwitchStatus(boolean status) {
+        return status ? "ON" : "OFF"
     }
 }
