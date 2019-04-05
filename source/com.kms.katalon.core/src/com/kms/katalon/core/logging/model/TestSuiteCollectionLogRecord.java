0e6bb3ffc8221c7f6e8df6689197efa00d65ad02
Duy Luong <duyluong@kms-technology.com>
2019-04-05 15:07:49 +0700

    Temp

---
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/logging/model/TestSuiteCollectionLogRecord.java
@@ -0,0 +1,12 @@
+package com.kms.katalon.core.logging.model;
+
+import java.util.List;
+
+public class TestSuiteCollectionLogRecord {
+
+    private String testSuiteCollectionId;
+
+    private String reportLocation;
+
+    private List<TestSuiteLogRecord> testSuiteLogRecords;
+}
