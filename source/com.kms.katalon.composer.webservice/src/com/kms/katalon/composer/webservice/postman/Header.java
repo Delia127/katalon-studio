0e03214daf1a996ad318e244f837cdba783dfbb7
hiepdang1908 <anhtutran3007@gmail.com>
2019-05-08 16:35:14 +0700

    issue2309: Import postman

--- source/com.kms.katalon.composer.webservice/src/com/kms/katalon/composer/webservice/postman/Header.java
+++ source/com.kms.katalon.composer.webservice/src/com/kms/katalon/composer/webservice/postman/Header.java
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.annotation.*;
 public class Header {
     private String key;
     private String value;
-
+    private String name;
+    private String type;
     @JsonProperty("key")
     public String getKey() { return key; }
     @JsonProperty("key")
@@ -15,4 +16,21 @@ public class Header {
     public String getValue() { return value; }
     @JsonProperty("value")
     public void setValue(String value) { this.value = value; }
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+    
 }
