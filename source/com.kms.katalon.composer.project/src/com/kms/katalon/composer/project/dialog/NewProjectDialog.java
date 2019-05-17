16a3302da656e369cbe410093726a1f7df635099
Duy Luong <duyluong@kms-technology.com>
2019-05-17 15:15:19 +0700

    Fix duplicated selection project type in New Project Dialog

--- source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/dialog/NewProjectDialog.java
+++ source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/dialog/NewProjectDialog.java
@@ -36,6 +36,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -288,8 +289,8 @@ public class NewProjectDialog extends TitleAreaDialog {
             cbProjects.select(cbProjects.indexOf(initialSampleProject.getName()));
         } else {
             populateWebProjects();
-            rbWebServiceProjectType.setSelection(true);
             rbWebProjectType.setSelection(true);
+            rbWebProjectType.notifyListeners(SWT.Selection, new Event());
             cbProjects.select(0);
         }
         showRepoUrlBySelectedProject();
