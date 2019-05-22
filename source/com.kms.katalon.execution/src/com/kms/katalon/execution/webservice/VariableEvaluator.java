33b8baf14dfb999e0c1fca13d257424ef5318b00
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2019-05-22 11:41:30 +0700

    issue2547: provide new mechanism to evaluate variables for web service test object

--- source/com.kms.katalon.execution/src/com/kms/katalon/execution/webservice/VariableEvaluator.java
+++ source/com.kms.katalon.execution/src/com/kms/katalon/execution/webservice/VariableEvaluator.java
@@ -1,54 +1,103 @@
 package com.kms.katalon.execution.webservice;
 
-import java.net.URL;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.URLClassLoader;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.codehaus.groovy.control.CompilerConfiguration;
-import org.codehaus.groovy.control.customizers.ImportCustomizer;
+import org.apache.commons.io.FileUtils;
 
+import com.kms.katalon.composer.components.log.LoggerSingleton;
+import com.kms.katalon.constants.IdConstants;
 import com.kms.katalon.controller.ProjectController;
-import com.kms.katalon.core.configuration.RunConfiguration;
-import com.kms.katalon.core.main.TestCaseMain;
-import com.kms.katalon.core.testcase.TestCaseFactory;
-import com.kms.katalon.core.testdata.TestDataFactory;
-import com.kms.katalon.core.testobject.ObjectRepository;
+import com.kms.katalon.entity.project.ProjectEntity;
+import com.kms.katalon.entity.testcase.WSVerificationTestCaseEntity;
+import com.kms.katalon.execution.configuration.VariableEvaluationRunConfiguration;
+import com.kms.katalon.execution.entity.WSVerificationTestCaseExecutedEntity;
+import com.kms.katalon.execution.util.ExecutionProfileStore;
 
+import groovy.lang.GroovyObject;
 import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 
 public class VariableEvaluator {
 
+    private static final String EVALUATION_SCRIPT_TEMPLATE_CLASS = IdConstants.KATALON_EXECUTION_BUNDLE_ID
+            + ".generator.VariableEvaluationScriptTemplate";
+    
+    private static final String GENERATE_EVALUATION_SCRIPT_METHOD = "generateEvaluationScript";
+
+    private static final String TEST_CASE_ID_PREFIX = "Variable-Eval_";
+
+    private static final String RESULT_FILE_PREFIX = "variable-eval-";
+
+    private static final String RESULT_TEMP_FOLDER = "variables";
+    
     public Map<String, Object> evaluate(String testObjectId, Map<String, String> variables) throws Exception {
         
         Map<String, Object> evaluatedVariables = new HashMap<>();
         
-        ImportCustomizer importCustomizer = new ImportCustomizer();
-        importCustomizer.addImport(TestDataFactory.class.getSimpleName(), TestDataFactory.class.getName());
-        importCustomizer.addImport(ObjectRepository.class.getSimpleName(), ObjectRepository.class.getName());
-        importCustomizer.addImport(TestCaseFactory.class.getSimpleName(), TestCaseFactory.class.getName());
-        importCustomizer.addImport(RunConfiguration.class.getSimpleName(), RunConfiguration.class.getName());
-        importCustomizer.addStaticImport(TestDataFactory.class.getName(), "findTestData");
-        importCustomizer.addStaticImport(ObjectRepository.class.getName(), "findTestObject");
-        importCustomizer.addStaticImport(TestCaseFactory.class.getName(), "findTestCase");
-        importCustomizer.addImport(TestCaseMain.class.getSimpleName(), TestCaseMain.class.getName());
-        importCustomizer.addImport("GlobalVariable", "internal.GlobalVariable");
+        File resultFile = createResultFile();
+
+        String evaluationScript = generateEvaluationScript(resultFile, variables);
         
-        CompilerConfiguration compilerConfig = new CompilerConfiguration();
-        compilerConfig.addCompilationCustomizers(importCustomizer);
+        WSVerificationTestCaseEntity testCaseEntity = createTestCaseEntity(evaluationScript);
+
+        VariableEvaluationRunConfiguration runConfig = new VariableEvaluationRunConfiguration();
+        runConfig.setExecutionProfile(ExecutionProfileStore.getInstance().getSelectedProfile());
+        runConfig.build(testCaseEntity, new WSVerificationTestCaseExecutedEntity(testCaseEntity));
+        
+        System.out.println(runConfig.getExecutionSetting().getSettingFilePath());
         
         URLClassLoader classLoader = ProjectController.getInstance().getProjectClassLoader(
                 ProjectController.getInstance().getCurrentProject());
-//        URL[] urls = classLoader.getURLs();
-//        for (URL url : urls) {
-//            System.out.println(url.toString());
-//        }
-        GroovyShell groovyShell = new GroovyShell(classLoader, compilerConfig);
-        for (Map.Entry<String, String> variableEntry : variables.entrySet()) {
-            Object evaluatedValue = groovyShell.evaluate("RunConfiguration.getExecutionProfile()");
-            evaluatedVariables.put(variableEntry.getKey(), evaluatedValue);
+        GroovyShell groovyShell = new GroovyShell(classLoader);
+        Script script = groovyShell.parse(runConfig.getExecutionSetting().getScriptFile());
+        script.run();
+        try (FileInputStream fis = new FileInputStream(resultFile);
+                ObjectInputStream ois = new ObjectInputStream(fis);) {
+
+            evaluatedVariables = (Map<String, Object>) ois.readObject();
+            
+        } catch (IOException | ClassNotFoundException e) {
+            LoggerSingleton.logError(e);
         }
         
+        FileUtils.forceDelete(resultFile);
+        
         return evaluatedVariables;
+    }
+    
+    
+    private String generateEvaluationScript(File resultFile, Map<String, String> variables) 
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Class clazz = Class.forName(EVALUATION_SCRIPT_TEMPLATE_CLASS);
+        GroovyObject object = (GroovyObject) clazz.newInstance();
+        String script = (String) object.invokeMethod(GENERATE_EVALUATION_SCRIPT_METHOD, 
+                new Object[] {resultFile.getAbsolutePath(), variables});
+        return script;
+    }
+    
+    private File createResultFile() throws IOException {
+        String tempFolderPath = ProjectController.getInstance().getTempDir() + File.separator + RESULT_TEMP_FOLDER;
+        File tempFolder = new File(tempFolderPath);
+        tempFolder.mkdirs();
+        File resultFile = File.createTempFile(RESULT_FILE_PREFIX, null, tempFolder);
+        System.out.println(resultFile.getAbsolutePath());
+        return resultFile;
+    }
+    
+    private WSVerificationTestCaseEntity createTestCaseEntity(String script) {
+        ProjectEntity project = ProjectController.getInstance().getCurrentProject();
+
+        WSVerificationTestCaseEntity testCaseEntity = new WSVerificationTestCaseEntity();
+        testCaseEntity.setId(TEST_CASE_ID_PREFIX + System.currentTimeMillis());
+        testCaseEntity.setProject(project);
+        testCaseEntity.setScript(script);
+
+        return testCaseEntity;
     }
 }
