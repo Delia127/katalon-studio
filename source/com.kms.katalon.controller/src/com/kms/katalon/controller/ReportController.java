0b4e62bb052ff91a5bc050f0d72851ee4b49ee1f
Thanh To <thanhto@FVFY81S1HV2H.local>
2019-06-21 20:15:26 +0700

    Fixed issue2700

--- source/com.kms.katalon.controller/src/com/kms/katalon/controller/ReportController.java
+++ source/com.kms.katalon.controller/src/com/kms/katalon/controller/ReportController.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.apache.commons.lang3.tuple.Pair;
 
 import com.kms.katalon.controller.exception.ControllerException;
 import com.kms.katalon.dal.exception.DALException;
@@ -25,6 +26,8 @@ import com.kms.katalon.entity.testsuite.TestSuiteCollectionEntity;
 import com.kms.katalon.entity.testsuite.TestSuiteEntity;
 
 public class ReportController extends EntityController {
+    private static final String TEST_SUITES_FOLDER = "Test Suites";
+
     private static EntityController _instance;
 
     public static String LOG_FILE_NAME = "execution0.log";
@@ -226,15 +229,23 @@ public class ReportController extends EntityController {
             return null;
         }
         String[] groupName = reportFolderId.split(StringConstants.ENTITY_ID_SEPARATOR);
-        return "Test Suites" + StringConstants.ENTITY_ID_SEPARATOR + groupName[groupName.length - 1];
+        return TEST_SUITES_FOLDER + StringConstants.ENTITY_ID_SEPARATOR + groupName[groupName.length - 1];
     }
 
     public TestSuiteEntity getTestSuiteByReportParentFolder(FolderEntity parentReportFolder) throws Exception {
-        String testSuiteDisplayId = "Test Suites" + StringConstants.ENTITY_ID_SEPARATOR + parentReportFolder.getName();
+        FolderEntity tempfolder = parentReportFolder;
+        StringBuilder strBuilder = new StringBuilder();
+        // Stop 1 level before 'Report' folder because 'Report' folder contains a 'sessionID' folder
+        while (tempfolder.getParentFolder() != null && tempfolder.getParentFolder().getParentFolder() != null
+                && !tempfolder.getParentFolder().getParentFolder().equals("Reports")) {
+            strBuilder.insert(0, StringConstants.ENTITY_ID_SEPARATOR + tempfolder.getName());
+            tempfolder = tempfolder.getParentFolder();
+        }
+        String testSuiteDisplayId = TEST_SUITES_FOLDER + strBuilder.toString();
         return TestSuiteController.getInstance().getTestSuiteByDisplayId(testSuiteDisplayId,
-                parentReportFolder.getProject());
+                tempfolder.getProject());
     }
-
+    
     public ReportEntity reloadReport(ReportEntity report, Entity entity) throws Exception {
         return getReportEntity(entity.getId());
     }
