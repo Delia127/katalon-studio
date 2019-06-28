334c303ada040967efa8a81a6319473dbaba1c04
Black <tuananhtran@kms-technology.com>
2019-06-28 13:11:46 +0700

    Revert "change variable"
    This reverts commit 6fe739efa30561fcae1045b96343769ba3f26a59.

--- source/com.kms.katalon.composer.integration.analytics/src/com/kms/katalon/composer/integration/analytics/preferences/AnalyticsPreferencesPage.java
+++ source/com.kms.katalon.composer.integration.analytics/src/com/kms/katalon/composer/integration/analytics/preferences/AnalyticsPreferencesPage.java
@@ -459,7 +459,7 @@ public class AnalyticsPreferencesPage extends FieldEditorPreferencePageWithHelp 
 
     private void updateDataStore() {
         try {
-            boolean encryptionEnabled = enableAnalyticsIntegration.getSelection();
+            boolean encryptionEnabled = chckEncrypt.getSelection();
             analyticsSettingStore.enableIntegration(isIntegratedSuccessfully());
             analyticsSettingStore.setServerEndPoint(txtServerUrl.getText(), encryptionEnabled);
             analyticsSettingStore.enableEncryption(encryptionEnabled);
