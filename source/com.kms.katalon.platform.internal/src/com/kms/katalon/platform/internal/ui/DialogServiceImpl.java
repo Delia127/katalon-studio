2b3c4e9110c7e1d00870eed43b7cd69e82c8b8e1
Black <tuananhtran@kms-technology.com>
2019-07-22 16:12:27 +0700

    update

--- source/com.kms.katalon.platform.internal/src/com/kms/katalon/platform/internal/ui/DialogServiceImpl.java
+++ source/com.kms.katalon.platform.internal/src/com/kms/katalon/platform/internal/ui/DialogServiceImpl.java
@@ -69,6 +69,7 @@ public class DialogServiceImpl implements DialogActionService {
         }
     }
     
+    @Override
     public FolderEntity showTestObjectFolderSelectionDialog(Shell parentShell, String dialogTitle) throws PlatformException {
         EntityLabelProvider labelProvider = new EntityLabelProvider();
         EntityProvider contentProvider = new EntityProvider();
@@ -97,6 +98,7 @@ public class DialogServiceImpl implements DialogActionService {
         }
     }
     
+    @Override
     public TestCaseEntity[] showTestCaseSelectionDialog(Shell parentShell, String dialogTitle)
             throws PlatformException {
         EntityLabelProvider labelProvider = new EntityLabelProvider();
