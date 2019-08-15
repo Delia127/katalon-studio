f4f8b6647b1c1072ad6ba7c0ecdc105badb3d49f
Thanh To <thanhto@FVFY81S1HV2H.local>
2019-08-16 01:34:46 +0700

    Revert unnecessary changes

--- source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/util/FileUtil.java
+++ source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/util/FileUtil.java
@@ -13,24 +13,24 @@ import org.osgi.framework.Bundle;
 import com.kms.katalon.execution.classpath.ClassPathResolver;
 
 public class FileUtil {
-    private static final String EXTENSIONS_FOLDER_NAME = "resources/extensions";
+	private static final String EXTENSIONS_FOLDER_NAME = "resources/extensions";
 
-    public static Image loadImage(Bundle bundle, String imageURI) {
-        URL url = FileLocator.find(bundle, new Path(imageURI), null);
-        ImageDescriptor image = ImageDescriptor.createFromURL(url);
-        return image.createImage();
-    }
-
-    public static File getExtensionsDirectory(Bundle bundle) throws IOException {
-        File bundleFile = FileLocator.getBundleFile(bundle);
-        if (bundleFile.isDirectory()) { // run by IDE
-            return new File(bundleFile + File.separator + EXTENSIONS_FOLDER_NAME);
-        } else { // run as product
-            return new File(ClassPathResolver.getConfigurationFolder(), EXTENSIONS_FOLDER_NAME);
-        }
-    }
-
-    public static File getExtensionBuildFolder() throws IOException {
-        return new File(ClassPathResolver.getConfigurationFolder(), EXTENSIONS_FOLDER_NAME);
-    }
+	public static Image loadImage(Bundle bundle, String imageURI) {
+		URL url = FileLocator.find(bundle, new Path(imageURI), null);
+		ImageDescriptor image = ImageDescriptor.createFromURL(url);
+		return image.createImage();
+	}
+	
+	public static File getExtensionsDirectory(Bundle bundle) throws IOException {
+		File bundleFile = FileLocator.getBundleFile(bundle);
+		if (bundleFile.isDirectory()) { // run by IDE
+			return new File(bundleFile + File.separator + EXTENSIONS_FOLDER_NAME);
+		} else { // run as product
+			return new File(ClassPathResolver.getConfigurationFolder(), EXTENSIONS_FOLDER_NAME);
+		}
+	}
+	
+	public static File getExtensionBuildFolder() throws IOException {
+	    return new File(ClassPathResolver.getConfigurationFolder(), EXTENSIONS_FOLDER_NAME);
+	}
 }
