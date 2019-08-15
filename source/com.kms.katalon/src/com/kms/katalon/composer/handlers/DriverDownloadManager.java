941589032add4bfc0c4a640b8fdcb5de221718a2
NgocHoGit <ngocho@kms-technology.com>
2019-08-15 16:47:25 +0700

    fix kill task webdriver running in OS_WIN32 OS_LINUX

--- source/com.kms.katalon/src/com/kms/katalon/composer/handlers/DriverDownloadManager.java
+++ source/com.kms.katalon/src/com/kms/katalon/composer/handlers/DriverDownloadManager.java
@@ -1,74 +1,119 @@
 package com.kms.katalon.composer.handlers;
 
+import static org.eclipse.core.runtime.Platform.getOS;
+import static org.eclipse.core.runtime.Platform.OS_WIN32;
+import static org.eclipse.core.runtime.Platform.OS_LINUX;
+
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.core.runtime.Platform;
+
+import com.kms.katalon.composer.components.log.LoggerSingleton;
+import com.kms.katalon.core.util.internal.ProcessUtil;
 import com.kms.katalon.core.webui.driver.WebUIDriverType;
 import com.kms.katalon.execution.webui.configuration.WebDriverManagerRunConfiguration;
 import com.kms.katalon.execution.webui.driver.SeleniumWebDriverProvider;
 
 public class DriverDownloadManager {
-	
-	public static int downloadDriver(String typeDriver){
-		
-//		try {
-//			String chromeDriverPath = SeleniumWebDriverProvider.getChromeDriverPath();
-//			
-//			WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
-//			webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.CHROME_DRIVER,
-//					 new File(chromeDriverPath).getParentFile());
-//               
-//			System.out.print(chromeDriverPath);
-//		} catch (IOException | InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		switch(typeDriver){
-		
-			case "Chrome":
-			{
-				// task kill
-				try {
-					Runtime.getRuntime().exec("TASKKILL /F /IM chromedriver.exe");
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-				// download driver
-				try {
-					String chromeDriverPath = SeleniumWebDriverProvider.getChromeDriverPath();
-					
-					WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
-					webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.CHROME_DRIVER,
-							 new File(chromeDriverPath).getParentFile());
-		               
-					System.out.print(chromeDriverPath);
-					return 1;
-				} catch (IOException | InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				break;
-			}
-			case "FireFox":
-			{
-				break;
-			}
-			case "IE":
-			{
-				break;
-			}
-			case "Edge":
-			{
-				break;
-			}
-			
-			default:
-				break;
-		}
-		
-		return 1;
-	}
 
+    public static void downloadDriver(WebUIDriverType typeDriver) throws InterruptedException, IOException {
+
+        switch (typeDriver) {
+        case CHROME_DRIVER:
+            switch (Platform.getOS()) {
+            case OS_WIN32:
+                ProcessUtil.killProcessOnWindows("chromedriver.exe");
+                break;
+            case OS_LINUX:
+                ProcessUtil.killProcessOnUnix("chromedriver");
+                break;
+            default:
+                break;
+            }
+            try {
+                String chromeDriverPath = SeleniumWebDriverProvider.getChromeDriverPath();
+
+                WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
+                webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.CHROME_DRIVER,
+                        new File(chromeDriverPath).getParentFile());
+                
+                LoggerSingleton.logInfo("Downloaded.");
+            } catch (IOException e) {
+                LoggerSingleton.logError(e);
+            }
+            break;
+        case FIREFOX_DRIVER:
+            switch (Platform.getOS()) {
+            case OS_WIN32:
+                ProcessUtil.killProcessOnWindows("geckodriver.exe");
+                break;
+            case OS_LINUX:
+                ProcessUtil.killProcessOnUnix("geckodriver");
+                break;
+            default:
+                break;
+            }
+            try {
+                String firefoxDriverPath = SeleniumWebDriverProvider.getGeckoDriverPath();
+
+                WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
+                webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.FIREFOX_DRIVER,
+                        new File(firefoxDriverPath).getParentFile());
+                
+                LoggerSingleton.logInfo("Downloaded.");
+            } catch (IOException e) {
+                LoggerSingleton.logError(e);
+            }
+            break;
+        case IE_DRIVER:
+            switch (Platform.getOS()) {
+            case OS_WIN32:
+                ProcessUtil.killProcessOnWindows("IEDriverServer.exe");
+                break;
+            case OS_LINUX:
+                ProcessUtil.killProcessOnUnix("IEDriverServer");
+                break;
+            default:
+                break;
+            }
+            try {
+                String ieDriverPath = SeleniumWebDriverProvider.getIEDriverPath();
+
+                WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
+                webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.IE_DRIVER,
+                        new File(ieDriverPath).getParentFile());
+                
+                LoggerSingleton.logInfo("Downloaded.");
+            } catch (IOException e) {
+                LoggerSingleton.logError(e);
+            }
+            break;
+        case EDGE_DRIVER:
+            switch (Platform.getOS()) {
+            case OS_WIN32:
+                ProcessUtil.killProcessOnWindows("msedgedriver.exe");
+                break;
+            case OS_LINUX:
+                ProcessUtil.killProcessOnUnix("msedgedriver");
+                break;
+            default:
+                break;
+            }
+            try {
+                String edgeDriverPath = SeleniumWebDriverProvider.getEdgeDriverPath();
+
+                WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
+                webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.EDGE_DRIVER,
+                        new File(edgeDriverPath).getParentFile());
+                
+                LoggerSingleton.logInfo("Downloaded.");
+            } catch (IOException e) {
+                LoggerSingleton.logError(e);
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }
--- source/com.kms.katalon/src/com/kms/katalon/composer/handlers/UpdateChromeWebdriverHandler.java
+++ source/com.kms.katalon/src/com/kms/katalon/composer/handlers/UpdateChromeWebdriverHandler.java
@@ -1,4 +1,5 @@
 package com.kms.katalon.composer.handlers;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -11,26 +12,12 @@ import com.kms.katalon.execution.webui.configuration.WebDriverManagerRunConfigur
 import com.kms.katalon.execution.webui.driver.SeleniumWebDriverProvider;
 
 public class UpdateChromeWebdriverHandler {
-	 
-	@Execute
-	public void execute(Shell shell) {
-//		try {
-//			String chromeDriverPath = SeleniumWebDriverProvider.getChromeDriverPath();
-//			
-//			WebDriverManagerRunConfiguration webDriverManagerRunConfiguration = new WebDriverManagerRunConfiguration();
-//			webDriverManagerRunConfiguration.downloadDriver(WebUIDriverType.CHROME_DRIVER,
-//					 new File(chromeDriverPath).getParentFile());
-//               
-//			System.out.print(chromeDriverPath);
-//			MessageDialog.openInformation(shell, "Path", chromeDriverPath);
-//		} catch (IOException | InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		DriverDownloadManager.downloadDriver("Chrome");
-		
-	}
-    
+
+    @Execute
+    public void execute(Shell shell) throws InterruptedException, IOException {
+
+        DriverDownloadManager.downloadDriver(WebUIDriverType.EDGE_DRIVER);
+
+    }
 
 }
