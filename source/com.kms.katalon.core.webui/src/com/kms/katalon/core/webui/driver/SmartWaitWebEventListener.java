4be578d2a4bfcb51075c87828b1afc7a5f21a006
Thanh To <thanhto@FVFY81S1HV2H.local>
2019-08-16 15:02:48 +0700

    Simplify SmartWaitWebEventListener implementation

--- source/com.kms.katalon.core.webui/src/com/kms/katalon/core/webui/driver/SmartWaitWebEventListener.java
+++ source/com.kms.katalon.core.webui/src/com/kms/katalon/core/webui/driver/SmartWaitWebEventListener.java
@@ -3,10 +3,9 @@ package com.kms.katalon.core.webui.driver;
 import java.util.Optional;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 
 import com.kms.katalon.core.configuration.RunConfiguration;
 import com.kms.katalon.core.webui.common.internal.SmartWaitHelper;
@@ -22,73 +21,7 @@ import com.kms.katalon.core.webui.common.internal.SmartWaitHelper;
  * @author thanhto
  *
  */
-public class SmartWaitWebEventListener implements WebDriverEventListener {
-
-    @Override
-    public void afterAlertAccept(WebDriver arg0) {
-    }
-
-    @Override
-    public void afterAlertDismiss(WebDriver arg0) {
-    }
-
-    @Override
-    public void afterChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
-
-    }
-
-    @Override
-    public void afterClickOn(WebElement arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void afterFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-    }
-
-    @Override
-    public <X> void afterGetScreenshotAs(OutputType<X> arg0, X arg1) {
-
-    }
-
-    @Override
-    public void afterGetText(WebElement arg0, WebDriver arg1, String arg2) {
-    }
-
-    @Override
-    public void afterNavigateBack(WebDriver arg0) {
-    }
-
-    @Override
-    public void afterNavigateForward(WebDriver arg0) {
-    }
-
-    @Override
-    public void afterNavigateRefresh(WebDriver arg0) {
-    }
-
-    @Override
-    public void afterNavigateTo(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void afterScript(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void afterSwitchToWindow(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void beforeAlertAccept(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeAlertDismiss(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
-    }
+public class SmartWaitWebEventListener extends AbstractWebDriverEventListener {
 
     @Override
     public void beforeClickOn(WebElement arg0, WebDriver arg1) {
@@ -101,40 +34,8 @@ public class SmartWaitWebEventListener implements WebDriverEventListener {
     }
 
     @Override
-    public <X> void beforeGetScreenshotAs(OutputType<X> arg0) {
-    }
-
-    @Override
     public void beforeGetText(WebElement arg0, WebDriver arg1) {
         doSmartWait();
-    }
-
-    @Override
-    public void beforeNavigateBack(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeNavigateForward(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeNavigateRefresh(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeNavigateTo(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void beforeScript(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void beforeSwitchToWindow(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void onException(Throwable arg0, WebDriver arg1) {
     }
 
     public void doSmartWait() {
