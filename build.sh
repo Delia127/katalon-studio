2dcd43ac0b9ffa6bd871f372e033f9d203bbaf57
Qui Le <quile@kms-technology.com>
2019-08-26 17:50:05 +0700

    update

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -xe
 
 ulimit -c unlimited
-cd source/com.kms.katalon.repo && mvn p2:site
-cd source/com.kms.katalon.repo && nohup mvn -Djetty.port=9999 jetty:run > /tmp/9999.log &
+cd /katalon/source/com.kms.katalon.repo && mvn p2:site
+cd /katalon/source/com.kms.katalon.repo && nohup mvn -Djetty.port=9999 jetty:run > /tmp/9999.log &
 
 until $(curl --output /dev/null --silent --head --fail http://localhost:9999/site); do
     printf '.'
@@ -12,13 +12,15 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:9999/sit
     sleep 5
 done
 
-cd source/com.kms.katalon.p2site && nohup mvn -Djetty.port=33333 jetty:run > /tmp/33333.log &
+cd /katalon/source/com.kms.katalon.p2site && nohup mvn -Djetty.port=33333 jetty:run > /tmp/33333.log &
                     
 until $(curl --output /dev/null --silent --head --fail http://localhost:33333/site); do
     printf '.'
     cat /tmp/33333.log
     sleep 5
 done
+
+cd /katalon/source
 
 command=verify
 
