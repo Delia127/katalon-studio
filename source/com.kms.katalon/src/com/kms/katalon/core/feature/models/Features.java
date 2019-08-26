7da4e600f286c365584db05062d26e93a069ed0e
Thanh To <thanhto@FVFY81S1HV2H.local>
2019-08-26 17:39:48 +0700

    Implement a mock service

--- source/com.kms.katalon/src/com/kms/katalon/core/feature/models/Features.java
+++
@@ -1,9 +0,0 @@
-package com.kms.katalon.core.feature.models;
-
-public enum Features {
-    PRIVATE_PLUGIN;
-
-    public Features[] getSupportedFeatures() {
-        return Features.values();
-    }
-}
