f52e59021b4570389eecd6b62ec215e317e52438
Qui Le <quile@kms-technology.com>
2019-08-27 13:33:30 +0700

    update

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 
 set -xe
 
+BUILD_REPOSITORY_LOCALPATH='/Users/quiducle/Documents/Projects/katalon'
+MAVEN_OPTS=''
+
 mavenVersion=3.5.4
 tmpDir="/tmp/katabuild"
+version=6.3.3
+isQtest=true
+isRelease=true
+withUpdate=true
+tag="6.3.3"
 
 # Building
 ulimit -c unlimited
@@ -27,8 +35,6 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:33333/si
     sleep 5
 done
 
-isQtest=true
-
 if [ "$isQtest" = "true" ]
 then
     echo "Building: qTest Prod"
@@ -38,19 +44,25 @@ else
     cd $BUILD_REPOSITORY_LOCALPATH/source && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} -pl '!com.kms.katalon.product.qtest_edition' clean verify -P prod
 fi
 
-cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.apidocs && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} clean verify && cp -R 'target/resources/apidocs' $tmpDir      
+cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.apidocs && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} clean verify && cp -R 'target/resources/apidocs' ${tmpDir}      
 
 # Copy builds
 cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.product/target/products
 if [ "$isQtest" = "false" ]
 then
-    cd com.kms.katalon.product.product/macosx/cocoa/x86_64 && cp -R 'Katalon Studio.app' $tmpDir
+    cd com.kms.katalon.product.product/macosx/cocoa/x86_64 && cp -R 'Katalon Studio.app' ${tmpDir}
+    cp *.zip ${tmpDir}
+    cp *.tar.gz ${tmpDir}
+    cp *.app ${tmpDir}
 fi
 
 cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.product.qtest_edition/target/products
 if [ "$isQtest" = "true" ]
 then
-    cd com.kms.katalon.product.qtest_edition.product/macosx/cocoa/x86_64 && cp -R 'Katalon Studio.app' $tmpDir
+    cd com.kms.katalon.product.qtest_edition.product/macosx/cocoa/x86_64 && cp -R 'Katalon Studio.app' ${tmpDir}
+    cp *.zip ${tmpDir}
+    cp *.tar.gz ${tmpDir}
+    cp *.app ${tmpDir}
 fi
 
 # Sign file
@@ -59,4 +71,70 @@ cd $BUILD_REPOSITORY_LOCALPATH
 
 # Package .DMG file
 cd $BUILD_REPOSITORY_LOCALPATH
-./dropdmg.sh $tmpDir
+if [ "$isRealease" = "true" ]
+then
+    ./dropdmg.sh $tmpDir
+fi
+
+# # Generate update packages
+# cd $BUILD_REPOSITORY_LOCALPATH
+# if [ "$withUpdate" = "true" ]
+# then
+#     cd tools/updater
+#     def updateInfo = [
+#         buildDir: "${WORKSPACE}/source/com.kms.katalon.product/target/products/com.kms.katalon.product.product",
+#         destDir: "${tmpDir}/update",
+#         version: "${version}"
+#     ]
+#     def json = JsonOutput.toJson(updateInfo)
+#     json = JsonOutput.prettyPrint(json)
+#     writeFile(file: 'scan_info.json', text: json)
+#     java -jar json-map-builder-1.0.0.jar
+# }
+               
+
+# # Repackage
+# cd $BUILD_REPOSITORY_LOCALPATH
+# cd tools/repackage
+# npm prune && npm install
+# node repackage.js ${tmpDir}/Katalon_Studio_Windows_32.zip ${version}
+# node repackage.js ${tmpDir}/Katalon_Studio_Windows_64.zip ${version}
+# node repackage.js ${tmpDir}/Katalon_Studio_Linux_64.tar.gz ${version}
+
+# rm -rf ${tmpDir}/*.zip
+# rm -rf ${tmpDir}/*.tar.gz
+# mv ${tmpDir}/output/*.zip ${tmpDir}/
+# mv ${tmpDir}/output/*.tar.gz ${tmpDir}/
+# rm -rf ${env.tmpDir}/output
+# cd '${tmpDir}' && zip -r '${tmpDir}/Katalon Studio.app.zip' 'Katalon Studio.app'
+
+# rm -rf '${env.tmpDir}/Katalon Studio.app'
+
+# cd '${tmpDir}' && zip -r '${tmpDir}/apidocs.zip' 'apidocs'
+# rm -rf '${tmpDir}/apidocs'
+
+# # Upload update packages to S3
+# if [ "$withUpdate" = "true" ]
+# then
+#     withAWS(region: 'us-east-1', credentials: 'katalon-deploy') {
+#         s3Upload(file: "${env.tmpDir}/update/${tag}", bucket:'katalon', path: "update/${tag}", acl:'PublicRead')
+#     }
+#     rm -rf '${env.tmpDir}/update'
+# fi
+        
+# # Upload build packages to S3
+# if [ "$isRelease" = "true" ]
+# then
+#     if [ "$isQtest" = "true" ]
+#     then
+#         s3Location = "${tag}/qTest"
+#     elif [ "$isBeta" = "true" ]
+#     then
+#         s3Location = "release-beta/${tag}"
+#     else
+#         s3Location = "${tag}"
+#     fi
+#     withAWS(region: 'us-east-1', credentials: 'katalon-deploy') {
+#         s3Upload(file: "${env.tmpDir}", bucket:'katalon', path: "${s3Location}", acl:'PublicRead')
+# fi
+      
