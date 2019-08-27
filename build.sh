6a41904beef6f35383aa580aa6eae5bbb638298f
Qui Le <quile@kms-technology.com>
2019-08-27 11:33:02 +0700

    update

--- build.sh
+++ build.sh
@@ -7,10 +7,12 @@ ulimit -c unlimited
 mavenVersion=3.5.4
 cd $BUILD_REPOSITORY_LOCALPATH/source && mvn ${MAVEN_OPTS} -N io.takari:maven:wrapper -Dmaven=$mavenVersion
 
+cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.repo && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} p2:site 
+
+ls -al 
 cd $BUILD_REPOSITORY_LOCALPATH/source
 ls -al 
 
-# cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.repo && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} p2:site 
 # cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.repo && nohup $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} -Djetty.port=9999 jetty:run > /tmp/9999.log &
 
 # until $(curl --output /dev/null --silent --head --fail http://localhost:9999/site); do
@@ -38,12 +40,8 @@ ls -al
 #     $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} -pl '!com.kms.katalon.product.qtest_edition' clean verify -P prod
 # fi
 
-# cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.apidocs && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} clean ${command} 
-
-# cd $BUILD_REPOSITORY_LOCALPATH/source
-# ls -al 
-# cd target
+# cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.product
 # ls -al
+# cd target/products
 
-# && cp -R 'target/resources/apidocs' '/tmp/katabuild'
-                 
+# cd $BUILD_REPOSITORY_LOCALPATH/source/com.kms.katalon.apidocs && $BUILD_REPOSITORY_LOCALPATH/source/mvnw ${MAVEN_OPTS} clean ${command} && cp -R 'target/resources/apidocs' '/tmp/katabuild'              
