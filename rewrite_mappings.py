7f7b6ef61219d3fb656f21b2d047025e849771b1
Qui Le <quile@kms-technology.com>
2019-08-28 23:54:36 +0700

    update

--- rewrite_mappings.py
+++ rewrite_mappings.py
@@ -1,11 +1,12 @@
 import sys
+import re
 from build_utils import write_file, read_file
 
 file_path = sys.argv[1]
 commit_id = sys.argv[2]
 tag = sys.argv[3]
 
-title_version = "{0}-{1}".format(tag, commit_id)
+title_version = '{0}-{1}'.format(tag, commit_id)
 version_mapping = read_file(file_path = file_path)
-version_mapping = version_mapping.replaceAll(/3=.*/, "3={0}".format(title_version))
+version_mapping = re.sub('3=.*', '3={0}'.format(title_version), version_mapping)
 write_file(file_path = file_path, text = version_mapping)
