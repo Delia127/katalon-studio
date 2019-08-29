88705f8ac2b48b6911fed90709daa96a9ea04165
Qui Le <quile@kms-technology.com>
2019-08-29 12:52:23 +0700

    update

--- create_release.sh
+++ create_release.sh
@@ -10,13 +10,7 @@ create_github_release() {
         node app.js ${github_token} v${tag} \
             "${tmpDir}/lastest_release.json" \
             "${tmpDir}/latest_version.json" \
-            "${tmpDir}/releases.json" \
-            "${tmpDir}/apidocs.zip" \
-            "${tmpDir}/commit.txt" \
-            "${tmpDir}/Katalon Studio.app.zip" \
-            "${tmpDir}/Katalon_Studio_Linux_64-${version}.tar.gz" \
-            "${tmpDir}/Katalon_Studio_Windows_32-${version}.zip" \
-            "${tmpDir}/Katalon_Studio_Windows_64-${version}.zip"
+            "${tmpDir}/releases.json"
     fi         
 }
 
