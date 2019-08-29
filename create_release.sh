820aa5d4ee546c4bbe8cc28107da527f2fa5686b
Qui Le <quile@kms-technology.com>
2019-08-29 12:35:53 +0700

    update

--- create_release.sh
+++ create_release.sh
@@ -3,20 +3,14 @@
 set -xe
 
 create_github_release() {
-    if [ "$isRelease" = "true" -a "$isRelease" = "false" ]
+    if [ "$isRelease" = "true" -a "$isQtest" = "false" ]
     then
         cd $katalonDir/tools/release
         npm prune && npm install
         node app.js ${github_token} v${tag} \
             '${tmpDir}/lastest_release.json' \
             '${tmpDir}/latest_version.json' \
-            '${tmpDir}/releases.json' \
-            '${tmpDir}/apidocs.zip' \
-            '${tmpDir}/commit.txt' \
-            '${tmpDir}/Katalon Studio.app.zip' \
-            '${tmpDir}/Katalon_Studio_Linux_64-${version}.tar.gz' \
-            '${tmpDir}/Katalon_Studio_Windows_32-${version}.zip' \
-            '${tmpDir}/Katalon_Studio_Windows_64-${version}.zip'
+            '${tmpDir}/releases.json'
     fi         
 }
 
