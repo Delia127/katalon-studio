b24fbef6f995b8ca25a5629631c17d74f5e8111e
Qui Le <quile@kms-technology.com>
2019-08-30 16:17:03 +0700

    update

--- create_dmg.sh
+++ create_dmg.sh
@@ -2,10 +2,6 @@
 ulimit -c unlimited
 KATABUILD="${1}"
 
-nvm install 10.15.3
-nvm use 10.15.3
-npm install --global create-dmg
-
 #Create .dmg file
 cd $KATABUILD
 sudo codesign --verbose --force --deep --sign "80166EC5AD274586C44BD6EE7A59F016E1AB00E4" --timestamp=none "Katalon Studio.app"
