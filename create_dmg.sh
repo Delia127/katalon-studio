ce62185ea63146929ccde7c2aaa7f73c355229b7
Qui Le <quile@kms-technology.com>
2019-08-30 17:42:48 +0700

    update

--- create_dmg.sh
+++ create_dmg.sh
@@ -15,4 +15,5 @@ npm install --global create-dmg
 cd $KATABUILD
 sudo codesign --verbose --force --deep --sign "80166EC5AD274586C44BD6EE7A59F016E1AB00E4" --timestamp=none "Katalon Studio.app"
 sudo create-dmg "Katalon Studio.app"
+mv 'Katalon 1.0.0.dmg' 'Katalon Studio.dmg'
 echo "Packaging DMG ... Done"
