0366ff33b9ebb5242bc078d7284e3ff0f7af2bee
Qui Le <quile@kms-technology.com>
2019-08-30 13:52:36 +0700

    update

--- dropdmg.sh
+++ dropdmg.sh
@@ -5,5 +5,6 @@ KATABUILD="${1}"
 #Create .dmg file
 cd $KATABUILD
 sudo codesign --verbose --force --deep --sign "80166EC5AD274586C44BD6EE7A59F016E1AB00E4" --timestamp=none "Katalon Studio.app"
-sudo /usr/local/bin/dropdmg --config-name "Katalon Studio" "Katalon Studio.app"
+# sudo /usr/local/bin/dropdmg --config-name "Katalon Studio" "Katalon Studio.app"
+sudo hdiutil create -srcfolder "Katalon Studio.app" "Katalon Studio"
 echo "Packaging DMG ... Done"
