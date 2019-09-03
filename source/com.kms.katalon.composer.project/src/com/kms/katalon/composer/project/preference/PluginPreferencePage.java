0c7779896c7febc2b0540927294de99b5a3b068f
Minh Thong <thongnmtran@kms-technology.com>
2019-09-03 16:24:10 +0700

    use LoggerSingleton

--- source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/preference/PluginPreferencePage.java
+++ source/com.kms.katalon.composer.project/src/com/kms/katalon/composer/project/preference/PluginPreferencePage.java
@@ -20,100 +20,99 @@ import com.kms.katalon.execution.constants.PluginOptions;
 import com.kms.katalon.execution.setting.PluginSettingStore;
 import com.kms.katalon.preferences.internal.PreferenceStoreManager;
 import com.kms.katalon.preferences.internal.ScopedPreferenceStore;
+import com.kms.katalon.composer.components.log.LoggerSingleton;
 import com.kms.katalon.composer.project.constants.StringConstants;
 
 public class PluginPreferencePage extends PreferencePage {
-    
-    private PluginSettingStore pluginSettingStore;
 
-    private Map<PluginOptions, Button> reloadOptionButtons;
-    
-    public PluginPreferencePage() {
-        pluginSettingStore = new PluginSettingStore(ProjectController.getInstance().getCurrentProject());
-        reloadOptionButtons = new HashMap<>();
-    }
+	private PluginSettingStore pluginSettingStore;
 
-    @Override
-    protected Control createContents(Composite parent) {
-        Composite container = new Composite(parent, SWT.NONE);
-        container.setLayout(new GridLayout());
-        container.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true));
-        
-        Composite composite = new Composite(container, SWT.NONE);
-        composite.setLayout(new GridLayout(3, false));
-        composite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
-        
-        Group grpReloadPluginOptions = new Group(container, SWT.NONE);
-        grpReloadPluginOptions.setText(StringConstants.PAGE_GRP_PLUGIN_REPOSITORY);
-        grpReloadPluginOptions.setLayout(new GridLayout(1, false));
-        grpReloadPluginOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+	private Map<PluginOptions, Button> reloadOptionButtons;
 
-        Button rbtnOnlineAndOfflineOpt = new Button(grpReloadPluginOptions, SWT.RADIO);
-        rbtnOnlineAndOfflineOpt.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-        rbtnOnlineAndOfflineOpt.setText(StringConstants.PAGE_OPTION_RELOAD_ONLINE_AND_OFFLINE);
-        reloadOptionButtons.put(PluginOptions.ONLINE_AND_OFFLINE, rbtnOnlineAndOfflineOpt);
+	public PluginPreferencePage() {
+		pluginSettingStore = new PluginSettingStore(ProjectController.getInstance().getCurrentProject());
+		reloadOptionButtons = new HashMap<>();
+	}
 
-        Button rbtnOnlineOpt = new Button(grpReloadPluginOptions, SWT.RADIO);
-        rbtnOnlineOpt.setText(StringConstants.PAGE_OPTION_RELOAD_ONLINE);
-        reloadOptionButtons.put(PluginOptions.ONLINE, rbtnOnlineOpt);
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(new GridLayout());
+		container.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true));
 
-        Button rbtnOfflineOpt = new Button(grpReloadPluginOptions, SWT.RADIO);
-        rbtnOfflineOpt.setText(StringConstants.PAGE_OPTION_RELOAD_OFFLINE);
-        reloadOptionButtons.put(PluginOptions.OFFLINE, rbtnOfflineOpt);
+		Composite composite = new Composite(container, SWT.NONE);
+		composite.setLayout(new GridLayout(3, false));
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
 
-        PluginOptions reloadOption = getReloadOption();
-        if (reloadOption != null) {
-            reloadOptionButtons.get(reloadOption).setSelection(true);
-        }
-        
-        return composite;
-    }
-    
-    @Override
-    public boolean performOk() {
-        if (!isControlCreated()) {
-            return super.performOk();
-        }
-        boolean performOk = super.performOk();
-        if (performOk) {
-            PluginOptions selectedReloadOption = reloadOptionButtons.entrySet().stream()
-                    .filter(entry -> entry.getValue().getSelection()).findFirst().get().getKey();
-            setReloadOption(selectedReloadOption);
-        }
+		Group grpReloadPluginOptions = new Group(container, SWT.NONE);
+		grpReloadPluginOptions.setText(StringConstants.PAGE_GRP_PLUGIN_REPOSITORY);
+		grpReloadPluginOptions.setLayout(new GridLayout(1, false));
+		grpReloadPluginOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 
-        return performOk;
-    }
+		Button rbtnOnlineAndOfflineOpt = new Button(grpReloadPluginOptions, SWT.RADIO);
+		rbtnOnlineAndOfflineOpt.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		rbtnOnlineAndOfflineOpt.setText(StringConstants.PAGE_OPTION_RELOAD_ONLINE_AND_OFFLINE);
+		reloadOptionButtons.put(PluginOptions.ONLINE_AND_OFFLINE, rbtnOnlineAndOfflineOpt);
 
-    @Override
-    protected void performDefaults() {
-        super.performDefaults();
-        getPreferenceStore().setToDefault(PreferenceConstants.PLUGIN_RELOAD_OPTION);
-        PluginOptions reloadOption = getReloadOption();
-        if (reloadOption != null) {
-            reloadOptionButtons.get(reloadOption).setSelection(true);
-        }
-    }
-    
-    public ScopedPreferenceStore getPreferenceStore() {
-        return PreferenceStoreManager.getPreferenceStore(IdConstants.KATALON_GENERAL_BUNDLE_ID);
-    }
-    
-    private PluginOptions getReloadOption() {
-    	try {
+		Button rbtnOnlineOpt = new Button(grpReloadPluginOptions, SWT.RADIO);
+		rbtnOnlineOpt.setText(StringConstants.PAGE_OPTION_RELOAD_ONLINE);
+		reloadOptionButtons.put(PluginOptions.ONLINE, rbtnOnlineOpt);
+
+		Button rbtnOfflineOpt = new Button(grpReloadPluginOptions, SWT.RADIO);
+		rbtnOfflineOpt.setText(StringConstants.PAGE_OPTION_RELOAD_OFFLINE);
+		reloadOptionButtons.put(PluginOptions.OFFLINE, rbtnOfflineOpt);
+
+		PluginOptions reloadOption = getReloadOption();
+		if (reloadOption != null) {
+			reloadOptionButtons.get(reloadOption).setSelection(true);
+		}
+
+		return composite;
+	}
+
+	@Override
+	public boolean performOk() {
+		if (!isControlCreated()) {
+			return super.performOk();
+		}
+		boolean performOk = super.performOk();
+		if (performOk) {
+			PluginOptions selectedReloadOption = reloadOptionButtons.entrySet().stream()
+					.filter(entry -> entry.getValue().getSelection()).findFirst().get().getKey();
+			setReloadOption(selectedReloadOption);
+		}
+
+		return performOk;
+	}
+
+	@Override
+	protected void performDefaults() {
+		super.performDefaults();
+		getPreferenceStore().setToDefault(PreferenceConstants.PLUGIN_RELOAD_OPTION);
+		PluginOptions reloadOption = getReloadOption();
+		if (reloadOption != null) {
+			reloadOptionButtons.get(reloadOption).setSelection(true);
+		}
+	}
+
+	public ScopedPreferenceStore getPreferenceStore() {
+		return PreferenceStoreManager.getPreferenceStore(IdConstants.KATALON_GENERAL_BUNDLE_ID);
+	}
+
+	private PluginOptions getReloadOption() {
+		try {
 			return pluginSettingStore.getReloadOption();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+            LoggerSingleton.getInstance().getLogger().error(e);
 		}
 		return null;
-    }
-    
-    private void setReloadOption(PluginOptions option) {
+	}
+
+	private void setReloadOption(PluginOptions option) {
         try {
 			pluginSettingStore.setReloadOption(option);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+            LoggerSingleton.getInstance().getLogger().error(e);
 		}
     }
 }
