56217e99cf90f59a96a7aa903a1d7d8db2efdb41
hiepdang1908 <anhtutran3007@gmail.com>
2019-09-24 14:38:14 +0700

    Option to disable sending tracking requests

--- source/com.kms.katalon/src/com/kms/katalon/composer/handlers/CheckForUpdateOnStartupHandler.java
+++ source/com.kms.katalon/src/com/kms/katalon/composer/handlers/CheckForUpdateOnStartupHandler.java
@@ -33,26 +33,9 @@ public class CheckForUpdateOnStartupHandler extends CheckForUpdatesHandler {
         IPreferenceStore prefStore = PreferenceStoreManager.getPreferenceStore(IdConstants.KATALON_GENERAL_BUNDLE_ID);
         boolean checkNewVersion = prefStore.contains(PreferenceConstants.GENERAL_AUTO_CHECK_NEW_VERSION)
                 ? prefStore.getBoolean(PreferenceConstants.GENERAL_AUTO_CHECK_NEW_VERSION) : true;
-
-        boolean checkAllowUsage = prefStore.contains(PreferenceConstants.GENERAL_AUTO_CHECK_ALLOW_USAGE_TRACKING)
-                ? prefStore.getBoolean(PreferenceConstants.GENERAL_AUTO_CHECK_ALLOW_USAGE_TRACKING) : true;
         if (!checkNewVersion) {
             return;
         }
-        else{
-            checkForUpdate(true);
-        }
-        if(!checkAllowUsage) {
-            return;
-        }
-        else{
-            trackingAllowUsage();
-        }
-
-    }
-
-    private void trackingAllowUsage() {
-        Trackings.trackOpenProject(ProjectController.getInstance().getCurrentProject());
-        
+        checkForUpdate(true);
     }
 }
