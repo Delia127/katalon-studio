075052db26a4591f6a32aca875c2cb62cedcbf8d
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2019-10-01 10:15:02 +0700

    issue3538: update

--- source/com.kms.katalon.core.webservice/src/com/kms/katalon/core/webservice/common/BasicRequestor.java
+++ source/com.kms.katalon.core.webservice/src/com/kms/katalon/core/webservice/common/BasicRequestor.java
@@ -321,16 +321,4 @@ public abstract class BasicRequestor implements Requestor {
         httpContext.setAttribute(SOCKET_FACTORY_REGISTRY, reg);
         return httpContext;
     }
-    
-    protected boolean isRedirectResponse(int statusCode) {
-        switch (statusCode) {
-        case 301:
-        case 302:
-        case 303:
-        case 307:
-            return true;
-        }
-        
-        return false;
-    }
 }
