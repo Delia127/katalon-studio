b8d154e1efe0db751550f7d249079e39a3f47262
Qui Le <quile@kms-technology.com>
2019-10-11 11:01:54 +0700

    update

--- source/com.kms.katalon.application/src/com/kms/katalon/application/utils/ActivationInfoCollector.java
+++ source/com.kms.katalon.application/src/com/kms/katalon/application/utils/ActivationInfoCollector.java
@@ -457,7 +457,7 @@ public class ActivationInfoCollector {
             } catch (Exception e) {
                 LogUtil.logError(e, "Error when check license");
             }
-        }, 0, 5, TimeUnit.SECONDS);
+        }, 0, 60, TimeUnit.SECONDS);
     }
 
     public static void postEndSession() {
