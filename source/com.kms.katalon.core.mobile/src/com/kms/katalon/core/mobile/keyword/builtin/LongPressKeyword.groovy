861c1ce584ff0d3f89878a2ebe0215ed0839cfea
Minh Thong <thongnmtran@kms-technology.com>
2019-10-30 11:39:03 +0700

    remove unused imports & update keyword documentation

--- source/com.kms.katalon.core.mobile/src/com/kms/katalon/core/mobile/keyword/builtin/LongPressKeyword.groovy
+++ source/com.kms.katalon.core.mobile/src/com/kms/katalon/core/mobile/keyword/builtin/LongPressKeyword.groovy
@@ -2,7 +2,6 @@ package com.kms.katalon.core.mobile.keyword.builtin
 
 import java.text.MessageFormat
 
-import org.codehaus.groovy.transform.tailrec.VariableReplacedListener.*
 import org.openqa.selenium.WebElement
 
 import com.kms.katalon.core.annotation.internal.Action
@@ -11,7 +10,6 @@ import com.kms.katalon.core.exception.StepFailedException
 import com.kms.katalon.core.helper.KeywordHelper
 import com.kms.katalon.core.keyword.internal.SupportLevel
 import com.kms.katalon.core.mobile.constants.StringConstants
-import com.kms.katalon.core.mobile.keyword.*
 import com.kms.katalon.core.mobile.keyword.internal.MobileAbstractKeyword
 import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
 import com.kms.katalon.core.mobile.keyword.internal.MobileKeywordMain
@@ -21,12 +19,11 @@ import com.kms.katalon.core.testobject.TestObject
 import groovy.transform.CompileStatic
 import io.appium.java_client.TouchAction
 import io.appium.java_client.touch.LongPressOptions
-import io.appium.java_client.touch.TapOptions
 import io.appium.java_client.touch.offset.ElementOption
 
 @Action(value = "longPress")
 public class LongPressKeyword extends MobileAbstractKeyword {
-    
+
     @CompileStatic
     @Override
     public SupportLevel getSupportLevel(Object ...params) {
@@ -53,10 +50,12 @@ public class LongPressKeyword extends MobileAbstractKeyword {
                 MobileKeywordMain.stepFailed(MessageFormat.format(StringConstants.KW_MSG_OBJ_NOT_FOUND, to.getObjectId()), flowControl, null, true)
                 return
             }
+
             TouchAction longPress = new TouchAction(MobileDriverFactory.getDriver())
-                        .longPress(LongPressOptions.longPressOptions().withElement(ElementOption.element(element, 1, 1)))
-                        .release()
+                    .longPress(LongPressOptions.longPressOptions().withElement(ElementOption.element(element, 1, 1)))
+                    .release()
             longPress.perform();
+
             logger.logPassed(MessageFormat.format(StringConstants.KW_LOG_PASSED_LONG_PRESSED_ON_ELEMENT, to.getObjectId()))
         }, flowControl, true, to != null ? MessageFormat.format(StringConstants.KW_MSG_FAILED_TO_LONG_PRESS_ON_ELEMENT_X, to.getObjectId()) : StringConstants.KW_MSG_FAILED_TO_LONG_PRESS_ON_ELEMENT)
     }
