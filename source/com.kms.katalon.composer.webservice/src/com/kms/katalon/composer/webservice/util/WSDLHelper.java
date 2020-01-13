7bb18991e62d1beffd9c285b238023646ed88832
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2020-01-13 17:20:57 +0700

    update

--- source/com.kms.katalon.composer.webservice/src/com/kms/katalon/composer/webservice/util/WSDLHelper.java
+++ source/com.kms.katalon.composer.webservice/src/com/kms/katalon/composer/webservice/util/WSDLHelper.java
@@ -498,22 +498,4 @@ public class WSDLHelper {
 
         return post;
     }
-
-    public static <T extends ExtensibilityElement> T getExtensiblityElement(List<?> list, Class<T> clazz) {
-        List<T> elements = getExtensiblityElements(list, clazz);
-        return elements.isEmpty() ? null : elements.get(0);
-    }
-
-    public static <T extends ExtensibilityElement> List<T> getExtensiblityElements(List list, Class<T> clazz) {
-        List<T> result = new ArrayList<T>();
-
-        for (Iterator<T> i = list.iterator(); i.hasNext(); ) {
-            T elm = i.next();
-            if (clazz.isAssignableFrom(elm.getClass())) {
-                result.add(elm);
-            }
-        }
-
-        return result;
-    }
 }
