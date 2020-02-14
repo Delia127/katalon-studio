b1947cf023480fac50f222f30e73b74f21634193
Minh Thong <thongnmtran@kms-technology.com>
2020-02-14 11:48:57 +0700

    Fix issue Unable to find Windows test object by relative path

--- source/com.kms.katalon.core/src/com/kms/katalon/core/testobject/ObjectRepository.java
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/testobject/ObjectRepository.java
@@ -180,12 +180,12 @@ public class ObjectRepository {
         return readTestObjectFile(testObjectId, objectFile, RunConfiguration.getProjectDir(), variables);
     }
     
-    public static WindowsTestObject findWindowsObject(final String windowsObjectId) {
-        File objectFile = new File(RunConfiguration.getProjectDir(), windowsObjectId + ".wrs");
-        return WindowsObjectRepository.readWindowsTestObjectFile(windowsObjectId, objectFile, RunConfiguration.getProjectDir(), Collections.emptyMap());
+    public static WindowsTestObject findWindowsObject(final String windowsObjectRelativeId) {
+        return findWindowsObject(windowsObjectRelativeId, Collections.emptyMap());
     }
     
-    public static WindowsTestObject findWindowsObject(final String windowsObjectId, Map<String, Object> variables) {
+    public static WindowsTestObject findWindowsObject(final String windowsObjectRelativeId, Map<String, Object> variables) {
+        String windowsObjectId = getTestObjectId(windowsObjectRelativeId);
         File objectFile = new File(RunConfiguration.getProjectDir(), windowsObjectId + ".wrs");
         return WindowsObjectRepository.readWindowsTestObjectFile(windowsObjectId, objectFile, RunConfiguration.getProjectDir(), variables);
     }
