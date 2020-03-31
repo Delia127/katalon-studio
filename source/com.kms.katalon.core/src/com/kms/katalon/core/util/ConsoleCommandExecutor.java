964fc3382bb1370dbc4eec73548c6404ed749eda
minhthanh3145 <mitatoto1997@gmail.com>
2020-03-31 10:10:42 +0700

    Add UTF-8 encoding

--- source/com.kms.katalon.core/src/com/kms/katalon/core/util/ConsoleCommandExecutor.java
+++ source/com.kms.katalon.core/src/com/kms/katalon/core/util/ConsoleCommandExecutor.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,7 +40,7 @@ public class ConsoleCommandExecutor {
         Process process = pb.start();
         List<String> resultLines = new ArrayList<String>();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
         try {
             String line;
             while ((line = br.readLine()) != null) {
