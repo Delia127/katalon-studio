a638c79cec80aba390eabd32b649ab1b1a398020
linhdoo <linhtkdo@kms-technology.com>
2020-06-16 14:39:00 +0700

    Format

--- source/com.kms.katalon.selenium.ide/src/com/kms/katalon/selenium/ide/model/TestSuite.java
+++ source/com.kms.katalon.selenium.ide/src/com/kms/katalon/selenium/ide/model/TestSuite.java
@@ -3,35 +3,35 @@ package com.kms.katalon.selenium.ide.model;
 import java.util.List;
 
 public class TestSuite {
-	private String filePath;
-	private String name;
-	private List<TestCase> testCases;
+    private String filePath;
+    private String name;
+    private List<TestCase> testCases;
     private List<String> tests;
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public List<TestCase> getTestCases() {
-		return testCases;
-	}
-	
-	public void setTestCases(List<TestCase> testCases) {
-		this.testCases = testCases;
-	}
-	
-	public String getFilePath() {
-		return filePath;
-	}
-	
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
-	
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<TestCase> getTestCases() {
+        return testCases;
+    }
+
+    public void setTestCases(List<TestCase> testCases) {
+        this.testCases = testCases;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
     public List<String> getTests() {
         return tests;
     }
@@ -40,8 +40,8 @@ public class TestSuite {
         this.tests = tests;
     }
 
-	@Override
-	public String toString() {
-		return "TestSuite [name" + name + ", testCases=" + testCases + "]";
-	}
+    @Override
+    public String toString() {
+        return "TestSuite [name" + name + ", testCases=" + testCases + "]";
+    }
 }
