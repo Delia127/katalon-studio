b192e520a056ecd38f3d5194edfac243e54f82e4
HuyenNguyen <huyenthingocnguyen@kms-technology.com>
2020-06-25 09:17:09 +0700

    Auto resize Test Suite Columns after saving

--- source/com.kms.katalon.composer.testsuite.collection/src/com/kms/katalon/composer/testsuite/collection/part/TestSuiteCollectionPart.java
+++ source/com.kms.katalon.composer.testsuite.collection/src/com/kms/katalon/composer/testsuite/collection/part/TestSuiteCollectionPart.java
@@ -12,7 +12,6 @@ import javax.inject.Inject;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.SerializationUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileInfo;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
@@ -31,6 +30,7 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnPixelData;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -72,8 +72,6 @@ import com.kms.katalon.composer.components.impl.editors.DefaultTableColumnViewer
 import com.kms.katalon.composer.components.impl.event.EventServiceAdapter;
 import com.kms.katalon.composer.components.impl.tree.TestSuiteCollectionTreeEntity;
 import com.kms.katalon.composer.components.impl.util.ControlUtils;
-import com.kms.katalon.composer.components.impl.util.EntityPartUtil;
-import com.kms.katalon.composer.components.impl.util.EventUtil;
 import com.kms.katalon.composer.components.impl.util.MenuUtils;
 import com.kms.katalon.composer.components.impl.util.TreeEntityUtil;
 import com.kms.katalon.composer.components.log.LoggerSingleton;
@@ -100,15 +98,12 @@ import com.kms.katalon.composer.testsuite.collection.part.support.RunEnabledEdit
 import com.kms.katalon.composer.testsuite.collection.part.support.TestSuiteIdEditingSupport;
 import com.kms.katalon.composer.testsuite.collection.transfer.TestSuiteRunConfigurationTransfer;
 import com.kms.katalon.composer.testsuite.collection.view.TestSuiteCollectionViewFactory;
-import com.kms.katalon.composer.testsuite.parts.TestSuiteCompositePart;
 import com.kms.katalon.constants.DocumentationMessageConstants;
 import com.kms.katalon.constants.EventConstants;
 import com.kms.katalon.controller.ProjectController;
 import com.kms.katalon.controller.TestSuiteCollectionController;
 import com.kms.katalon.dal.exception.DALException;
 import com.kms.katalon.entity.file.FileEntity;
-import com.kms.katalon.entity.project.ProjectEntity;
-import com.kms.katalon.entity.project.ProjectType;
 import com.kms.katalon.entity.testsuite.TestSuiteCollectionEntity;
 import com.kms.katalon.entity.testsuite.TestSuiteCollectionEntity.ExecutionMode;
 import com.kms.katalon.entity.testsuite.TestSuiteEntity;
@@ -165,6 +160,8 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
     private Callable<Boolean> enableWhenItemSelected;
     
     private TestSuiteCollectionCompositePart parentCompositePart;
+
+    private List<TableColumn> columnsNeedResize;
 
     private Listener layoutExecutionInformationCompositeListener = new Listener() {
 
@@ -234,6 +231,7 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
 
     private void updateInput() {
         tableViewer.setInput(cloneTestSuite.getTestSuiteRunConfigurations());
+        this.resizeTableColumns(columnsNeedResize);
         updateExecutionInfoInput();
         updateRunColumn();
 
@@ -500,13 +498,15 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         Table testSuiteWrapperTable = tableViewer.getTable();
         testSuiteWrapperTable.setLinesVisible(ControlUtils.shouldLineVisble(testSuiteWrapperTable.getDisplay()));
         testSuiteWrapperTable.setHeaderVisible(true);
+        
+        columnsNeedResize = new ArrayList<>();
 
         TableViewerColumn tbvcNo = new TableViewerColumn(tableViewer, SWT.NONE);
         TableColumn tblclmnNo = tbvcNo.getColumn();
         tblclmnNo.setText(StringConstants.NO_);
         tbvcNo.setLabelProvider(
                 new TestSuiteRunConfigLabelProvider(this, TestSuiteRunConfigLabelProvider.NO_COLUMN_IDX));
-        tableLayout.setColumnData(tblclmnNo, new ColumnWeightData(1, 50));
+        tableLayout.setColumnData(tblclmnNo, new ColumnWeightData(0, 40));
 
         TableViewerColumn tbvcId = new TableViewerColumn(tableViewer, SWT.NONE);
         TableColumn tblclmnId = tbvcId.getColumn();
@@ -514,7 +514,7 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         tbvcId.setEditingSupport(new TestSuiteIdEditingSupport(this));
         tbvcId.setLabelProvider(
                 new TestSuiteRunConfigLabelProvider(this, TestSuiteRunConfigLabelProvider.ID_COLUMN_IDX));
-        tableLayout.setColumnData(tblclmnId, new ColumnWeightData(50, 300));
+        tableLayout.setColumnData(tblclmnId, new ColumnWeightData(1, 200));
 
         TableViewerColumn tbvcRunWith = new TableViewerColumn(tableViewer, SWT.NONE);
         TableColumn tblclmnEnviroment = tbvcRunWith.getColumn();
@@ -522,7 +522,8 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         tbvcRunWith.setEditingSupport(new RunConfigurationChooserEditingSupport(this));
         tbvcRunWith.setLabelProvider(
                 new TestSuiteRunConfigLabelProvider(this, TestSuiteRunConfigLabelProvider.RUN_WITH_COLUMN_IDX));
-        tableLayout.setColumnData(tblclmnEnviroment, new ColumnWeightData(20, 70));
+        tableLayout.setColumnData(tblclmnEnviroment, new ColumnPixelData(80, true, true));
+        columnsNeedResize.add(tblclmnEnviroment);
 
         TableViewerColumn tbvcRunWithData = new TableViewerColumn(tableViewer, SWT.NONE);
         TableColumn tblclmnRunWithData = tbvcRunWithData.getColumn();
@@ -530,7 +531,8 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         tbvcRunWithData.setEditingSupport(new RunConfigurationDataEditingSupport(this));
         tbvcRunWithData.setLabelProvider(
                 new TestSuiteRunConfigLabelProvider(this, TestSuiteRunConfigLabelProvider.RUN_WITH_DATA_COLUMN_IDX));
-        tableLayout.setColumnData(tblclmnRunWithData, new ColumnWeightData(40, 200));
+        tableLayout.setColumnData(tblclmnRunWithData, new ColumnPixelData(70, false, true));
+        columnsNeedResize.add(tblclmnRunWithData);
 
         TableViewerColumn tbvcProfile = new TableViewerColumn(tableViewer, SWT.NONE);
         TableColumn tblclmnProfile = tbvcProfile.getColumn();
@@ -538,7 +540,8 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         tbvcProfile.setLabelProvider(
                 new TestSuiteRunConfigLabelProvider(this, TestSuiteRunConfigLabelProvider.PROFILE_COLUMN_IDX));
         tbvcProfile.setEditingSupport(new ExecutionProfileEditingSupport(this));
-        tableLayout.setColumnData(tblclmnProfile, new ColumnWeightData(20, 100));
+        tableLayout.setColumnData(tblclmnProfile, new ColumnPixelData(70, false, true));
+        columnsNeedResize.add(tblclmnProfile);
 
         TableViewerColumn tbvcRun = new TableViewerColumn(tableViewer, SWT.NONE);
         tblclmnRun = tbvcRun.getColumn();
@@ -546,7 +549,7 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         tbvcRun.setEditingSupport(new RunEnabledEditingSupport(this));
         tbvcRun.setLabelProvider(
                 new TestSuiteRunConfigLabelProvider(this, TestSuiteRunConfigLabelProvider.RUN_COLUMN_IDX));
-        tableLayout.setColumnData(tblclmnRun, new ColumnWeightData(10, 70));
+        tableLayout.setColumnData(tblclmnRun, new ColumnWeightData(0, 70));
 
         tableViewer.setContentProvider(new ArrayContentProvider());
         DefaultTableColumnViewerEditor.create(tableViewer);
@@ -904,4 +907,9 @@ public class TestSuiteCollectionPart extends EventServiceAdapter implements Tabl
         return mpart;
     }
 
+    private void resizeTableColumns(List<TableColumn> tableColumns) {
+        for (TableColumn column: tableColumns) {
+            column.pack();
+        }
+    }
 }
