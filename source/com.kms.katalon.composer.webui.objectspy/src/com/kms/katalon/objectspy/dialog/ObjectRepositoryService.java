681f66c1e441cae4ee5d8d372545b5bdb6032efe
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2020-07-13 11:46:21 +0700

    Update tracking for Spy Web Object

--- source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/dialog/ObjectRepositoryService.java
+++ source/com.kms.katalon.composer.webui.objectspy/src/com/kms/katalon/objectspy/dialog/ObjectRepositoryService.java
@@ -52,7 +52,7 @@ public class ObjectRepositoryService {
             clonedWebPage.setTag("cloned");
             for (ConflictWebElementWrapper webElementChildWrapper : webPageWrapper.getChildren()) {
                 if (webElementChildWrapper.isConflicted()) {
-                    addConflictedWebElement(page, (ConflictWebElementWrapper) webElementChildWrapper, dialogResult, testObjectIds);
+                    addConflictedWebElement(clonedWebPage, (ConflictWebElementWrapper) webElementChildWrapper, dialogResult, testObjectIds);
                 } else {
                     WebElement clonedWebElement = webElementChildWrapper.getOriginalWebElement();
                     clonedWebElement.setParent(clonedWebPage);
