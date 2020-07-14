38f00249e72f9915829de7c8ed82b5293930645c
minhthanh3145 <mitatoto1997@gmail.com>
2020-07-14 16:51:10 +0700

    Done STUDIO-254

--- source/com.kms.katalon.composer.webui.objectspy.test/src/com/kms/katalon/objectsly/util/test/WebElementUtilsTest.java
+++ source/com.kms.katalon.composer.webui.objectspy.test/src/com/kms/katalon/objectsly/util/test/WebElementUtilsTest.java
@@ -2,13 +2,13 @@ package com.kms.katalon.objectsly.util.test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
 import org.junit.Test;
 import org.testng.Assert;
 
+import com.kms.katalon.core.testobject.SelectorMethod;
 import com.kms.katalon.entity.folder.FolderEntity;
 import com.kms.katalon.entity.project.ProjectEntity;
 import com.kms.katalon.entity.repository.WebElementEntity;
@@ -39,5 +39,44 @@ public class WebElementUtilsTest {
         parentFolder.setName("parent_folder");
         WebElementEntity entity = WebElementUtils.convertWebElementToTestObject(webElement, null, parentFolder);
         Assert.assertEquals(entity.getName(), webElement.getName());
+    }
+
+    @Test
+    public void canConvertWebElementToTestObjectAndRemoveInvalidCharactersFromName() throws Exception {
+        WebElement webElement = new WebElement("This@is!my?name");
+        FolderEntity parentFolder = new FolderEntity();
+        ProjectEntity projectEntity = new ProjectEntity();
+        File file = getExtensionsDirectory("/test_project");
+        projectEntity.setProjectFile(file);
+        projectEntity.setName("test_project_name");
+        parentFolder.setProject(projectEntity);
+        parentFolder.setName("parent_folder");
+        WebElementEntity entity = WebElementUtils.convertWebElementToTestObject(webElement, null, parentFolder);
+        Assert.assertEquals(entity.getName(), "Thisismyname");
+    }
+
+    @Test
+    public void canConvertWebElementToTestObjectAndRetainCorrectInformation() throws Exception {
+        WebElement webElement = new WebElement("This is my name");
+        webElement.addProperty("id", "my_id");
+        webElement.addProperty("name", "my_name");
+        webElement.addXpath("neighbor", "neighbor_xpath");
+        webElement.addXpath("id", "id_xpath");
+        webElement.setSelectorMethod(SelectorMethod.XPATH);
+        webElement.setTag("a");
+        webElement.setSelectorValue(SelectorMethod.XPATH, "selected_xpath");
+        FolderEntity parentFolder = new FolderEntity();
+        ProjectEntity projectEntity = new ProjectEntity();
+        File file = getExtensionsDirectory("/test_project");
+        projectEntity.setProjectFile(file);
+        projectEntity.setName("test_project_name");
+        parentFolder.setProject(projectEntity);
+        parentFolder.setName("parent_folder");
+        WebElementEntity entity = WebElementUtils.convertWebElementToTestObject(webElement, null, parentFolder);
+        Assert.assertEquals(entity.getName(), webElement.getName());
+        Assert.assertEquals(entity.getWebElementProperties(), webElement.getProperties());
+        Assert.assertEquals(entity.getWebElementXpaths(), webElement.getXpaths());
+        Assert.assertEquals(entity.getSelectorMethod().toString(), webElement.getSelectorMethod().toString());
+        Assert.assertEquals(entity.getSelectorCollection().values(), webElement.getSelectorCollection().values());
     }
 }
