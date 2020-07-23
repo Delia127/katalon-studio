a849ac527cd613de1ac7605a6403e8042d8fc922
Minh Thong <thongnmtran@kms-technology.com>
2020-07-23 15:02:19 +0700

    update

--- source/com.kms.katalon.composer.webui.recorder/src/com/kms/katalon/composer/webui/recorder/handler/RecordHandler.java
+++ source/com.kms.katalon.composer.webui.recorder/src/com/kms/katalon/composer/webui/recorder/handler/RecordHandler.java
@@ -18,8 +18,8 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.IJobChangeListener;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
@@ -312,33 +312,13 @@ public class RecordHandler {
             }
         };
         
-        job.addJobChangeListener(new IJobChangeListener() {
+        job.addJobChangeListener(new JobChangeAdapter() {
             @Override
             public void done(IJobChangeEvent event) {
                 UserProfile currentProfile = UserProfileHelper.getCurrentProfile();
                 if (newTestCase != null && currentProfile.isNewUser() && !currentProfile.isDoneRunFirstTestCase()) {
                     eventBroker.post(EventConstants.FIRST_TEST_CASE_CREATED, newTestCase);
                 }
-            }
-
-            @Override
-            public void aboutToRun(IJobChangeEvent event) {
-            }
-
-            @Override
-            public void awake(IJobChangeEvent event) {
-            }
-
-            @Override
-            public void running(IJobChangeEvent event) {
-            }
-
-            @Override
-            public void scheduled(IJobChangeEvent event) {
-            }
-
-            @Override
-            public void sleeping(IJobChangeEvent event) {
             }
         });
 
